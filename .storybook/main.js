c6f473879435a074a511d375baf36e923265a86c
Niels Lyngsø <niels.lyngso@gmail.com>
2020-11-26 08:44:07 +0100

    rename to story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../**/out-tsc/src/**/*.story.js'],
+  stories: ['../**/out-tsc/src/**/*.stories.js'],
   addons: [
     'storybook-prebuilt/addon-knobs/register.js',
     'storybook-prebuilt/addon-docs/register.js',
