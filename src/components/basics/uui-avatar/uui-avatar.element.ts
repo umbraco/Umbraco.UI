48d912e8264e859aa39cf5fcfeeb74f7252b2ef6
Mads Rasmussen <madsr@hey.com>
2021-01-14 14:51:59 +0100

    init uui-avatar component

---
+++ src/components/basics/uui-avatar/uui-avatar.element.ts
@@ -0,0 +1,11 @@
+import { LitElement, html } from 'lit-element';
+
+/**
+ *  @element uui-avatar
+ */
+
+export class UUIAvatarElement extends LitElement {
+  render() {
+    return html`<div>Avatar</div>`;
+  }
+}
---
+++ src/components/basics/uui-avatar/uui-avatar.story.ts
@@ -0,0 +1,9 @@
+import { html } from 'lit-html';
+import './index';
+
+export default {
+  title: 'Basics/Avatar',
+  component: 'uui-avatar',
+};
+
+export const Overview = () => html` <uui-avatar></uui-avatar> `;
