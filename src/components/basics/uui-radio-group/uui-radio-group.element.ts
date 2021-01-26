7179e7e187a5a841c948ceaa3252b0aeff7bdbf5
Julia Gru <56249914+julczka@users.noreply.github.com>
2021-01-26 15:41:31 +0100

    Update uui-radio-group.element.ts

--- src/components/basics/uui-radio-group/uui-radio-group.element.ts
+++ src/components/basics/uui-radio-group/uui-radio-group.element.ts
@@ -6,18 +6,37 @@ import { UUIRadioElement } from '../uui-radio/uui-radio.element';
  */
 
 //TODO internals and expose a form value
+//TODO required
+//TODO disabled
 //TODO styles - where can i find it
 //TODO keyboard nav and focus
+//? what types should we allow as a value of that?
 
 export class UUIRadioGroup extends LitElement {
   static styles = [css``];
 
+  static formAssociated = true;
+
+  private _internals;
+
   constructor() {
     super();
+    this._internals = (this as any).attachInternals();
     this.addEventListener('change', this._handleSelect);
   }
 
   @query('slot') protected slotElement!: HTMLSlotElement;
+
+  private _value: string | null = null;
+
+  @property()
+  get value() {
+    return this._value;
+  }
+  set value(newValue) {
+    this._value = newValue;
+    this._internals.setFormValue(this._value);
+  }
 
   protected get radioElements(): UUIRadioElement[] {
     return this.slotElement
@@ -35,6 +54,7 @@ export class UUIRadioGroup extends LitElement {
     radios.forEach(el => {
       if (el === e.target) {
         selectedElement = el;
+        this._value = selectedElement.value;
       }
     });
 
