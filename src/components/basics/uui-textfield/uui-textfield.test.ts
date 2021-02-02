77eb85bc27665be67953abfe70099eaa68657bab
Niels Lyngsø <nsl@umbraco.com>
2021-02-02 13:19:57 +0100

    uui-textfield test

--- src/components/basics/uui-textfield/uui-textfield.test.ts
+++ src/components/basics/uui-textfield/uui-textfield.test.ts
@@ -6,9 +6,7 @@ import { UUITextFieldElement } from './uui-textfield.element';
 describe('UuiTextfield', () => {
   let element: UUITextFieldElement;
   beforeEach(async () => {
-    element = await fixture(
-      html` <uui-textfield>Hello uui-textfield</uui-textfield> `
-    );
+    element = await fixture(html` <uui-textfield></uui-textfield> `);
   });
 
   it('passes the a11y audit', async () => {
@@ -17,5 +15,36 @@ describe('UuiTextfield', () => {
 
   it('test that disable works', async () => {
     return true;
+  });
+});
+
+describe('UuiToggle in Form', () => {
+  let formElement: HTMLFormElement;
+  let element: UUITextFieldElement;
+  beforeEach(async () => {
+    formElement = await fixture(
+      html` <form>
+        <uui-textfield
+          name="textfield"
+          value="Hello uui-textfield"
+        ></uui-textfield>
+      </form>`
+    );
+    element = formElement.querySelector('uui-textfield') as any;
+  });
+
+  it('value is correct', async () => {
+    await expect(element.value).to.be.equal('Hello uui-textfield');
+  });
+
+  it('form output', async () => {
+    const formData = new FormData(formElement);
+    await expect(formData.get('textfield')).to.be.equal('Hello uui-textfield');
+  });
+
+  it('change value and check output', async () => {
+    element.value = 'anotherValue';
+    const formData = new FormData(formElement);
+    await expect(formData.get('textfield')).to.be.equal('anotherValue');
   });
 });
