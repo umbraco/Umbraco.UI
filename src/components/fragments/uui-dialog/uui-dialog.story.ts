090546201455c798f4f04a89a8d36571a60d7acd
Niels Lyngsø <nsl@umbraco.com>
2021-02-24 14:20:39 +0100

    format uui-dialog story

--- src/components/fragments/uui-dialog/uui-dialog.story.ts
+++ src/components/fragments/uui-dialog/uui-dialog.story.ts
@@ -1,34 +1,30 @@
 import { html } from 'lit-html';
-import './index';
-import '../../basics/uui-button/index';
+import '.';
+import '../../basics/uui-button';
 
 export default {
   title: 'Fragments/Dialog',
   component: 'uui-dialog',
 };
 
-export const Default = () => html` <uui-dialog> </uui-dialog> `;
+export const Default = () => html` <uui-dialog></uui-dialog> `;
 
 export const Slots = () => html`
   <uui-dialog>
-    <uui-button look="slot" style="width:100%; height: 96px;"
-      >Default slot</uui-button
-    >
-    <uui-button look="slot" style="width:50%;" slot="actions"
-      >Actions slot</uui-button
-    >
+    <uui-button look="slot" style="width:100%; height: 96px;">
+      Default slot
+    </uui-button>
+    <uui-button look="slot" style="width:50%;" slot="actions">
+      Actions slot
+    </uui-button>
   </uui-dialog>
 `;
 
 export const WithContent = () => html`
   <uui-dialog>
-    <div>
-      <h3>Headline</h3>
-      <p>Some description for this dialog</p>
-    </div>
-    <div slot="actions">
-      <uui-button>Cancel</uui-button>
-      <uui-button look="positive">Confirm</uui-button>
-    </div>
+    <h3>Headline</h3>
+    <p>Some description for this dialog</p>
+    <uui-button slot="actions">Cancel</uui-button>
+    <uui-button slot="actions" look="positive">Confirm</uui-button>
   </uui-dialog>
 `;
