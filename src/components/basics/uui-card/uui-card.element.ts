667f10173b74956c611c63f006c36a393ddc0a61
Julia Gru <56249914+julczka@users.noreply.github.com>
2021-03-11 10:08:49 +0100

    init

---
+++ src/components/basics/uui-card/uui-card.element.ts
@@ -0,0 +1,19 @@
+import { LitElement, html, css, property } from 'lit-element';
+/**
+ *  @element uui-card
+ *
+ */
+
+// TODO: Allow for slotted SVG.
+export class UUICardElement extends LitElement {
+  static styles = [
+    css`
+      :host {
+      }
+    `,
+  ];
+
+  render() {
+    return html`Hello`;
+  }
+}
---
+++ src/components/basics/uui-card/uui-card.story.ts
@@ -0,0 +1,9 @@
+import { html } from 'lit-html';
+import './index';
+
+export default {
+  title: 'Basics/Card',
+  component: 'uui-card',
+};
+
+export const Basic = () => html` <uui-card></uui-card> `;
