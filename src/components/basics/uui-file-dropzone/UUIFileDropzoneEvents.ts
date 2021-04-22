0d81068c5927dd927cd6626624dcd8035bc1c794
Julia gru <julia.gru@gmail.com>
2021-04-22 15:00:38 +0200

    file uploader rename to file dropzone

--- src/components/basics/uui-file-dropzone/UUIFileDropzoneEvents.ts
+++ src/components/basics/uui-file-dropzone/UUIFileDropzoneEvents.ts
@@ -1,5 +1,5 @@
 import { UUIEvent } from '../../../event/UUIEvent';
-import { UUIFileDropzoneElement } from './uui-file-uploader.element';
+import { UUIFileDropzoneElement } from './uui-file-dropzone.element';
 
 export class UUIFileUploaderEvent extends UUIEvent<{}, UUIFileDropzoneElement> {
   public static readonly FILE_DROP: string = 'file-drop';
