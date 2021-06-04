d408f93530407fd815e94f72a9c8c4e88418f6e8
Niels Lyngsø <nsl@umbraco.com>
2021-06-04 10:18:12 +0200

    merge files into select

--- src/components/uui-select/uui-select.element.ts
+++ src/components/uui-select/uui-select.element.ts
@@ -1,14 +1,16 @@
-import { html, css } from 'lit';
-import { query, property } from 'lit/decorators';
+import { html, css, LitElement } from 'lit';
+import { query, property, state } from 'lit/decorators';
 import { UUIDropdownElement } from '../uui-dropdown/uui-dropdown.element';
-import { UUISingleSelectBaseElement } from './uui-single-select-base.element';
-import { keys } from './keys';
+import { UUIOverflowContainer } from '../uui-overflow-container/uui-overflow-container.element';
+import { UUISelectOptionElement } from '../uui-select-option/uui-select-option.element';
+import { UUISelectEvent } from './UUISelectEvent';
+import { UUISelectOptionEvent } from '../uui-select-option/UUISelectOptionEvent';
 /**
  *  @element uui-select
  *  @slot - for stuff
  */
 
-export class UUISelectElement extends UUISingleSelectBaseElement {
+export class UUISelectElement extends LitElement {
   static styles = [
     css`
       :host {
@@ -111,16 +113,74 @@ export class UUISelectElement extends UUISingleSelectBaseElement {
   @property()
   placeholder = '';
 
+  @query('uui-overflow-container')
+  overflow!: UUIOverflowContainer;
+
+  @query('slot') protected slotElement!: HTMLSlotElement;
+  protected listElements!: UUISelectOptionElement[];
+
+  //returns an Array of ListElements if they're in the slot or empty array
+  protected getListElements(): UUISelectOptionElement[] {
+    return this.slotElement
+      ? (this.slotElement
+          .assignedElements({ flatten: true })
+          .filter(
+            el => el instanceof UUISelectOptionElement
+          ) as UUISelectOptionElement[])
+      : [];
+  }
+
+  protected onSlotChange() {
+    if (this.listElements) {
+      this.listElements.forEach(el => {
+        el.removeEventListener(
+          'change',
+          this.onListElementChange as EventListener
+        );
+      });
+    }
+    this.listElements =
+      (this.slotElement
+        .assignedElements({ flatten: true })
+        .filter(
+          el => el instanceof UUISelectOptionElement
+        ) as UUISelectOptionElement[]) || [];
+    this.updateSelectedElement();
+    this.listElements.forEach(el => {
+      el.addEventListener('change', this.onListElementChange as EventListener);
+    });
+  }
+
+  private _value = '';
+  @property()
+  get value() {
+    return this._value;
+  }
+  set value(newValue) {
+    const oldVal = this._value;
+    this._value = newValue;
+    this.updateSelectedElement();
+    this.deselectChildren();
+    this.requestUpdate('value', oldVal);
+  }
+
+  @state()
+  protected selectedElement: UUISelectOptionElement | null = null;
+  protected updateSelectedElement() {
+    this.selectedElement =
+      this.listElements?.find(el => el.value === this._value) || null;
+  }
+
   private _onKeydown(e: KeyboardEvent) {
     switch (e.key) {
-      case keys.ARROW_UP: {
+      case 'ArrowUp': {
         e.preventDefault();
         if (!this.open) this.open = true;
         this.selectPreviousElement();
         break;
       }
 
-      case keys.ARROW_DOWN: {
+      case 'ArrowDown': {
         e.preventDefault();
 
         if (!this.open) this.open = true;
@@ -128,34 +188,104 @@ export class UUISelectElement extends UUISingleSelectBaseElement {
         break;
       }
 
-      case keys.SPACE:
-      case keys.ENTER: {
+      case ' ':
+      case 'Enter': {
         e.preventDefault();
         this.open = !this.open;
         break;
       }
 
-      case keys.ESCAPE: {
+      case 'Escape': {
         e.preventDefault();
         if (this.open) this.open = false;
         break;
       }
 
-      case keys.TAB: {
+      case 'Tab': {
         if (this.open) this.open = false;
         break;
       }
 
-      case keys.HOME: {
+      case 'Home': {
         this.selectIndex(0);
         break;
       }
 
-      case keys.END: {
+      case 'End': {
         this.selectIndex(this.listElements.length - 1);
         break;
       }
     }
+  }
+
+  private deselectChildren() {
+    if (this.listElements) {
+      const notValue = this.listElements.filter(el => el.value !== this._value);
+      notValue.forEach(el => el.deselect());
+    }
+  }
+
+  protected onListElementChange = (e: UUISelectOptionEvent) => {
+    if (e.target.selected === true) {
+      this.value = e.target.value;
+      this._fireChangeEvent();
+    }
+  };
+
+  private _fireChangeEvent() {
+    this.dispatchEvent(new UUISelectEvent(UUISelectEvent.CHANGE));
+  }
+
+  protected selectIndex(index: number) {
+    if (this.listElements && index < this.listElements.length) {
+      this.select(this.listElements[index]);
+      this._fireChangeEvent();
+    }
+  }
+  protected select(newSelection: UUISelectOptionElement) {
+    newSelection.select();
+    this.value = newSelection.value;
+  }
+  protected selectPreviousElement() {
+    this.moveSelection(-1);
+  }
+  protected selectNextElement() {
+    this.moveSelection(1);
+  }
+  private moveSelection(move: number) {
+    let newSelection: UUISelectOptionElement | null = null;
+    if (this.listElements && this.listElements.length > 0) {
+      if (this.selectedElement) {
+        const index: number = this.listElements.indexOf(this.selectedElement);
+        if (index !== -1) {
+          newSelection = this.getNextEnabledElement(index, index, move);
+        }
+      } else {
+        newSelection = this.listElements[
+          move > 0 ? 0 : this.listElements.length - 1
+        ];
+      }
+      if (newSelection) {
+        this.select(newSelection);
+      }
+    }
+  }
+  private getNextEnabledElement(
+    originalIndex: number,
+    index: number,
+    move: number
+  ): UUISelectOptionElement | null {
+    let nextIndex = (index + move) % this.listElements.length;
+    if (nextIndex < 0) {
+      nextIndex += this.listElements.length;
+    }
+    if (originalIndex === nextIndex) {
+      return null;
+    }
+    if (this.listElements[nextIndex].disabled !== true) {
+      return this.listElements[nextIndex];
+    }
+    return this.getNextEnabledElement(originalIndex, nextIndex, move);
   }
 
   render() {
