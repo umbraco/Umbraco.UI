60f63312c0c0b5f721ad6559fc45b38dd269ebc1
Niels Lyngsø <nsl@umbraco.com>
2021-06-17 11:56:25 +0200

    lock-symbol

---
+++ src/components/uui-lock-symbol/uui-lock-symbol.story.ts
@@ -0,0 +1,16 @@
+import { html } from 'lit-html';
+import './index';
+
+export default {
+  title: 'Symbols/Lock Symbol',
+  component: 'uui-lock-symbol',
+};
+
+export const Overview = () =>
+  html`
+    <uui-lock-symbol
+      @click=${(e: MouseEvent) => {
+        (e.target as any).open = !(e.target as any).open;
+      }}
+    ></uui-lock-symbol>
+  `;
