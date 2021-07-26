453435577bfe119a522acbd24e63cf34127d3db4
Niels Lyngsø <nsl@umbraco.com>
2021-07-26 15:55:30 +0200

    only one active when interaction

--- src/components/uui-tab-group/uui-tab-group.element.ts
+++ src/components/uui-tab-group/uui-tab-group.element.ts
@@ -1,7 +1,4 @@
 import { LitElement, html, css } from 'lit';
-import { query, property } from 'lit/decorators';
-import { UUITabEvent } from '../uui-tab/UUITabEvent';
-import { UUITabGroupEvent } from './UUITabGroupEvent';
 import { UUITabElement } from '../uui-tab/uui-tab.element';
 
 /**
@@ -21,7 +18,38 @@ export class UUITabGroupElement extends LitElement {
     `,
   ];
 
+  private tabElements: UUITabElement[] = [];
+
+  private onSlotChange(e: any) {
+    if (this.tabElements) {
+      this.tabElements.forEach(el => {
+        el.removeEventListener(
+          'click',
+          this.onTabActive as EventHandlerNonNull
+        );
+      });
+    }
+    this.tabElements = (e.target as HTMLSlotElement)
+      .assignedElements({ flatten: true })
+      .filter(el => el instanceof UUITabElement) as UUITabElement[];
+
+    this.tabElements.forEach(el => {
+      el.addEventListener('click', this.onTabActive as EventHandlerNonNull);
+    });
+  }
+
+  private onTabActive = (e: MouseEvent) => {
+    const selectedElement: UUITabElement = e.target as UUITabElement;
+    selectedElement.active = true;
+
+    const filtered = this.tabElements.filter(el => el !== selectedElement);
+
+    filtered.forEach(el => {
+      el.active = false;
+    });
+  };
+
   render() {
-    return html` <slot></slot> `;
+    return html` <slot @slotchange=${this.onSlotChange}></slot> `;
   }
 }
