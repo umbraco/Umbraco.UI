e6e04c696caaa77c3972b3f543a8bc22b7e6020e
Niels Lyngsø <nsl@umbraco.com>
2021-08-09 11:40:49 +0200

    adding space in comment

--- src/components/uui-loader-circle/uui-loader-circle.element.ts
+++ src/components/uui-loader-circle/uui-loader-circle.element.ts
@@ -12,6 +12,7 @@ export class UUILoaderCircleElement extends LitElement {
   static styles = [
     css`
       :host {
+        /* currently this components color is defined through currentColor, if we like to use a different color, we need to implemenet a --uui-interface- color which will be set/overwritten when looks are set, aka. if this element is used within a button with the look danger, then this component would get an appropriate color. */
         --uui-loader-circle-size: var(--uui-size-small, 12px);
         display: inline-block;
         vertical-align: middle;
