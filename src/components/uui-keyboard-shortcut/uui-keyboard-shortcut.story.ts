18b0f2bec5fdc9429212d718302dd4b0d0f54523
Niels Lyngsø <nsl@umbraco.com>
2021-08-11 15:05:13 +0200

    linebreaks

--- src/components/uui-keyboard-shortcut/uui-keyboard-shortcut.story.ts
+++ src/components/uui-keyboard-shortcut/uui-keyboard-shortcut.story.ts
@@ -8,7 +8,12 @@ export default {
 
 export const Basic = () => html`
   <uui-keyboard-shortcut>
-    <uui-key>ALT</uui-key
-    >+<uui-key>shift</uui-key>+<uui-key>&#8593;</uui-key>+<uui-key>z</uui-key>
+    <uui-key>ALT</uui-key>
+    +
+    <uui-key>shift</uui-key>
+    +
+    <uui-key>&#8593;</uui-key>
+    +
+    <uui-key>z</uui-key>
   </uui-keyboard-shortcut>
 `;
