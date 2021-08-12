bbc82df9e92ad87374686204b772fd1beb7aafc1
Niels Lyngsø <nsl@umbraco.com>
2021-08-12 14:25:43 +0200

    re style story

--- src/components/uui-table/uui-table.story.ts
+++ src/components/uui-table/uui-table.story.ts
@@ -12,41 +12,48 @@ export const Basic = () =>
     <uui-table>
       <uui-table-column
         style="width: 50px; background-color: var(--uui-color-spanish-pink);"
-      ></uui-table-column>
-      <uui-table-head
-        >${ArrayOfUmbracoWords(5).map(
+      >
+      </uui-table-column>
+      <uui-table-head>
+        ${ArrayOfUmbracoWords(5).map(
           el => html`<uui-table-head-cell>${el}</uui-table-head-cell>`
-        )}</uui-table-head
-      ><uui-table-row
-        >${ArrayOfUmbracoWords(5).map(
+        )}
+      </uui-table-head>
+      <uui-table-row>
+        ${ArrayOfUmbracoWords(5).map(
           el => html`<uui-table-cell>${el}</uui-table-cell>`
-        )}</uui-table-row
-      ><uui-table-row
-        >${ArrayOfUmbracoWords(5).map(
+        )}
+      </uui-table-row>
+      <uui-table-row>
+        ${ArrayOfUmbracoWords(5).map(
           el => html`<uui-table-cell>${el}</uui-table-cell>`
-        )}</uui-table-row
-      ></uui-table
-    >
+        )}
+      </uui-table-row>
+    </uui-table>
   `;
 
 export const SelectableRows = () =>
   html`
     <div style="width: 100%;">
       <uui-table>
-        <uui-table-column style="width: 5%;"></uui-table-column>
-        <uui-table-head
-          >${ArrayOfUmbracoWords(5).map(
+        <uui-table-column
+          style="width: 5%; min-width: 32px; max-width: 48px;"
+        ></uui-table-column>
+        <uui-table-head>
+          ${ArrayOfUmbracoWords(5).map(
             el => html`<uui-table-head-cell>${el}</uui-table-head-cell>`
-          )}</uui-table-head
-        ><uui-table-row selectable
-          >${ArrayOfUmbracoWords(5).map(
+          )}
+        </uui-table-head>
+        <uui-table-row selectable>
+          ${ArrayOfUmbracoWords(5).map(
             el => html`<uui-table-cell>${el}</uui-table-cell>`
-          )}</uui-table-row
-        ><uui-table-row selectable
-          >${ArrayOfUmbracoWords(5).map(
+          )}
+        </uui-table-row>
+        <uui-table-row selectable>
+          ${ArrayOfUmbracoWords(5).map(
             el => html`<uui-table-cell>${el}</uui-table-cell>`
-          )}</uui-table-row
-        ></uui-table
-      >
+          )}
+        </uui-table-row>
+      </uui-table>
     </div>
   `;
