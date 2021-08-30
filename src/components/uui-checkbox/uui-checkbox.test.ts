b8b47b52202e7f4a39423bd34a9d901d0514ca95
Julia gru <julia.gru@gmail.com>
2021-08-30 16:13:52 +0200

    add additional tests for checkbox and toggle

--- src/components/uui-checkbox/uui-checkbox.test.ts
+++ src/components/uui-checkbox/uui-checkbox.test.ts
@@ -6,7 +6,7 @@ describe('UuiToggle', () => {
   let element: UUICheckboxElement;
   beforeEach(async () => {
     element = await fixture(
-      html` <uui-checkbox label="test label"></uui-checkbox> `
+      html` <uui-checkbox label="test label" name="test"></uui-checkbox> `
     );
   });
 
@@ -18,18 +18,26 @@ describe('UuiToggle', () => {
     await expect(element).to.have.property('_internals');
   });
 
-  it('has value', () => {
+  it('has default value equal to on', () => {
     expect(element.value).to.be.equal('on');
+  });
+
+  it('has name', () => {
+    expect(element.name).to.be.equal('test');
   });
 });
 
-describe('UuiToggle in a Form', () => {
+describe('UuiChebox in a Form', () => {
   let formElement: HTMLFormElement;
   let element: UUICheckboxElement;
   beforeEach(async () => {
     formElement = await fixture(
       html` <form>
-        <uui-checkbox name="test" label="test label"></uui-checkbox>
+        <uui-checkbox
+          name="test"
+          value="testValue"
+          label="test label"
+        ></uui-checkbox>
       </form>`
     );
     element = formElement.querySelector('uui-checkbox') as any;
@@ -40,16 +48,47 @@ describe('UuiToggle in a Form', () => {
     expect(formData.get(`${element.name}`)).to.be.equal(null);
   });
 
+  it('the form property on element internals is equal the form element', () => {
+    expect(element._internals.form).to.be.equal(formElement);
+  });
+
   it('form output is on if element is checked and has no value attribute', () => {
     element.checked = true;
     const formData = new FormData(formElement);
-    expect(formData.get(`${element.name}`)).to.be.equal('on');
+    expect(formData.get(`${element.name}`)).to.be.equal('testValue');
   });
 
-  it('element hav value atribute form value should be the same', () => {
+  it('element has value atribute. form value should be the same', () => {
     element.value = 'bike';
     element.checked = true;
     const formData = new FormData(formElement);
     expect(formData.get(`test`)).to.equal('bike');
+  });
+});
+
+describe('UuiToggle in a Form with no attributes', () => {
+  let formElement: HTMLFormElement;
+  let element: UUICheckboxElement;
+  beforeEach(async () => {
+    formElement = await fixture(
+      html` <form>
+        <uui-checkbox label="test label"></uui-checkbox>
+      </form>`
+    );
+    element = formElement.querySelector('uui-toggle') as any;
+  });
+
+  it('element does not create form value if no name is provided', () => {
+    element.name = '';
+
+    element.checked = true;
+
+    const formData = new FormData(formElement);
+    const formDataKeys: Array<any> = [];
+    formData.forEach(key => {
+      formDataKeys.push(key);
+    });
+
+    expect(formDataKeys.length).to.equal(0);
   });
 });
--- src/components/uui-toggle/uui-toggle.test.ts
+++ src/components/uui-toggle/uui-toggle.test.ts
@@ -1,6 +1,7 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import { UUIToggleElement } from './uui-toggle.element';
 import '.';
+import { property } from 'lit/decorators';
 
 describe('UuiToggle', () => {
   let element: UUIToggleElement;
@@ -18,8 +19,13 @@ describe('UuiToggle', () => {
     await expect(element).to.have.property('_internals');
   });
 
-  it('has value', () => {
+  it('has default value equal to on', () => {
     expect(element.value).to.be.equal('on');
+  });
+
+  it('can be checked', () => {
+    element.checked = true;
+    expect(element.checked).to.be.equal(true);
   });
 });
 
@@ -29,12 +35,19 @@ describe('UuiToggle in a Form', () => {
   beforeEach(async () => {
     formElement = await fixture(
       html` <form>
-        <uui-toggle name="test" label="test label"></uui-toggle>
+        <uui-toggle
+          label="test label"
+          name="test"
+          value="testValue"
+        ></uui-toggle>
       </form>`
     );
     element = formElement.querySelector('uui-toggle') as any;
   });
 
+  it('the form property on element internals is equal the form element', () => {
+    expect(element._internals.form).to.be.equal(formElement);
+  });
   it('form output is null if element not checked', () => {
     const formData = new FormData(formElement);
     expect(formData.get(`${element.name}`)).to.be.equal(null);
@@ -43,13 +56,40 @@ describe('UuiToggle in a Form', () => {
   it('form output is on if element is checked and has no value attribute', () => {
     element.checked = true;
     const formData = new FormData(formElement);
-    expect(formData.get(`${element.name}`)).to.be.equal('on');
+    expect(formData.get(`${element.name}`)).to.be.equal('testValue');
   });
 
-  it('element hav value atribute form value should be the same', () => {
+  it('if element has value atribute form value should be the same', () => {
     element.value = 'bike';
     element.checked = true;
     const formData = new FormData(formElement);
     expect(formData.get(`test`)).to.equal('bike');
+  });
+});
+
+describe('UuiToggle in a Form with no attributes', () => {
+  let formElement: HTMLFormElement;
+  let element: UUIToggleElement;
+  beforeEach(async () => {
+    formElement = await fixture(
+      html` <form>
+        <uui-toggle label="test label"></uui-toggle>
+      </form>`
+    );
+    element = formElement.querySelector('uui-toggle') as any;
+  });
+
+  it('element does not create form value if no name is provided', () => {
+    element.name = '';
+
+    element.checked = true;
+
+    const formData = new FormData(formElement);
+    const formDataKeys: Array<any> = [];
+    formData.forEach(key => {
+      formDataKeys.push(key);
+    });
+
+    expect(formDataKeys.length).to.equal(0);
   });
 });
