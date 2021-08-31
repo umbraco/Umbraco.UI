7364414fce3fb66a48052fea2c613d81774caeb2
Julia gru <julia.gru@gmail.com>
2021-08-31 17:41:34 +0200

    add tests for checkbox and toggle

--- src/components/uui-checkbox/uui-checkbox.test.ts
+++ src/components/uui-checkbox/uui-checkbox.test.ts
@@ -1,16 +1,34 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
 import { UUICheckboxElement } from './uui-checkbox.element';
 import '.';
 
 describe('UuiToggle', () => {
   let element: UUICheckboxElement;
+  let input: HTMLInputElement | null | undefined;
+  let iconCheck: HTMLElement;
+  let ticker: HTMLElement;
   beforeEach(async () => {
     element = await fixture(
       html` <uui-checkbox label="test label" name="test"></uui-checkbox> `
     );
+    input = element.shadowRoot?.querySelector('#input');
+    ticker = element.shadowRoot?.querySelector('#ticker') as HTMLElement;
+    iconCheck = element.shadowRoot?.querySelector('#icon-check') as HTMLElement;
   });
 
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
   });
+
+  it('native input has a correct role', async () => {
+    expect(input).to.have.attr('role', 'checkbox');
+  });
+
+  it('changes the opacity value of #icon-check element to 1 when element is checked', async () => {
+    element.checked = true;
+    await elementUpdated(element);
+    expect(window.getComputedStyle(iconCheck as Element).opacity).to.equal('1');
+  });
+
+  //test label text in the dom with slots way to
 });
--- src/components/uui-toggle/uui-toggle.test.ts
+++ src/components/uui-toggle/uui-toggle.test.ts
@@ -5,13 +5,19 @@ import { property } from 'lit/decorators';
 
 describe('UuiToggle', () => {
   let element: UUIToggleElement;
+  let input: HTMLInputElement | null | undefined;
   beforeEach(async () => {
     element = await fixture(
-      html` <uui-toggle label="test label"></uui-toggle> `
+      html` <uui-toggle label="test label" name="test"></uui-toggle> `
     );
+    input = element.shadowRoot?.querySelector('#input');
   });
 
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
+  });
+
+  it('native input has a correct role', async () => {
+    expect(input).to.have.attr('role', 'switch');
   });
 });
