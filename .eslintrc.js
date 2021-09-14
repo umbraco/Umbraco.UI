163e5febd9d3d5d2066d9c58cad0d42d60afe642
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-09-14 13:42:59 +0200

    linting v2

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,22 @@ module.exports = {
   plugins: ['@typescript-eslint', 'import', 'html'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    "@open-wc/eslint-config",
-    "eslint-config-prettier",
+    "plugin:lit/recommended",
+    "plugin:lit-a11y/recommended"
+    // 'plugin:import/errors',
+    // 'plugin:import/warnings',
+    // "@open-wc/eslint-config",
+    // "eslint-config-prettier",
     
   ],
   rules: {
     // disable the rule for all files
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'import/named': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'import/no-unresolved': 'off',
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // '@typescript-eslint/no-non-null-assertion': 'off',
+    // 'import/named': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    // 'import/no-unresolved': 'off',
   },
 };
