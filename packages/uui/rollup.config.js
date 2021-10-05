cdb146ea32c4089eedf1212a9229f50f02977542
Mads Rasmussen <madsr@hey.com>
2021-10-05 09:28:30 +0200

    add fake test

--- packages/uui/rollup.config.js
+++ packages/uui/rollup.config.js
@@ -1,6 +1,6 @@
 import { UUIProdConfig } from '../rollup-package.config';
 
 export default UUIProdConfig({
-  entryPoints: ['index'],
+  entryPoints: ['index', 'index.test'],
   bundles: ['index'],
 });
