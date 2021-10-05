8915bf50963300a839f62a24266c401304db3be5
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-10-05 21:59:48 +0200

    fix tag story

--- stories/uui-tag.story.ts
+++ stories/uui-tag.story.ts
@@ -28,16 +28,24 @@ export default {
   },
 };
 
-export const Overview: Story = props =>
-  html` <uui-tag .look=${props.look} .size=${props.size} .>Hello</uui-tag> `;
+const Template: Story = props =>
+  html` <uui-tag .look=${props.look} .size=${props.size}>Hello</uui-tag> `;
 
-export const Looks: Story = () =>
-  html`${InterfaceLookNames.map(
-    look => html`<uui-tag size="m" look="${look}">${look}</uui-tag>`
-  )} `;
+export const Overview = Template.bind({});
 
-export const Sizes: Story = () =>
+export const Looks: Story = props =>
   html`
+    <uui-tag .look=${props.look} .size=${props.size}>Hello</uui-tag>
+    <h5>Looks</h5>
+    ${InterfaceLookNames.map(
+      look => html`<uui-tag size="m" look="${look}">${look}</uui-tag>`
+    )}
+  `;
+
+export const Sizes: Story = props =>
+  html`
+    <uui-tag .look=${props.look} .size=${props.size}>Hello</uui-tag>
+    <h5>Sizes</h5>
     <uui-tag size="xs" look="primary">extra small</uui-tag>
     <uui-tag size="s" look="primary">small</uui-tag>
     <uui-tag size="m" look="primary">medium</uui-tag>
@@ -45,29 +53,28 @@ export const Sizes: Story = () =>
     <uui-tag size="xl" look="primary">extra large</uui-tag>
   `;
 
-const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M401.431 167.814l-58.757-58.76-88.029 88.026-88.028-88.026-58.76 58.76 88.026 88.027-88.026 88.024 58.76 58.768 88.028-88.031 88.029 88.031 58.757-58.768-88.027-88.024z"/></svg>`;
-
-export const WithButton: Story = () =>
-  html`
-    <uui-tag look="primary" size="xl">
-      <span>Hello</span>
-      <uui-button
-        label="delete this"
-        look="primary"
-        compact
-        style="margin-right:-10px; --uui-button-height:2em;">
-        <uui-icon svg=${svg}></uui-icon>
-      </uui-button>
-    </uui-tag>
-    <br />
-    <uui-tag look="danger" size="m">
-      <span>Hello</span>
-      <uui-button
-        label="delete this"
-        look="danger"
-        compact
-        style="margin-right:-10px; --uui-button-height:1em;">
-        <uui-icon svg=${svg}></uui-icon>
-      </uui-button>
-    </uui-tag>
-  `;
+// Would you ever put a button inside a tag?
+// export const WithButton: Story = () =>
+//   html`
+//     <uui-tag look="primary" size="xl">
+//       <span>Hello</span>
+//       <uui-button
+//         label="delete this"
+//         look="primary"
+//         compact
+//         style="margin-right:-10px; --uui-button-height:2em;">
+//         <uui-icon svg=${svg}></uui-icon>
+//       </uui-button>
+//     </uui-tag>
+//     <br />
+//     <uui-tag look="danger" size="m">
+//       <span>Hello</span>
+//       <uui-button
+//         label="delete this"
+//         look="danger"
+//         compact
+//         style="margin-right:-10px; --uui-button-height:1em;">
+//         <uui-icon svg=${svg}></uui-icon>
+//       </uui-button>
+//     </uui-tag>
+//   `;
