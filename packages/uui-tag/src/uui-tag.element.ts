59c16021798f1790d8a450696664113ca56a9924
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-10-06 10:15:03 +0200

    fixed tag and its stories

--- packages/uui-tag/src/uui-tag.element.ts
+++ packages/uui-tag/src/uui-tag.element.ts
@@ -12,7 +12,6 @@ import {
  *  @slot - for tag contents
  *  @description Tag component from Umbraco UI components library.
  *  @cssprop --uui-tag-font-size - Define the components font size.
- * @cssprop --uui-tag-size-unit - Base unit for components padding and mnargin
  */
 export class UUITagElement extends LitElement {
   static styles = [
@@ -25,13 +24,9 @@ export class UUITagElement extends LitElement {
 
         font-size: var(--uui-tag-font-size, 0.9em);
         margin: var(--uui-tag-margin, var(--uui-size-base-unit));
-        padding: var(--uui-tag-size-unit, var(--uui-size-base-unit))
-          calc(var(--uui-tag-size-unit, var(--uui-size-base-unit)) * 2);
 
-        --uui-button-border-radius: calc(
-          var(--uui-tag-size-unit, var(--uui-size-base-unit)) * 3
-        );
-        border-radius: var(--uui-button-border-radius);
+        padding: calc(var(--uui-tag-font-size) / 2.3) var(--uui-tag-font-size);
+        border-radius: 1000px;
         background-color: var(--uui-interface-surface-alt);
         color: var(--uui-interface-contrast);
         user-select: none;
@@ -45,22 +40,19 @@ export class UUITagElement extends LitElement {
       }
 
       :host([size='xs']) {
-        --uui-tag-size-unit: 4px;
         --uui-tag-font-size: 10px;
       }
       :host([size='s']) {
-        --uui-tag-size-unit: 4px;
         --uui-tag-font-size: 11px;
       }
       :host([size='m']) {
-        --uui-tag-size-unit: 5px;
         --uui-tag-font-size: 12px;
       }
       :host([size='l']) {
-        --uui-tag-size-unit: 6px;
+        --uui-tag-font-size: 13px;
       }
       :host([size='xl']) {
-        --uui-tag-size-unit: 8px;
+        --uui-tag-font-size: 14px;
       }
     `,
     InterfaceLookCSSCreator(
