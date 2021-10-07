9d506a8093d1f8db4c2c68d8af8a8d0224617b38
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-10-07 13:23:42 +0200

    work on uui-toggle

--- stories/uui-toggle.story.ts
+++ stories/uui-toggle.story.ts
@@ -5,15 +5,50 @@ import '@umbraco-ui/uui-toggle/lib/index';
 export default {
   title: 'Inputs/Toggle',
   component: 'uui-toggle',
-  argTypes: {
-    labelPosition: { options: ['left', 'right', 'top', 'bottom'] },
+  args: {
+    value: 'toggle',
+    name: 'toggle',
+    error: false,
+    label: 'label',
+    labelPosition: 'right',
+    disabled: false,
+    checked: false,
+    'hide-label': false,
+    '--uui-toggle-size': '18px',
+    '--uui-toggle-switch-width': '36px',
+  },
+  parameters: {
+    controls: {
+      exclude: ['formAssociated', 'change-'],
+    },
   },
 };
 
 export const Overview: Story = props =>
-  html` <uui-toggle .value=${props.value}></uui-toggle> `;
+  html`
+    <uui-toggle
+      style="--uui-toggle-size: ${props[
+        '--uui-toggle-size'
+      ]}; --uui-toggle-switch-width: ${props['--uui-toggle-switch-width']};"
+      .value=${props.value}
+      .name=${props.name}
+      .error=${props.error}
+      .label=${props.label}
+      .labelPosition=${props.labelPosition}
+      ?disabled=${props.disabled}
+      ?checked=${props.checked}
+      ?hide-label=${props['hide-label']}
+      >${props.slot}</uui-toggle
+    >
+  `;
 Overview.storyName = 'Overview';
 Overview.args = { label: 'I am a label' };
+Overview.argTypes = {
+  labelPosition: { options: ['left', 'right', 'top', 'bottom'] },
+  slot: { control: { type: 'text' } },
+  '--uui-toggle-size': { control: { type: 'text' } },
+  '--uui-toggle-switch-width': { control: { type: 'text' } },
+};
 
 export const Error: Story = () =>
   html`
@@ -23,18 +58,19 @@ export const Error: Story = () =>
     <uui-toggle disabled checked error label="Disabled"></uui-toggle>
   `;
 
-export const Preselected: Story = () =>
-  html` <uui-toggle label="Toggle me" value="bike" checked></uui-toggle>`;
-
-export const WithSlottedLabel: Story = () =>
+export const WithSlottedLabel: Story = props =>
   html`
     <uui-toggle label="Toggle label" value="bike"
       >Using <b>Slot</b> for displayed label</uui-toggle
     >
   `;
+WithSlottedLabel.parameters = {
+  controls: { include: [] },
+};
 
 export const LabelPosition: Story = () => html`
-  <div style="display: flex; justify-content: space-evenly;">
+  <div
+    style="display: grid; grid-template-columns: repeat(4, 128px); align-items: center; justify-items: center">
     <uui-toggle label="Left" label-position="left"></uui-toggle>
     <uui-toggle label="Top" label-position="top"></uui-toggle>
     <uui-toggle label="Right" label-position="right"></uui-toggle>
@@ -44,25 +80,11 @@ export const LabelPosition: Story = () => html`
 
 export const NoLabel: Story = () =>
   html`<uui-toggle
-      hide-label
-      label="Toggle label"
-      name="Hidden Label"></uui-toggle
-    ><uui-toggle
-      hide-label
-      label="Toggle label"
-      name="Hidden Label 2"></uui-toggle>
-    <p>
-      label is set via label attribute. Hide it by adding hide-label bool
-      attribute
-    </p>`;
+    hide-label
+    label="Toggle label"
+    name="Hidden Label"></uui-toggle>`;
 
 export const Disabled: Story = () => html`
   <uui-toggle disabled label="Disabled"></uui-toggle>
   <uui-toggle disabled label="Disabled &amp; checked" checked></uui-toggle>
-`;
-
-export const InAForm: Story = () => html`
-  <form action="">
-    <uui-toggle label="Lol"></uui-toggle>
-  </form>
 `;
