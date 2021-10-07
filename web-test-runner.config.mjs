199ff449cf7a014e9b6ec0b0f616b14db5a38bb0
Julia gru <julia.gru@gmail.com>
2021-10-07 13:45:45 +0200

    tests watch mode add config plugin

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { esbuildPlugin } from '@web/dev-server-esbuild';
 
 const packages = fs
   .readdirSync('packages')
@@ -11,4 +12,5 @@ export default {
     files: `packages/${pkg}/**/*.test.js`,
     rootDir: './src'
   })),
+  plugins: [esbuildPlugin({ ts: true, target: 'auto' })],
 };
