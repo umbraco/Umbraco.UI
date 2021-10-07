a01db026e307dc2b98dde3b8d28384bef056faf2
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-10-07 13:53:10 +0200

    fixed label stories

--- stories/uui-toggle.story.ts
+++ stories/uui-toggle.story.ts
@@ -50,41 +50,89 @@ Overview.argTypes = {
   '--uui-toggle-switch-width': { control: { type: 'text' } },
 };
 
-export const Error: Story = () =>
+export const Error: Story = props =>
   html`
-    <uui-toggle error .label=${'Toggle label'} value="bike"></uui-toggle><br />
-    <uui-toggle error label="Toggle me" value="bike" checked></uui-toggle
-    ><br /><uui-toggle disabled error label="Disabled"></uui-toggle><br />
-    <uui-toggle disabled checked error label="Disabled"></uui-toggle>
+    <uui-toggle
+      ?error=${props.error}
+      .label=${'Toggle label'}
+      value="toggle"></uui-toggle
+    ><br />
+    <uui-toggle
+      ?error=${props.error}
+      label="Toggle me"
+      value="toggle"
+      checked></uui-toggle
+    ><br /><uui-toggle
+      disabled
+      ?error=${props.error}
+      label="Disabled"></uui-toggle
+    ><br />
+    <uui-toggle
+      disabled
+      checked
+      ?error=${props.error}
+      label="Disabled"></uui-toggle>
   `;
+Error.args = { error: true };
+Error.parameters = {
+  controls: { include: ['error'] },
+};
 
 export const WithSlottedLabel: Story = props =>
   html`
-    <uui-toggle label="Toggle label" value="bike"
+    <uui-toggle label="Toggle label" ?checked=${props.checked} value="toggle"
       >Using <b>Slot</b> for displayed label</uui-toggle
     >
   `;
 WithSlottedLabel.parameters = {
-  controls: { include: [] },
+  controls: { include: ['checked'] },
 };
 
-export const LabelPosition: Story = () => html`
+export const LabelPosition: Story = props => html`
   <div
     style="display: grid; grid-template-columns: repeat(4, 128px); align-items: center; justify-items: center">
-    <uui-toggle label="Left" label-position="left"></uui-toggle>
-    <uui-toggle label="Top" label-position="top"></uui-toggle>
-    <uui-toggle label="Right" label-position="right"></uui-toggle>
-    <uui-toggle label="Bottom" label-position="bottom"></uui-toggle>
+    <uui-toggle
+      ?checked=${props.checked}
+      label="Left"
+      label-position="left"></uui-toggle>
+    <uui-toggle
+      ?checked=${props.checked}
+      label="Top"
+      label-position="top"></uui-toggle>
+    <uui-toggle
+      ?checked=${props.checked}
+      label="Right"
+      label-position="right"></uui-toggle>
+    <uui-toggle
+      ?checked=${props.checked}
+      label="Bottom"
+      label-position="bottom"></uui-toggle>
   </div>
 `;
+LabelPosition.parameters = {
+  controls: { include: ['checked'] },
+};
 
-export const NoLabel: Story = () =>
-  html`<uui-toggle
-    hide-label
-    label="Toggle label"
-    name="Hidden Label"></uui-toggle>`;
+export const NoLabel: Story = props =>
+  html`
+    <uui-toggle
+      label="Toggle label"
+      ?checked=${props.checked}
+      ?hide-label=${props['hide-label']}
+      value="toggle"></uui-toggle>
+  `;
+NoLabel.parameters = {
+  controls: { include: ['checked', 'hide-label'] },
+};
 
-export const Disabled: Story = () => html`
-  <uui-toggle disabled label="Disabled"></uui-toggle>
-  <uui-toggle disabled label="Disabled &amp; checked" checked></uui-toggle>
+export const Disabled: Story = props => html`
+  <uui-toggle ?disabled=${props.disabled} label="Disabled"></uui-toggle>
+  <uui-toggle
+    ?disabled=${props.disabled}
+    label="Disabled &amp; checked"
+    checked></uui-toggle>
 `;
+Disabled.args = { disabled: true };
+Disabled.parameters = {
+  controls: { include: ['disabled'] },
+};
