3b826bc048ee230ef8302ac5495cb50fa04f10de
Mads Rasmussen <madsr@hey.com>
2021-10-14 13:12:00 +0200

    update path to storybook css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { setCustomElements } from '@storybook/web-components';
 import customElements from '../custom-elements.json';
-import '../packages/uui-base/src/styles/index.css';
+import '../packages/uui-base/lib/styles/index.css';
 
 const sort = (a, b) => {
   if (a[1].name === 'Overview') {
