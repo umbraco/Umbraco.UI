717963fab11a757be3e489fe0de54cf3ada25e14
Mads Rasmussen <madsr@hey.com>
2021-10-18 10:01:45 +0200

    add simple story + code examples

--- packages/uui-keyboard-shortcut/lib/uui-keyboard-shortcut.story.ts
+++ packages/uui-keyboard-shortcut/lib/uui-keyboard-shortcut.story.ts
@@ -7,7 +7,20 @@ export default {
   id: 'uui-keyboard-shortcut',
 };
 
-export const Overview = () => html`
+export const AAAOverview = () => html`
+  <uui-key>Enter</uui-key>
+`;
+
+AAAOverview.storyName = 'Overview';
+AAAOverview.parameters = {
+  docs: {
+    source: {
+      code: `<uui-key>Enter</uui-key>`,
+    },
+  },
+};
+
+export const KeyCombination = () => html`
   <uui-keyboard-shortcut>
     <uui-key>ALT</uui-key>
     +
@@ -18,3 +31,20 @@ export const Overview = () => html`
     <uui-key>z</uui-key>
   </uui-keyboard-shortcut>
 `;
+
+KeyCombination.parameters = {
+  docs: {
+    source: {
+      code: `  
+<uui-keyboard-shortcut>
+  <uui-key>ALT</uui-key>
+  +
+  <uui-key>shift</uui-key>
+  +
+  <uui-key>&#8593;</uui-key>
+  +
+  <uui-key>z</uui-key>
+</uui-keyboard-shortcut>`,
+    },
+  },
+};
