d95b57454e7c59a019a80884c9334d9477e09460
Julia gru <julia.gru@gmail.com>
2021-10-25 12:47:32 +0200

    docs: :memo: show unnamed slots in the storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -33,11 +33,6 @@ export const parameters = {
         disable: true,
       },
     },
-    '': {
-      table: {
-        disable: true,
-      },
-    },
     formAssociated: {
       table: {
         disable: true,
