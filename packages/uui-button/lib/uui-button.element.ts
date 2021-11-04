3245743e9d7a751febd926da262449396761e006
Julia Gru <56249914+julczka@users.noreply.github.com>
2021-11-04 10:38:19 +0100

    Update uui-button.element.ts

--- packages/uui-button/lib/uui-button.element.ts
+++ packages/uui-button/lib/uui-button.element.ts
@@ -402,6 +402,11 @@ export class UUIButtonElement extends LabelMixin('label', LitElement) {
   @property({ type: String, reflect: true })
   state: UUIButtonState = null;
 
+  
+    /**
+   * This is a static class field indicating that the element is can be used inside a native form and participate in its events. It may require a polyfill, check support here https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/attachInternals.  Read more about form controls here https://web.dev/more-capable-form-controls/
+   * @type {boolean}
+   */
   static readonly formAssociated = true;
 
   private _internals;
