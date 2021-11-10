d168b692540c69d354a00152c26e6e98b2e33d0f
Mads Rasmussen <madsr@hey.com>
2021-11-10 09:51:47 +0100

    add mixin for form controls

---
+++ packages/uui-base/lib/mixins/FormControlMixin.ts
@@ -0,0 +1,90 @@
+import { LitElement } from 'lit';
+import { property, state } from 'lit/decorators.js';
+
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+export declare class FormControlMixinInterface {
+  formAssociated: boolean;
+  get value(): string;
+  set value(a:string);
+  name: string;
+  disabled: boolean;
+  _value: string;
+  _internals: any;
+}
+
+/**
+ * The mixin allows a custom element to participate in HTML forms.
+ *
+ * @param {Object} superClass - superclass to be extended.
+ * @mixin
+ */
+export const FormControlMixin = <T extends Constructor<LitElement>>(
+  superClass: T
+) => {
+  class FormControlMixinClass extends superClass {
+
+    /**
+     * This is a static class field indicating that the element is can be used inside a native form and participate in its events. 
+     * It may require a polyfill, check support here https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/attachInternals.
+     * Read more about form controls here https://web.dev/more-capable-form-controls/
+     * @type {boolean}
+     */
+    static readonly formAssociated = true;
+
+    /**
+     * This is a name property of the component.
+     * @type {string}
+     * @attr
+     * @default ''
+     */
+    @property({ type: String })
+    name = '';
+
+    /**
+     * This is a value property
+     * @type {string}
+     * @attr
+     * @default ''
+     */
+    @property()
+    get value() {
+      return this._value;
+    }
+    set value(newValue) {
+      const oldValue = this._value;
+      this._value = newValue;
+      this._internals.setFormValue(this._value);
+      this.requestUpdate('value', oldValue);
+    }
+
+    /**
+     * Disables the input.
+     * @type {boolean}
+     * @attr
+     * @default false
+     */
+    @property({ type: Boolean, reflect: true })
+    disabled = false;
+
+    @state()
+    private _value = '';
+
+    private _internals: any;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this._internals = (this as any).attachInternals();
+    }
+
+    public formResetCallback() {
+      this.value = this.getAttribute('value') || '';
+    }
+    
+    public checkValidity () {
+      return this._internals?.checkValidity();
+    }
+
+  }
+  return FormControlMixinClass as unknown as Constructor<FormControlMixinInterface> & T;
+};
