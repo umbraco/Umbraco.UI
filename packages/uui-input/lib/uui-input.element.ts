8c324c8366e1c875494431b71010f76bf17be288
Mads Rasmussen <madsr@hey.com>
2021-11-10 10:11:08 +0100

    implement FormControlMixin in uui-input

--- packages/uui-input/lib/uui-input.element.ts
+++ packages/uui-input/lib/uui-input.element.ts
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
-import { property, state, query } from 'lit/decorators.js';
-import { LabelMixin } from '@umbraco-ui/uui-base/lib/mixins';
+import { property, query } from 'lit/decorators.js';
+import { FormControlMixin, LabelMixin } from '@umbraco-ui/uui-base/lib/mixins';
 import { UUIInputEvent } from './UUIInputEvent';
 
 export type InputType =
@@ -26,7 +26,7 @@ export type InputType =
  * @fires InputEvent#input on input
  * @fires KeyboardEvent#keyup on keyup
  */
-export class UUIInputElement extends LabelMixin('input label', LitElement) {
+export class UUIInputElement extends FormControlMixin(LabelMixin('input label', LitElement)) {
   static styles = [
     css`
       :host {
@@ -105,32 +105,14 @@ export class UUIInputElement extends LabelMixin('input label', LitElement) {
         font-weight: bold;
       }
 
-      :host(:invalid) input {
+      :host(:invalid) input,
+      :host([internals-invalid]) input {
         border: 1px solid var(--uui-look-danger-border);
       }
-
-      /*
-      :host([error]) input {
-        border: 1px solid var(--uui-look-danger-border);
-      }
-      */
     `,
   ];
 
-  /**
-   * This is a static class field indicating that the element is can be used inside a native form and participate in its events. It may require a polyfill, check support here https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/attachInternals.  Read more about form controls here https://web.dev/more-capable-form-controls/
-   * @type {boolean}
-   */
-  static readonly formAssociated = true;
-
-  private _internals;
-  private _error: Boolean = false;
   private _validityState: any = {};
-
-  constructor() {
-    super();
-    this._internals = (this as any).attachInternals();
-  }
 
   /**
    * Defines the input placeholder.
@@ -142,15 +124,6 @@ export class UUIInputElement extends LabelMixin('input label', LitElement) {
   placeholder = '';
 
   /**
-   * Disables the input.
-   * @type {boolean}
-   * @attr
-   * @default false
-   */
-  @property({ type: Boolean, reflect: true })
-  disabled = false;
-
-  /**
    * Set to true to hide the labeling provided by the component.
    * @type {boolean}
    * @attr hide-label
@@ -158,51 +131,6 @@ export class UUIInputElement extends LabelMixin('input label', LitElement) {
    */
   @property({ type: Boolean, attribute: 'hide-label', reflect: true })
   hideLabel = false;
-
-  @state()
-  private _value = '';
-
-  /**
-   * This is a value property of the uui-input.
-   * @type {string}
-   * @attr
-   * @default ''
-   */
-  @property()
-  get value() {
-    return this._value;
-  }
-  set value(newValue) {
-    const oldValue = this._value;
-    this._value = newValue;
-    this._internals.setFormValue(this._value);
-    this.requestUpdate('value', oldValue);
-  }
-
-  /**
-   * This is a value property of the uui-input.
-   * @type {boolean}
-   * @attr
-   * @default ''
-   */
-  @property({ type: Boolean, reflect: true })
-  get error() {
-    return this._error;
-  }
-  set error(newValue) {
-    const oldValue = this._error;
-    this._error = newValue;
-    this.requestUpdate('error', oldValue);
-  }
-
-  /**
-   * This is a name property of the `<uui-input>` component. It reflects the behaviour of the native `<input />` element and its name attribute.
-   * @type {string}
-   * @attr
-   * @default ''
-   */
-  @property({ type: String })
-  name = '';
 
   /**
    * This property specifies the type of input that will be rendered.
@@ -213,12 +141,12 @@ export class UUIInputElement extends LabelMixin('input label', LitElement) {
   @property({ type: String })
   type: InputType = 'text';
 
+  @query('input')
+  private _input?: HTMLInputElement;
+
   private onInput(e: Event) {
     this.value = (e.target as HTMLInputElement).value;
   }
-
-  @query('input')
-  private _input?: HTMLInputElement;
 
   private onChange() {
     this.dispatchEvent(
@@ -230,9 +158,28 @@ export class UUIInputElement extends LabelMixin('input label', LitElement) {
     this._setValidity();
   }
 
-  private _setValidity () {
+  // Validation
+  /**
+   * This is a value property of the uui-input.
+   * @type {boolean}
+   * @attr
+   * @default false
+   */
+  @property({ type: Boolean, reflect: true })
+  required = false;
 
-    if (this.hasAttribute('required') && this.value === '') {
+  /**
+   * This is a value property of the uui-input.
+   * @type {boolean}
+   * @attr
+   * @default false
+   */
+  @property({ type: Boolean, reflect: true })
+  error = false;
+
+  private _setValidity () {
+    // check for required
+    if (this.required && this.value === '') {
       this._validityState.valueMissing = true;
       this._internals.setValidity(this._validityState, 'The field is required', this._input);
     }
@@ -240,7 +187,8 @@ export class UUIInputElement extends LabelMixin('input label', LitElement) {
       this._validityState.valueMissing = false;
     }
 
-    if (this._error) {
+    // check for custom error
+    if (this.error) {
       this._validityState.customError = true;
       this._internals.setValidity(this._validityState, 'The field is invalid', this._input);
     }
@@ -248,29 +196,11 @@ export class UUIInputElement extends LabelMixin('input label', LitElement) {
       this._validityState.customError = false;
     }
 
-
-    let hasError = false;
-
-    for (const [key, value] of Object.entries(this._validityState)) {
-      if (value === true) {
-        hasError = true;
-      }
-    }
+    const hasError = Object.values(this._validityState).includes(true);
 
     if (hasError === false) {
       this._internals.setValidity({});
     }
-
-    console.log('Has error', hasError);
-  }
-  
-  // FORM CONTROLS
-  public formResetCallback() {
-    this.value = this.getAttribute('value') || '';
-  }
-  
-  public checkValidity () {
-    return this._internals?.checkValidity();
   }
 
   render() {
