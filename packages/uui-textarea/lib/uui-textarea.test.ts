2f76d338d7c06b4e40a3bec6a2d23c62be0a2b40
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-11-10 16:27:03 +0100

    added tests and updated package json descirption

--- packages/uui-textarea/lib/uui-textarea.test.ts
+++ packages/uui-textarea/lib/uui-textarea.test.ts
@@ -1,4 +1,4 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
 import { UUITextareaElement } from './uui-textarea.element';
 import '.';
 
@@ -6,10 +6,92 @@ describe('UUITextareaElement', () => {
   let element: UUITextareaElement;
 
   beforeEach(async () => {
-    element = await fixture(html` <uui-textarea></uui-textarea> `);
+    element = await fixture(
+      html` <uui-textarea label="textarea"></uui-textarea> `
+    );
   });
 
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UUITextareaElement', () => {
+  let element: UUITextareaElement;
+  let textarea: HTMLTextAreaElement;
+  beforeEach(async () => {
+    element = await fixture(
+      html` <uui-textarea label="a textarea label"></uui-textarea> `
+    );
+    textarea = element.shadowRoot?.querySelector(
+      'textarea'
+    ) as HTMLTextAreaElement;
+  });
+  it('test that disable works', async () => {
+    element.disabled = true;
+    await elementUpdated(element);
+    expect(textarea.disabled).to.be.true;
+  });
+
+  it('changes the value to the textarea value when textarea event is emitted', async () => {
+    textarea.value = 'test value';
+    textarea.dispatchEvent(
+      new Event('input', { bubbles: true, composed: true })
+    );
+    expect(element.value).to.equal('test value');
+  });
+
+  it('emits a change event when native textarea fires one', async () => {
+    let event: Event | null = null;
+    element.addEventListener('change', e => (event = e));
+    textarea.dispatchEvent(
+      new Event('change', { bubbles: true, composed: false })
+    );
+    expect(event!.target).to.equal(element);
+  });
+});
+
+describe('UuiTextarea with label', () => {
+  let dom: Element;
+  beforeEach(async () => {
+    dom = await fixture(html`
+      <label for="test">Textarea</label>
+      <uui-textarea id="test" label="a textarea label"></uui-textarea>
+    `);
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(dom).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UuiTextarea in Form', () => {
+  let formElement: HTMLFormElement;
+  let element: UUITextareaElement;
+  beforeEach(async () => {
+    formElement = await fixture(
+      html` <form>
+        <uui-textarea
+          label="a textarea label"
+          name="textarea"
+          value="Hello uui-textarea"></uui-textarea>
+      </form>`
+    );
+    element = formElement.querySelector('uui-textarea') as any;
+  });
+
+  it('value is correct', async () => {
+    await expect(element.value).to.be.equal('Hello uui-textarea');
+  });
+
+  it('form output', async () => {
+    const formData = new FormData(formElement);
+    await expect(formData.get('textarea')).to.be.equal('Hello uui-textarea');
+  });
+
+  it('change value and check output', async () => {
+    element.value = 'anotherValue';
+    const formData = new FormData(formElement);
+    await expect(formData.get('textarea')).to.be.equal('anotherValue');
   });
 });
