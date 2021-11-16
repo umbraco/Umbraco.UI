cef8467429a25e7af5c7aadc692d7f30567d8500
Niels Lyngsø <nsl@umbraco.com>
2021-11-16 12:46:03 +0100

    uui-icon converted into a seperate package

---
+++ packages/uui-icon/lib/uui-icon.test.ts
@@ -0,0 +1,17 @@
+import { html, fixture, expect } from '@open-wc/testing';
+import { UUIIconElement } from './uui-icon.element';
+import '.';
+
+describe('UUIIconElement', () => {
+  let element: UUIIconElement;
+
+  beforeEach(async () => {
+    element = await fixture(
+      html` <uui-icon></uui-icon> `
+    );
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
