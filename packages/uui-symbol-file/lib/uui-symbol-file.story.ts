52b0a361d69a1d298909da2f9a833bd638941541
Niels Lyngsø <nsl@umbraco.com>
2021-11-22 14:33:35 +0100

    fix stories

--- packages/uui-symbol-file/lib/uui-symbol-file.story.ts
+++ packages/uui-symbol-file/lib/uui-symbol-file.story.ts
@@ -1,21 +1,21 @@
 import { html } from 'lit-html';
-import '@umbraco-ui/uui-file-symbol/lib/index';
+import '@umbraco-ui/uui-symbol-file/lib/index';
 import { Story } from '@storybook/web-components';
 export default {
-  title: 'Symbols/File Symbol',
-  component: 'uui-file-symbol',
-  id: 'uui-file-symbol',
+  title: 'Symbols/File',
+  component: 'uui-symbol-file',
+  id: 'uui-symbol-file',
 };
 
 export const Overview: Story = props =>
   html`<div style="width: 240px">
-    <uui-file-symbol .type=${props.type}></uui-file-symbol>
+    <uui-symbol-file .type=${props.type}></uui-symbol-file>
   </div>`;
 
 Overview.parameters = {
   docs: {
     source: {
-      code: '<uui-file-symbol type="pdf"></uui-file-symbol>',
+      code: '<uui-symbol-file type="pdf"></uui-symbol-file>',
     },
   },
 };
