3c340bb42e410c681ed1ae567d3d1543cad5b82f
Niels Lyngsø <nsl@umbraco.com>
2021-11-23 13:44:02 +0100

    corrected paddings

--- packages/uui-action-bar/lib/uui-action-bar.element.ts
+++ packages/uui-action-bar/lib/uui-action-bar.element.ts
@@ -10,17 +10,17 @@ export class UUIActionBarElement extends UUIButtonGroupElement {
     ...UUIButtonGroupElement.styles,
     css`
       ::slotted(*) {
-        --uui-button-slot-padding-r-factor: 0.333;
-        --uui-button-slot-padding-l-factor: 0.333;
+        --uui-button-slot-padding-r-factor: 0.5;
+        --uui-button-slot-padding-l-factor: 0.5;
       }
 
       ::slotted(uui-button:first-child) {
         --uui-button-border-radius: 50px 0 0 50px;
-        --uui-button-slot-padding-l-factor: 1.666;
+        --uui-button-slot-padding-l-factor: 1.5;
       }
       ::slotted(uui-button:last-child) {
         --uui-button-border-radius: 0 50px 50px 0;
-        --uui-button-slot-padding-r-factor: 1.666;
+        --uui-button-slot-padding-r-factor: 1.5;
       }
       ::slotted(uui-button:first-child:last-child) {
         --uui-button-border-radius: 50px 50px 50px 50px;
@@ -30,25 +30,25 @@ export class UUIActionBarElement extends UUIButtonGroupElement {
 
       ::slotted([look='outline']),
       ::slotted([look='placeholder']) {
-        --uui-button-slot-padding-r-factor: 0.666;
-        --uui-button-slot-padding-l-factor: 0.666;
+        --uui-button-slot-padding-r-factor: 1;
+        --uui-button-slot-padding-l-factor: 1;
       }
 
       ::slotted(uui-button[look='outline']:first-child),
       ::slotted(uui-button[look='placeholder']:first-child) {
         --uui-button-border-radius: 50px 0 0 50px;
-        --uui-button-slot-padding-l-factor: 0.8;
+        --uui-button-slot-padding-l-factor: 1.5;
       }
       ::slotted(uui-button[look='outline']:last-child),
       ::slotted(uui-button[look='placeholder']:last-child) {
         --uui-button-border-radius: 0 50px 50px 0;
-        --uui-button-slot-padding-r-factor: 0.8;
+        --uui-button-slot-padding-r-factor: 1.5;
       }
       ::slotted(uui-button[look='outline']:first-child:last-child),
       ::slotted(uui-button[look='placeholder']:first-child:last-child) {
         --uui-button-border-radius: 50px 50px 50px 50px;
-        --uui-button-slot-padding-l-factor: 0.8;
-        --uui-button-slot-padding-r-factor: 0.8;
+        --uui-button-slot-padding-l-factor: 1.5;
+        --uui-button-slot-padding-r-factor: 1.5;
       }
     `,
   ];
