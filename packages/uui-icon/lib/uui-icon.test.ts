4c255894245681158c98fdd1049dcf70272d0e7e
Niels Lyngsø <nsl@umbraco.com>
2021-11-23 14:18:20 +0100

    more uui-icon tests

--- packages/uui-icon/lib/uui-icon.test.ts
+++ packages/uui-icon/lib/uui-icon.test.ts
@@ -4,14 +4,49 @@ import '.';
 
 import { iconRegistry } from './UUIIconRegistry';
 
-describe('UUIIconElement', () => {
-  const TEST_SVG =
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"></svg>';
+const TEST_SVG =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"></svg>';
+iconRegistry.defineIcon('test', TEST_SVG);
+
+describe('UUIIconElement with name', () => {
   let element: UUIIconElement;
 
   beforeEach(async () => {
-    iconRegistry.defineIcon('test', TEST_SVG);
     element = await fixture(html` <uui-icon name="test"></uui-icon> `);
+  });
+
+  it('contains svg of icon', async () => {
+    await expect(element).shadowDom.to.equal(TEST_SVG);
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UUIIconElement with svg', () => {
+  let element: UUIIconElement;
+
+  beforeEach(async () => {
+    element = await fixture(html` <uui-icon svg=${TEST_SVG}></uui-icon> `);
+  });
+
+  it('contains svg of icon', async () => {
+    await expect(element).shadowDom.to.equal(TEST_SVG);
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UUIIconElement with fallback', () => {
+  let element: UUIIconElement;
+
+  beforeEach(async () => {
+    element = await fixture(
+      html` <uui-icon name="not_existing" fallback=${TEST_SVG}></uui-icon> `
+    );
   });
 
   it('contains svg of icon', async () => {
