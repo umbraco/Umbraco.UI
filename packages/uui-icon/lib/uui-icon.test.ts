6c335b158c118bbefc75b561b666dfe86d29d273
Niels Lyngsø <nsl@umbraco.com>
2021-11-23 14:14:56 +0100

    test svg is there

--- packages/uui-icon/lib/uui-icon.test.ts
+++ packages/uui-icon/lib/uui-icon.test.ts
@@ -2,11 +2,20 @@ import { html, fixture, expect } from '@open-wc/testing';
 import { UUIIconElement } from './uui-icon.element';
 import '.';
 
+import { iconRegistry } from './UUIIconRegistry';
+
 describe('UUIIconElement', () => {
+  const TEST_SVG =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"></svg>';
   let element: UUIIconElement;
 
   beforeEach(async () => {
-    element = await fixture(html` <uui-icon></uui-icon> `);
+    iconRegistry.defineIcon('test', TEST_SVG);
+    element = await fixture(html` <uui-icon name="test"></uui-icon> `);
+  });
+
+  it('contains svg of icon', async () => {
+    await expect(element).shadowDom.to.equal(TEST_SVG);
   });
 
   it('passes the a11y audit', async () => {
