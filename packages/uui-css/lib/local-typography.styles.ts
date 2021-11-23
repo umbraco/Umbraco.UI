0c432c7c616a682ab789ed2a22832ccfcab226e0
Niels Lyngsø <nsl@umbraco.com>
2021-11-23 21:44:15 +0100

    testing linter

--- packages/uui-css/lib/local-typography.styles.ts
+++ packages/uui-css/lib/local-typography.styles.ts
@@ -1,6 +1,5 @@
 import { unsafeCSS } from 'lit';
 // @ts-ignore-start
-// @typescript-eslint/ban-ts-comment // @ts-ignore
 import css from '../dist/local-typography.css'; // eslint-disable-line no-use-before-define
 // @ts-ignore-end
 
