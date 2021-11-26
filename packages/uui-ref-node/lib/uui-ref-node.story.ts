3a405a9ac06abcb1f164d50d1f21c80ada927103
Mads Rasmussen <madsr@hey.com>
2021-11-26 16:47:19 +0100

    uui-ref-node add docs to stories

--- packages/uui-ref-node/lib/uui-ref-node.story.ts
+++ packages/uui-ref-node/lib/uui-ref-node.story.ts
@@ -7,108 +7,168 @@ export default {
   id: 'uui-ref-node',
   title: 'Displays/References/Node',
   component: 'uui-ref-node',
-  parameters: {
-    docs: {
-      source: {
-        code: `<uui-ref-node></uui-ref-node>`,
-      },
+};
+
+const Template: Story = props => html`
+  <div style="max-width: 420px;">
+    <uui-ref-node
+      name="${props.name}"
+      icon="${props.icon}"
+      detail="${props.detail}"
+      ?selectable=${props.selectable}
+      ?error=${props.error}
+      ?disabled=${props.disabled}>
+      <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+      <uui-action-bar slot="actions"
+        ><uui-button><uui-icon name="delete"></uui-icon></uui-button
+      ></uui-action-bar>
+    </uui-ref-node>
+  </div>
+`;
+
+export const AAAOverview = Template.bind({});
+AAAOverview.args = {
+  name: 'Rabbit Suit Product Page',
+  icon: 'bug',
+  detail: 'path/to/nowhere',
+};
+AAAOverview.storyName = 'Overview';
+AAAOverview.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node
+  name="Rabbit Suit Product Page"
+  icon="bug"
+  detail="path/to/nowhere">
+  <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+  <uui-action-bar slot="actions"
+    ><uui-button><uui-icon name="delete"></uui-icon></uui-button
+  ></uui-action-bar>
+</uui-ref-node>
+    `,
     },
   },
 };
 
-export const Overview: Story = () => html`
-  <div style="max-width: 420px;">
-    <uui-ref-node
-      name="Rabbit suit product page"
-      icon="bug"
-      detail="path/to/nowhere">
-      <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
-      <uui-action-bar slot="actions"
-        ><uui-button><uui-icon name="delete"></uui-icon></uui-button
-      ></uui-action-bar>
-    </uui-ref-node>
-  </div>
-`;
-
-export const Border = () => html`
-  <div style="max-width: 420px;">
-    <uui-ref-node
-      name="Rabbit suit product page"
-      icon="bug"
-      detail="path/to/nowhere"
-      border>
-      <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
-      <uui-action-bar slot="actions"
-        ><uui-button><uui-icon name="delete"></uui-icon></uui-button
-      ></uui-action-bar>
-    </uui-ref-node>
-  </div>
-`;
-
-export const Selectable = () => html`
-  <div style="max-width: 420px;">
-    <uui-ref-node
-      selectable
-      name="Rabbit suit product page"
-      icon="bug"
-      detail="path/to/nowhere">
-      <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
-      <uui-action-bar slot="actions"
-        ><uui-button><uui-icon name="delete"></uui-icon></uui-button
-      ></uui-action-bar>
-    </uui-ref-node>
-  </div>
-`;
-
-export const BorderAndSelectable = () => html`
-  <div style="max-width: 420px;">
-    <uui-ref-node
-      selectable
-      border
-      name="Rabbit suit product page"
-      icon="bug"
-      detail="path/to/nowhere">
-      <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
-      <uui-action-bar slot="actions"
-        ><uui-button><uui-icon name="delete"></uui-icon></uui-button
-      ></uui-action-bar>
-    </uui-ref-node>
-  </div>
-`;
-
-export const Disabled = () => html`
-  <div style="max-width: 420px;">
-    <uui-ref-node
-      disabled
-      name="Rabbit suit product page"
-      icon="bug"
-      detail="path/to/nowhere">
-      <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
-      <uui-action-bar slot="actions"
-        ><uui-button><uui-icon name="delete"></uui-icon></uui-button
-      ></uui-action-bar>
-    </uui-ref-node>
-  </div>
-`;
-
-export const BorderAndDisabled = () => html`
+export const Border: Story = () => html`
   <div style="max-width: 420px;">
     <uui-ref-node
       border
-      disabled
-      name="Rabbit suit product page"
+      name="Rabbit Suit Product Page"
       icon="bug"
       detail="path/to/nowhere">
       <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
-      <uui-action-bar slot="actions"
-        ><uui-button><uui-icon name="delete"></uui-icon></uui-button
-      ></uui-action-bar>
+      <uui-action-bar slot="actions">
+        <uui-button type="button" label="Delete"
+          ><uui-icon name="delete"></uui-icon
+        ></uui-button>
+      </uui-action-bar>
     </uui-ref-node>
   </div>
 `;
+
+Border.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node
+  border
+  name="Rabbit Suit Product Page"
+  icon="bug"
+  detail="path/to/nowhere">
+  <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+  <uui-action-bar slot="actions">
+    <uui-button type="button" label="Delete"><uui-icon name="delete"></uui-icon></uui-button>
+  </uui-action-bar>
+</uui-ref-node>
+    `,
+    },
+  },
+};
+
+export const Selectable: Story = props => html`
+  <div style="max-width: 420px;">
+    <uui-ref-node
+      ?selectable="${props.selectable}"
+      name="Rabbit Suit Product Page"
+      icon="bug"
+      detail="path/to/nowhere">
+      <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+      <uui-action-bar slot="actions">
+        <uui-button type="button" label="Delete"
+          ><uui-icon name="delete"></uui-icon
+        ></uui-button>
+      </uui-action-bar>
+    </uui-ref-node>
+  </div>
+`;
+
+Selectable.args = {
+  selectable: true,
+};
+
+Selectable.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node
+  selectable
+  name="Rabbit Suit Product Page"
+  icon="bug"
+  detail="path/to/nowhere">
+  <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+  <uui-action-bar slot="actions">
+    <uui-button type="button" label="Delete"><uui-icon name="delete"></uui-icon></uui-button>
+  </uui-action-bar>
+</uui-ref-node>
+    `,
+    },
+  },
+};
+
+export const Disabled: Story = props => html`
+  <div style="max-width: 420px;">
+    <uui-ref-node
+      ?disabled="${props.disabled}"
+      name="Rabbit Suit Product Page"
+      icon="bug"
+      detail="path/to/nowhere">
+      <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+      <uui-action-bar slot="actions">
+        <uui-button type="button" label="Delete"
+          ><uui-icon name="delete"></uui-icon
+        ></uui-button>
+      </uui-action-bar>
+    </uui-ref-node>
+  </div>
+`;
+
+Disabled.args = {
+  disabled: true,
+};
+
+Disabled.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node
+  disabled
+  name="Rabbit Suit Product Page"
+  icon="bug"
+  detail="path/to/nowhere">
+  <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+  <uui-action-bar slot="actions">
+    <uui-button type="button" label="Delete"><uui-icon name="delete"></uui-icon></uui-button>
+  </uui-action-bar>
+</uui-ref-node>
+    `,
+    },
+  },
+};
 
 const listOfNodeNames: string[] = ArrayOfUmbracoWords(10);
-export const Listed = () => html`
+export const Listed: Story = () => html`
   <uui-ref-list style="max-width: 420px;">
     ${listOfNodeNames.map(
       name => html`<uui-ref-node
@@ -116,10 +176,43 @@ export const Listed = () => html`
         icon="bug"
         detail="path/to/nowhere">
         <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
-        <uui-action-bar slot="actions"
-          ><uui-button><uui-icon name="delete"></uui-icon></uui-button
-        ></uui-action-bar>
+        <uui-action-bar slot="actions">
+          <uui-button type="button" label="Delete"
+            ><uui-icon name="delete"></uui-icon
+          ></uui-button>
+        </uui-action-bar>
       </uui-ref-node>`
     )}
   </uui-ref-list>
 `;
+
+Listed.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-list>
+  
+  <uui-ref-node name="Node 1" icon="bug" detail="path/to/nowhere">
+    <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+    <uui-action-bar slot="actions">
+      <uui-button type="button" label="Delete"><uui-icon name="delete"></uui-icon></uui-button>
+    </uui-action-bar>
+  </uui-ref-node>
+
+  <uui-ref-node name="Node 2" icon="bug" detail="path/to/nowhere">
+    <uui-action-bar slot="actions">
+      <uui-button type="button" label="Delete"><uui-icon name="delete"></uui-icon></uui-button>
+    </uui-action-bar>
+  </uui-ref-node>
+  
+  <uui-ref-node name="Node 3" icon="bug" detail="path/to/nowhere">
+    <uui-action-bar slot="actions">
+      <uui-button type="button" label="Delete"><uui-icon name="delete"></uui-icon></uui-button>
+    </uui-action-bar>
+  </uui-ref-node>
+
+</uui-ref-list>
+    `,
+    },
+  },
+};
