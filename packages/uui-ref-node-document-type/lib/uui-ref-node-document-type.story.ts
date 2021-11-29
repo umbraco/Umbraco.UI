37fa3342b1b359c3abec7e054703d7b297ee58e0
Mads Rasmussen <madsr@hey.com>
2021-11-29 10:44:21 +0100

    update stories for document type with controls

--- packages/uui-ref-node-document-type/lib/uui-ref-node-document-type.story.ts
+++ packages/uui-ref-node-document-type/lib/uui-ref-node-document-type.story.ts
@@ -7,41 +7,199 @@ export default {
   id: 'uui-ref-node-document-type',
   title: 'Displays/References/Document Type',
   component: 'uui-ref-node-document-type',
-  parameters: {
-    docs: {
-      source: {
-        code: `<uui-ref-node-document-type></uui-ref-node-document-type>`,
-      },
+};
+
+const Template: Story = props => html`
+  <div style="max-width: 420px;">
+    <uui-ref-node-document-type
+      name="${props.name}"
+      icon="${props.icon}"
+      alias="${props.alias}"
+      ?selectable=${props.selectable}
+      ?error=${props.error}
+      ?disabled=${props.disabled}>
+      <uui-action-bar slot="actions">
+        <uui-button label="Remove">Remove</uui-button>
+      </uui-action-bar>
+    </uui-ref-node-document-type>
+  </div>
+`;
+
+export const AAAOverview = Template.bind({});
+AAAOverview.args = {
+  name: 'Product Page',
+  icon: 'bug',
+  alias: 'productPage',
+};
+AAAOverview.storyName = 'Overview';
+AAAOverview.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node-document-type
+  name="Product Page"
+  icon="bug"
+  alias="productPage">
+  <uui-action-bar slot="actions">
+    <uui-button label="Remove">Remove</uui-button>
+  </uui-action-bar>
+</uui-ref-node-document-type>
+    `,
     },
   },
 };
 
-export const Overview: Story = () => html`
+export const Border: Story = () => html`
   <div style="max-width: 420px;">
     <uui-ref-node-document-type
+      border
       name="Product Page"
       icon="bug"
-      alias="productPageAlias">
+      alias="productPage">
       <uui-action-bar slot="actions">
-        <uui-button>Remove</uui-button>
+        <uui-button label="Remove">Remove</uui-button>
       </uui-action-bar>
     </uui-ref-node-document-type>
   </div>
 `;
 
-export const Selectable = () => html`
+Border.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node-document-type
+  border
+  name="Product Page"
+  icon="bug"
+  alias="productPage">
+  <uui-action-bar slot="actions">
+    <uui-button label="Remove">Remove</uui-button>
+  </uui-action-bar>
+</uui-ref-node-document-type>
+    `,
+    },
+  },
+};
+
+export const Selectable: Story = props => html`
   <div style="max-width: 420px;">
     <uui-ref-node-document-type
-      selectable
+      ?selectable="${props.selectable}"
       name="Product Page"
       icon="bug"
-      alias="productPageAlias">
+      alias="productPage">
       <uui-action-bar slot="actions">
-        <uui-button>Remove</uui-button>
+        <uui-button label="Remove">Remove</uui-button>
       </uui-action-bar>
     </uui-ref-node-document-type>
   </div>
 `;
+
+Selectable.args = {
+  selectable: true,
+};
+
+Selectable.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node-document-type
+  selectable
+  name="Product Page"
+  icon="bug"
+  alias="productPage">
+  <uui-action-bar slot="actions">
+    <uui-button label="Remove">Remove</uui-button>
+  </uui-action-bar>
+</uui-ref-node-document-type>
+    `,
+    },
+  },
+};
+
+export const Disabled: Story = props => html`
+  <div style="max-width: 420px;">
+    <uui-ref-node-document-type
+      ?disabled="${props.disabled}"
+      name="Product Page"
+      icon="bug"
+      alias="productPage">
+      <uui-action-bar slot="actions">
+        <uui-button label="Remove">Remove</uui-button>
+      </uui-action-bar>
+    </uui-ref-node-document-type>
+  </div>
+`;
+
+Disabled.args = {
+  disabled: true,
+};
+
+Disabled.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node-document-type
+  disabled
+  name="Product Page"
+  icon="bug"
+  alias="productPage">
+  <uui-action-bar slot="actions">
+    <uui-button label="Remove">Remove</uui-button>
+  </uui-action-bar>
+</uui-ref-node-document-type>
+    `,
+    },
+  },
+};
+
+const listOfNodeNames: string[] = ArrayOfUmbracoWords(10);
+export const Listed: Story = () => html`
+  <uui-ref-list style="max-width: 420px;">
+    ${listOfNodeNames.map(
+      name => html`<uui-ref-node-document-type
+        name=${name}
+        icon="bug"
+        alias="alias">
+        <uui-action-bar slot="actions">
+          <uui-button label="Remove">Remove</uui-button>
+        </uui-action-bar>
+      </uui-ref-node-document-type>`
+    )}
+  </uui-ref-list>
+`;
+
+Listed.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-list>
+  
+  <uui-ref-node-document-type name="Document Type 1" icon="bug" alias="alias">
+    <uui-action-bar slot="actions">
+      <uui-button label="Remove">Remove</uui-button>
+    </uui-action-bar>
+  </uui-ref-node-document-type>
+
+  <uui-ref-node-document-type name="Document Type 2" icon="bug" alias="alias">
+    <uui-action-bar slot="actions">
+      <uui-button label="Remove">Remove</uui-button>
+    </uui-action-bar>
+  </uui-ref-node-document-type>
+
+  <uui-ref-node-document-type name="Document Type 3" icon="bug" alias="alias">
+    <uui-action-bar slot="actions">
+      <uui-button label="Remove">Remove</uui-button>
+    </uui-action-bar>
+  </uui-ref-node-document-type>
+
+</uui-ref-list>
+    `,
+    },
+  },
+};
+
+/*
 
 export const Disabled = () => html`
   <div style="max-width: 420px;">
@@ -72,3 +230,4 @@ export const Listed = () => html`
     )}
   </uui-ref-list>
 `;
+*/
