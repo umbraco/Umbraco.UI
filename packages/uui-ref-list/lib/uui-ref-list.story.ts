65961d36a6daed9620ef9b2fec57b716e99b59a6
Mads Rasmussen <madsr@hey.com>
2021-11-29 14:10:45 +0100

    update ref-list story with docs

--- packages/uui-ref-list/lib/uui-ref-list.story.ts
+++ packages/uui-ref-list/lib/uui-ref-list.story.ts
@@ -1,3 +1,4 @@
+import { Story } from '@storybook/web-components';
 import { html } from 'lit-html';
 import { ArrayOfUmbracoWords } from '../../../src/helper/UmbracoWordGenerator';
 import './index';
@@ -8,10 +9,13 @@ export default {
 };
 
 const listOfNodeNames: string[] = ArrayOfUmbracoWords(10);
-export const ContentReferenceList = () => html`
-  <uui-ref-list style="max-width: 640px;">
+const Template: Story = () => html`
+  <uui-ref-list style="max-width: 420px;">
     ${listOfNodeNames.map(
-      name => html`<uui-ref-node name=${name} icon="bug" url="path/to/nowhere">
+      name => html`<uui-ref-node
+        name=${name}
+        icon="bug"
+        detail="path/to/nowhere">
         <uui-action-bar slot="actions">
           <uui-button label="Remove">Remove</uui-button>
         </uui-action-bar>
@@ -19,3 +23,36 @@ export const ContentReferenceList = () => html`
     )}
   </uui-ref-list>
 `;
+
+export const AAAOverview = Template.bind({});
+AAAOverview.storyName = 'Overview';
+AAAOverview.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-list>
+
+  <uui-ref-node name="Node 1" icon="bug" detail="path/to/nowhere">
+    <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+    <uui-action-bar slot="actions">
+      <uui-button label="Remove">Remove</uui-button>
+    </uui-action-bar>
+  </uui-ref-node>
+
+  <uui-ref-node name="Node 2" icon="bug" detail="path/to/nowhere">
+    <uui-action-bar slot="actions">
+      <uui-button label="Remove">Remove</uui-button>
+    </uui-action-bar>
+  </uui-ref-node>
+  
+  <uui-ref-node name="Node 3" icon="bug" detail="path/to/nowhere">
+    <uui-action-bar slot="actions">
+      <uui-button label="Remove">Remove</uui-button>
+    </uui-action-bar>
+  </uui-ref-node>
+
+</uui-ref-list>
+    `,
+    },
+  },
+};
