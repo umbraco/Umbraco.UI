f90023952e76e645df4e81845db1f8ca99b9d5a9
Julia gru <julia.gru@gmail.com>
2021-11-30 15:05:23 +0100

    remove old files

--- src/components/uui-pagination/UUIPaginationEvent.ts
+++
@@ -1,6 +0,0 @@
-import { UUIEvent } from '@umbraco-ui/uui-base/lib/events';
-import { UUIPaginationElement } from './uui-pagination.element';
-
-export class UUIPaginationEvent extends UUIEvent<{}, UUIPaginationElement> {
-  public static readonly CHANGE = 'change';
-}
