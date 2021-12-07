3108e5d61902fc0f7512303f927c848745d61420
Mads Rasmussen <madsr@hey.com>
2021-12-07 11:28:56 +0100

    uui-dialog: add tests for slots

--- packages/uui-dialog/lib/uui-dialog.test.ts
+++ packages/uui-dialog/lib/uui-dialog.test.ts
@@ -9,7 +9,19 @@ describe('UUIDialogElement', () => {
     element = await fixture(html` <uui-dialog></uui-dialog> `);
   });
 
-  it('passes the a11y audit', async () => {
-    await expect(element).shadowDom.to.be.accessible();
+  it('passes the a11y audit', () => {
+    expect(element).shadowDom.to.be.accessible();
+  });
+
+  describe('template', () => {
+    it('renders a default slot', () => {
+      const slot = element.shadowRoot!.querySelector('slot')!;
+      expect(slot).to.exist;
+    });
+
+    it('renders an action slot', () => {
+      const slot = element.shadowRoot!.querySelector('slot[name=actions]')!;
+      expect(slot).to.exist;
+    });
   });
 });
