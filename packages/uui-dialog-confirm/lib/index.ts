fda88f34d557a3f6a8be3afa68e4374c373c31ce
Niels Lyngsø <niels.lyngso@gmail.com>
2021-12-13 14:33:06 +0100

    remove confirm dialog

--- packages/uui-dialog-confirm/lib/index.ts
+++
@@ -1,4 +0,0 @@
-import { UUIDialogConfirmElement } from './uui-dialog-confirm.element';
-import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
-
-defineElement('uui-dialog-confirm', UUIDialogConfirmElement as any);
