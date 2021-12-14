929f311a42b53f194f8e2374700f4e2769a5901b
Mads Rasmussen <madsr@hey.com>
2021-12-14 11:01:29 +0100

    add temp hardcoded box-shadow value

--- packages/uui-dialog/lib/uui-dialog.element.ts
+++ packages/uui-dialog/lib/uui-dialog.element.ts
@@ -22,7 +22,13 @@ export class UUIDialogElement extends LitElement {
           --uui-dialog-background-color,
           var(--uui-interface-surface)
         );
-        box-shadow: var(--uui-dialog-box-shadow, var(--uui-shadow-depth-5));
+
+        /* TODO: fix automatic fallback values for shadows shadows. var(--uui-shadow-depth-5) */
+        box-shadow: var(
+          --uui-dialog-box-shadow,
+          0 19px 38px rgba(0, 0, 0, 0.3),
+          0 15px 12px rgba(0, 0, 0, 0.22)
+        );
         border-radius: var(
           --uui-dialog-border-radius,
           calc(var(--uui-border-radius) * 2)
