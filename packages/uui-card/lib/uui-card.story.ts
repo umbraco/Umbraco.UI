4a3a5443c30e23fd97e8c11de3b094a01c7be938
Niels Lyngsø <niels.lyngso@gmail.com>
2021-12-15 22:18:26 +0100

    Card story

--- packages/uui-card/lib/uui-card.story.ts
+++ packages/uui-card/lib/uui-card.story.ts
@@ -1,42 +1,29 @@
+import { Story } from '@storybook/web-components';
 import { html } from 'lit-html';
-// import '../uui-card-content-node/index';
-// import '../uui-card-media/index';
-// import '../uui-card-user/index';
 
 export default {
-  title: 'Displays/Cards',
+  id: 'uui-card',
+  title: 'Displays/Cards/Card',
+  component: 'uui-card',
 };
 
-// export const Overview = () => html`
-//   <div style="width: 300px">
-//     <uui-content-node-card selectable name="The card" icon="bug">
-//       <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
-//       <!-- TODO: we should make some kind of component for this data layout: -->
-//       <ul style="list-style: none; padding-inline-start: 0px; margin: 0;">
-//         <li><span style="font-weight: 700">Created:</span> Yesterday</li>
-//         <li>
-//           <span style="font-weight: 700">Last Edited: </span> 2021-03-15 09:29
-//         </li>
-//         <li><span style="font-weight: 700">Some property:</span> Some value</li>
-//         <li>
-//           <span style="font-weight: 700">Another property:</span> Another value
-//         </li>
-//       </ul>
-//     </uui-content-node-card>
-//     <br />
-//     <uui-media-card selectable name="Doggo 1" file-ext="image"
-//       ><img src="https://placedog.net/1447/?random" alt=""
-//     /></uui-media-card>
-//     <br />
-//     <uui-media-card selectable name="File 1"></uui-media-card>
-//     <br />
-//     <uui-media-card selectable name="File 1" file-ext="txt"></uui-media-card>
-//     <br />
-//     <uui-user-card selectable name="Sherlock Holmes" group-name="Editors">
-//       <uui-tag size="s" slot="tag" look="positive">Invited</uui-tag>
-//       <div style="margin-bottom: 12px">Editors</div>
-//       <div>Last login</div>
-//       <div>March 19, 2021 9:14 AM</div>
-//     </uui-user-card>
-//   </div>
-// `;
+const Template: Story = () => html`
+  <p>
+    Card is a Component that provides the basics for a Card component. This can
+    be extended in code to match a certain need.
+  </p>
+`;
+
+export const AAAOverview = Template.bind({});
+AAAOverview.storyName = 'Overview';
+AAAOverview.parameters = {
+  docs: {
+    source: {
+      code: `
+import { UUICardElement } from '@umbraco-ui/uui-card/lib/uui-card.element;
+
+class MyCardElement extends UUICardElement {}
+    `,
+    },
+  },
+};
