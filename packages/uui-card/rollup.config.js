7583f56764a92579f549d4f1db96e05985a58a4b
Niels Lyngsø <niels.lyngso@gmail.com>
2021-12-15 22:46:50 +0100

    remove index from package rollup script

--- packages/uui-card/rollup.config.js
+++ packages/uui-card/rollup.config.js
@@ -1,6 +1,5 @@
 import { UUIProdConfig } from '../rollup-package.config';
 
 export default UUIProdConfig({
-  entryPoints: ['index', 'uui-card.element'],
-  bundles: ['index'],
+  entryPoints: ['uui-card.element'],
 });
