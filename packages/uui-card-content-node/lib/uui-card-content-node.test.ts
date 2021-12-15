2059640d5e72c1aa7a765c24ad75073197c70574
Niels Lyngsø <niels.lyngso@gmail.com>
2021-12-15 11:00:49 +0100

    actions slot, ui adjustments

--- packages/uui-card-content-node/lib/uui-card-content-node.test.ts
+++ packages/uui-card-content-node/lib/uui-card-content-node.test.ts
@@ -1,6 +1,13 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import {
+  html,
+  fixture,
+  expect,
+  oneEvent,
+  elementUpdated,
+} from '@open-wc/testing';
 import { UUICardContentNodeElement } from './uui-card-content-node.element';
 import '.';
+import { UUICardEvent } from '@umbraco-ui/uui-card/lib/UUICardEvent';
 
 describe('UUICardContentNodeElement', () => {
   let element: UUICardContentNodeElement;
@@ -13,5 +20,78 @@ describe('UUICardContentNodeElement', () => {
 
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
+  });
+
+  describe('properties', () => {
+    it('has an error property', () => {
+      expect(element).to.have.property('error');
+    });
+
+    it('has a disabled property', () => {
+      expect(element).to.have.property('disabled');
+    });
+
+    it('has a name property', () => {
+      expect(element).to.have.property('name');
+    });
+  });
+
+  describe('template', () => {
+    it('renders a default slot', () => {
+      const slot = element.shadowRoot!.querySelector('slot')!;
+      expect(slot).to.exist;
+    });
+
+    it('renders an icon slot', () => {
+      const slot = element.shadowRoot!.querySelector('slot[name=icon]')!;
+      expect(slot).to.exist;
+    });
+
+    it('renders a tag slot', () => {
+      const slot = element.shadowRoot!.querySelector('slot[name=tag]')!;
+      expect(slot).to.exist;
+    });
+
+    it('renders an actions slot', () => {
+      const slot = element.shadowRoot!.querySelector('slot[name=actions]')!;
+      expect(slot).to.exist;
+    });
+  });
+
+  describe('events', () => {
+    describe('open', () => {
+      it('emits a open event when info is clicked', async () => {
+        const listener = oneEvent(element, UUICardEvent.OPEN);
+        const infoElement: HTMLElement | null =
+          element.shadowRoot!.querySelector('#open-part');
+        infoElement?.click();
+        const event = await listener;
+        expect(event).to.exist;
+        expect(event.type).to.equal(UUICardEvent.OPEN);
+      });
+
+      it('emits a open event when icon is clicked', async () => {
+        const listener = oneEvent(element, UUICardEvent.OPEN);
+        const iconElement: HTMLElement | null =
+          element.shadowRoot!.querySelector('#icon');
+        iconElement?.click();
+        const event = await listener;
+        expect(event).to.exist;
+        expect(event.type).to.equal(UUICardEvent.OPEN);
+      });
+    });
+
+    describe('select', () => {
+      it('emits a selected event when selectable', async () => {
+        element.selectable = true;
+        await elementUpdated(element);
+        const listener = oneEvent(element, UUICardEvent.SELECTED);
+        element.click();
+        const event = await listener;
+        expect(event).to.exist;
+        expect(event.type).to.equal(UUICardEvent.SELECTED);
+        expect(element.selected).to.be.true;
+      });
+    });
   });
 });
