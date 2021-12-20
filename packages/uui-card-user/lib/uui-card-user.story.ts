82bcd334007f9ee2d9068504ba804208a0117de0
Mads Rasmussen <madsr@hey.com>
2021-12-20 09:57:45 +0100

    update user card stories

--- packages/uui-card-user/lib/uui-card-user.story.ts
+++ packages/uui-card-user/lib/uui-card-user.story.ts
@@ -8,10 +8,6 @@ export default {
   id: 'uui-card-user',
   args: {
     name: 'John Rabbit',
-    selectable: false,
-    selected: false,
-    error: false,
-    disabled: false,
   },
 };
 
@@ -31,12 +27,12 @@ const Template: Story = props => html`
 
 export const AAAOverview = Template.bind({});
 AAAOverview.storyName = 'Overview';
-AAAOverview.parameters = {
-  docs: {
-    source: {
-      code: `<uui-card-user name="John Rabbit"></uui-card-user>`,
-    },
-  },
+
+AAAOverview.args = {
+  selectable: false,
+  selected: false,
+  error: false,
+  disabled: false,
 };
 
 export const Selectable: Story = Template.bind({});
@@ -44,9 +40,29 @@ Selectable.args = {
   selectable: true,
 };
 
+Selectable.parameters = {
+  controls: { include: ['selectable'] },
+  docs: {
+    source: {
+      code: `
+<uui-card-user name="John Rabbit" selectable></uui-card-user>`,
+    },
+  },
+};
+
 export const Disabled: Story = Template.bind({});
 Disabled.args = {
   disabled: true,
+};
+
+Disabled.parameters = {
+  controls: { include: ['disabled'] },
+  docs: {
+    source: {
+      code: `
+<uui-card-user name="John Rabbit" disabled></uui-card-user>`,
+    },
+  },
 };
 
 export const Error: Story = Template.bind({});
@@ -54,16 +70,19 @@ Error.args = {
   error: true,
 };
 
-export const Actions: Story = props => html`
+Error.parameters = {
+  controls: { include: ['error'] },
+  docs: {
+    source: {
+      code: `
+<uui-card-user name="John Rabbit" error></uui-card-user>`,
+    },
+  },
+};
+
+export const Actions: Story = () => html`
   <div style="width: 200px">
-    <uui-card-user
-      name=${props.name}
-      ?selectable=${props.selectable}
-      ?selected=${props.selected}
-      ?error=${props.error}
-      ?disabled=${props.disabled}>
-      <div style="margin-bottom: 12px">Editors</div>
-      <div>Has not logged in yet</div>
+    <uui-card-user name="John Rabbit">
       <uui-action-bar slot="actions">
         <uui-button label="Remove">Remove</uui-button>
       </uui-action-bar>
@@ -71,17 +90,35 @@ export const Actions: Story = props => html`
   </div>
 `;
 
-export const Tags: Story = props => html`
+Actions.parameters = {
+  docs: {
+    source: {
+      code: `
+    <uui-card-user 
+      name="John Rabbit">
+      <uui-action-bar slot="actions">
+        <uui-button label="Remove">Remove</uui-button>
+      </uui-action-bar>
+    </uui-card-user>`,
+    },
+  },
+};
+
+export const Tags: Story = () => html`
   <div style="width: 200px">
-    <uui-card-user
-      name=${props.name}
-      ?selectable=${props.selectable}
-      ?selected=${props.selected}
-      ?error=${props.error}
-      ?disabled=${props.disabled}>
-      <div style="margin-bottom: 12px">Editors</div>
-      <div>Has not logged in yet</div>
-      <uui-tag slot="tag" size="s" look="positive">Published</uui-tag>
+    <uui-card-user name="John Rabbit">
+      <uui-tag slot="tag" size="s" look="danger">Disabled</uui-tag>
     </uui-card-user>
   </div>
 `;
+
+Tags.parameters = {
+  docs: {
+    source: {
+      code: `
+    <uui-card-user name="John Rabbit">
+      <uui-tag slot="tag" size="s" look="danger">Disabled</uui-tag>
+    </uui-card-user>`,
+    },
+  },
+};
