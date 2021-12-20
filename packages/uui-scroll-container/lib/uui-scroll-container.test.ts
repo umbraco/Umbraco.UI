ebb7924fb133778f2927c507905b2deb7fb14425
Niels Lyngsø <niels.lyngso@gmail.com>
2021-12-20 20:42:43 +0100

    more tests

--- packages/uui-scroll-container/lib/uui-scroll-container.test.ts
+++ packages/uui-scroll-container/lib/uui-scroll-container.test.ts
@@ -8,8 +8,6 @@ describe('UUIScrollContainerElement', () => {
   beforeEach(async () => {
     element = await fixture(
       html`<uui-scroll-container style="width:200px; height:200px;">
-        initial line is way toooo long
-        WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY<br />
         1 line<br />
         2 line<br />
         3 line<br />
@@ -42,6 +40,44 @@ describe('UUIScrollContainerElement', () => {
     element.scrollTop = 42;
     await expect(element.scrollTop).to.be.equal(42);
   });
+});
+
+describe('UUIScrollContainerElement with a lot of content', () => {
+  let element: UUIScrollContainerElement;
+
+  beforeEach(async () => {
+    element = await fixture(
+      html`<uui-scroll-container style="width:200px; height:200px;">
+        initial line is way toooo long
+        WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY<br />
+        1 line<br />
+        2 line<br />
+        3 line<br />
+        4 line<br />
+        5 line<br />
+        6 line<br />
+        7 line<br />
+        8 line<br />
+        9 line<br />
+        10 line<br />
+        11 line<br />
+        12 line<br />
+        13 line<br />
+        14 line<br />
+        15 line<br />
+        16 line<br />
+        17 line<br />
+        18 line<br />
+        19 line<br />
+        20 line
+      </uui-scroll-container>`
+    );
+  });
+
+  it('can scroll', async () => {
+    element.scrollTop = 42;
+    await expect(element.scrollTop).to.be.equal(42);
+  });
 
   it('cant scroll to far', async () => {
     element.scrollTop = 9999;
@@ -56,5 +92,26 @@ describe('UUIScrollContainerElement', () => {
   it('can scroll sideways if content enforces it', async () => {
     element.scrollLeft = 42;
     await expect(element.scrollLeft).to.be.equal(42);
+  });
+});
+
+describe('UUIScrollContainerElement with very little content', () => {
+  let element: UUIScrollContainerElement;
+
+  beforeEach(async () => {
+    element = await fixture(
+      html`<uui-scroll-container style="width:200px; height:200px;">
+        very little content.
+      </uui-scroll-container>`
+    );
+  });
+  it('cannot scroll', async () => {
+    element.scrollTop = 10;
+    await expect(element.scrollTop).to.be.equal(0);
+  });
+
+  it('cannot scroll sideways', async () => {
+    element.scrollLeft = 42;
+    await expect(element.scrollLeft).to.be.equal(0);
   });
 });
