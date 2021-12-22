676099ccc61f3b5d106b0f904c797bc99a8539f7
Niels Lyngsø <niels.lyngso@gmail.com>
2021-12-22 20:45:56 +0100

    Remove sizing option

--- packages/uui-tag/lib/uui-tag.element.ts
+++ packages/uui-tag/lib/uui-tag.element.ts
@@ -3,14 +3,12 @@ import { property } from 'lit/decorators.js';
 import {
   InterfaceLookType,
   InterfaceLookDefaultValue,
-  Size,
 } from '@umbraco-ui/uui-base/lib/types';
 
 /**
  *  Tag component from Umbraco UI components library. Comes in one shape, but different looks and sizes
  *  @element uui-tag
  *  @slot - for tag contents
- *  @cssprop --uui-tag-font-size - Set the components font size. Setting this property overwrites what comes from size attribute.
  */
 export class UUITagElement extends LitElement {
   static styles = [
@@ -19,9 +17,9 @@ export class UUITagElement extends LitElement {
         display: inline-block;
         font-family: inherit;
         font-weight: 700;
-        line-height: 1.1;
-        font-size: var(--uui-tag-font-size, 0.9em);
-        padding: calc(var(--uui-tag-font-size) / 2.3) var(--uui-tag-font-size);
+        line-height: 1.1; /* should have have another line height, lets check with multiple line tag. */
+        /*font-size: var(--uui-tag-font-size, 0.9em);*/
+        padding: 0.5em 1em;
         border-radius: 1000px;
         background-color: var(--uui-interface-surface-alt);
         color: var(--uui-interface-contrast);
@@ -32,24 +30,6 @@ export class UUITagElement extends LitElement {
       slot {
         display: flex;
         align-items: center;
-      }
-
-      /** TODO: Revisit size options: */
-
-      :host([size='xs']) {
-        --uui-tag-font-size: 10px;
-      }
-      :host([size='s']) {
-        --uui-tag-font-size: 11px;
-      }
-      :host([size='m']) {
-        --uui-tag-font-size: 12px;
-      }
-      :host([size='l']) {
-        --uui-tag-font-size: 13px;
-      }
-      :host([size='xl']) {
-        --uui-tag-font-size: 14px;
       }
 
       /* Looks */
@@ -91,15 +71,6 @@ export class UUITagElement extends LitElement {
       }
     `,
   ];
-
-  /**
-   * Defines the size of the tag. It changes the `--uui-tag-font-size` value.
-   * @type {'xs' | 's' | 'm' | 'l' | 'xl'}
-   * @attr
-   * @default m
-   */
-  @property({ reflect: true })
-  public size: Size = 'm';
 
   /**
    * Defines the look of the tag.
