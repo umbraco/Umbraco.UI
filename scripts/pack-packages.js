4e68acef7547aedf0b16338ad7db965552ceaad9
Niels Lyngsø <niels.lyngso@gmail.com>
2022-01-06 16:57:52 +0100

    remove log

--- scripts/pack-packages.js
+++ scripts/pack-packages.js
@@ -2,6 +2,5 @@ const { packageDirectoryNames } = require('./packages');
 const { execSync } = require('child_process');
 
 packageDirectoryNames.forEach(packageDirectoryName => {
-  console.log(packageDirectoryName);
   execSync('cd packages/' + packageDirectoryName + ' && npm pack');
 });
