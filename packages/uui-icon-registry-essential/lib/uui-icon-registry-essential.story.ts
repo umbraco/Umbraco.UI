88989234afdae2cf13a856edb44837777ef522ba
Niels Lyngsø <niels.lyngso@gmail.com>
2022-01-11 20:27:40 +0100

    initial essential icon set

--- packages/uui-icon-registry-essential/lib/uui-icon-registry-essential.story.ts
+++ packages/uui-icon-registry-essential/lib/uui-icon-registry-essential.story.ts
@@ -13,9 +13,9 @@ export default {
       source: {
         code: `
 <uui-icon-registry-essential>
-  <uui-icon name="<IconName>"></uui-icon>
-  <uui-icon name="<IconName>"></uui-icon>
-  <uui-icon name="<IconName>"></uui-icon>
+  <uui-icon name="[IconName]"></uui-icon>
+  <uui-icon name="[IconName]"></uui-icon>
+  <uui-icon name="[IconName]"></uui-icon>
   ...
 </uui-icon-registry-essential>`,
       },
@@ -30,15 +30,21 @@ export const Overview: Story = () => {
     ?.sort((a, b) => a.localeCompare(b));
 
   return html`
-    <uui-icon-registry-essential style="display: flex;">
+    <uui-icon-registry-essential
+      style="display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(80px, 1fr));
+        grid-gap: var(--uui-size-layout-2);
+        margin: var(--uui-size-layout-2);
+        place-items: start;
+        justify-content: space-between;">
       ${sortedIcons?.map(
         name => html`
           <div
-            style="flex: 0 0 100px; display: flex; flex-direction: column; align-items: center;">
+            style="width:100%; display: flex; flex-direction: column; align-items: center;">
             <uui-icon
               name="${name}"
               style="font-size: 24px; margin-bottom: 6px;"></uui-icon>
-            <div>${name}</div>
+            <small>${name}</small>
           </div>
         `
       )}
