0cfd85bd7355cea406ba33a2b5ac946b5b3e5c6b
Niels Lyngsø <niels.lyngso@gmail.com>
2022-01-12 20:06:30 +0100

    stories

--- packages/uui-label/lib/uui-label.story.ts
+++ packages/uui-label/lib/uui-label.story.ts
@@ -2,6 +2,7 @@ import { Story } from '@storybook/web-components';
 import { html } from 'lit-html';
 import '@umbraco-ui/uui-label/lib/index';
 import '@umbraco-ui/uui-input/lib/index';
+import '@umbraco-ui/uui-checkbox/lib/index';
 
 export default {
   id: 'uui-label',
@@ -21,3 +22,13 @@ export const Overview: Story = () =>
     <uui-label for="MyInput">My Label</uui-label>
     <uui-input id="MyInput" label="My A11Y Label">My Input value</uui-input>
   `;
+export const ForCheckbox: Story = () =>
+  html`
+    <uui-label for="MyInput">My Label</uui-label>
+    <uui-checkbox id="MyInput" label="My A11Y Label"></uui-checkbox>
+  `;
+export const UseWithNativeInput: Story = () =>
+  html`
+  <uui-label for="MyInput">My Label</uui-label>
+  <input id="MyInput" type="text" aria-label="My A11Y Label"></input>
+`;
