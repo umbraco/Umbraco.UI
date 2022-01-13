3997096b2ad535c92e0ea6a345f06dba70b1481a
Mads Rasmussen <madsr@hey.com>
2022-01-13 12:45:45 +0100

    update stories

--- packages/uui-label/lib/uui-label.story.ts
+++ packages/uui-label/lib/uui-label.story.ts
@@ -8,27 +8,50 @@ export default {
   id: 'uui-label',
   title: 'Inputs/Label',
   component: 'uui-label',
-  parameters: {
-    docs: {
-      source: {
-        code: `<uui-label></uui-label>`,
-      },
+};
+
+const Template: Story = props => html`
+  <uui-label ?disabled=${props.disabled}>Label</uui-label>
+`;
+
+export const AAAOverview = Template.bind({});
+AAAOverview.args = {
+  disabled: false,
+};
+AAAOverview.storyName = 'Overview';
+
+/*
+AAAOverview.parameters = {
+  docs: {
+    source: {
+      code: `
+<uui-ref-node
+  name="Rabbit Suit Product Page"
+  detail="path/to/nowhere">
+  <uui-tag size="s" slot="tag" look="positive">Published</uui-tag>
+  <uui-action-bar slot="actions"
+    ><uui-button><uui-icon name="delete"></uui-icon></uui-button
+  ></uui-action-bar>
+</uui-ref-node>
+    `,
     },
   },
 };
+*/
 
-export const Overview: Story = () =>
-  html`
-    <uui-label for="MyInput">My Label</uui-label>
-    <uui-input id="MyInput" label="My A11Y Label">My Input value</uui-input>
-  `;
-export const ForCheckbox: Story = () =>
-  html`
-    <uui-label for="MyInput">My Label</uui-label>
-    <uui-checkbox id="MyInput" label="My A11Y Label"></uui-checkbox>
-  `;
-export const UseWithNativeInput: Story = () =>
-  html`
-  <uui-label for="MyInput">My Label</uui-label>
-  <input id="MyInput" type="text" aria-label="My A11Y Label"></input>
+export const ConnectWithFormControl: Story = () => html`
+<uui-label for="MyInput">My Label</uui-label>
+<uui-input id="MyInput" label="My A11Y Label"></uui-input>
+
+<br />
+<br />
+
+<uui-label for="MyCheckbox">My Label</uui-label>
+<uui-checkbox id="MyCheckbox" label="My A11Y Label"></uui-checkbox>
+
+<br />
+<br />
+
+<uui-label for="MyNativeInput">My Label</uui-label>
+<input type="text" id="MyNativeInput" label="My A11Y Label"></input>
 `;
