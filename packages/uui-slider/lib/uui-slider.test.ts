9e4fc23cc53ec281404966055d28c18f014ecba0
Niels Lyngsø <niels.lyngso@gmail.com>
2022-01-14 21:39:45 +0100

    Clean up and refactor slide component

--- packages/uui-slider/lib/uui-slider.test.ts
+++ packages/uui-slider/lib/uui-slider.test.ts
@@ -1,16 +1,57 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import {
+  html,
+  fixture,
+  expect,
+  elementUpdated,
+  oneEvent,
+} from '@open-wc/testing';
 import { UUISliderElement } from './uui-slider.element';
 import './index';
+import { UUISliderEvent } from './UUISliderEvents';
 
 describe('UuiSlider', () => {
   let element: UUISliderElement;
+  let input: HTMLInputElement;
+
   beforeEach(async () => {
     element = await fixture(
       html` <uui-slider label="a slider label"></uui-slider> `
     );
+    input = element.shadowRoot?.querySelector('input') as HTMLInputElement;
   });
-  it('test that disable works', async () => {
-    expect(element).to.exist;
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+
+  describe('properties', () => {
+    it('has a disabled property', () => {
+      expect(element).to.have.property('disabled');
+    });
+    it('disable property set input to disabled', async () => {
+      element.disabled = true;
+      await elementUpdated(element);
+      expect(input.disabled).to.be.true;
+    });
+
+    it('has a value property', () => {
+      expect(element).to.have.property('value');
+    });
+    it('has a label property', () => {
+      expect(element).to.have.property('label');
+    });
+    it('has a min property', () => {
+      expect(element).to.have.property('min');
+    });
+    it('has a max property', () => {
+      expect(element).to.have.property('max');
+    });
+    it('has a step property', () => {
+      expect(element).to.have.property('step');
+    });
+    it('has a hideStepValues property', () => {
+      expect(element).to.have.property('hideStepValues');
+    });
   });
 
   describe('methods', () => {
@@ -22,6 +63,36 @@ describe('UuiSlider', () => {
       element.focus();
       expect(document.activeElement).to.equal(element);
     });
+    describe('change', () => {
+      it('emits a change event when native input fires one', async () => {
+        const listener = oneEvent(element, UUISliderEvent.CHANGE);
+
+        input.dispatchEvent(new Event('change'));
+
+        const event = await listener;
+        expect(event).to.exist;
+        expect(event.type).to.equal(UUISliderEvent.CHANGE);
+        expect(event!.target).to.equal(element);
+      });
+    });
+    describe('input', () => {
+      it('emits a input event when native input fires one', async () => {
+        const listener = oneEvent(element, UUISliderEvent.INPUT);
+
+        input.dispatchEvent(new Event('input'));
+
+        const event = await listener;
+        expect(event).to.exist;
+        expect(event.type).to.equal(UUISliderEvent.INPUT);
+        expect(event!.target).to.equal(element);
+      });
+    });
+  });
+
+  it('changes the value to the input value when input event is emitted', async () => {
+    input.value = '10';
+    input.dispatchEvent(new Event('input'));
+    expect(element.value).to.equal('10');
   });
 });
 
