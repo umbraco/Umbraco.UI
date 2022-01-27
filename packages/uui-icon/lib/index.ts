1ea387932237623cddf74e2fc5d83558019cf209
jov <jov@umbraco.dk>
2022-01-27 16:38:16 +0100

    Add missing exports to barrels

--- packages/uui-icon/lib/index.ts
+++ packages/uui-icon/lib/index.ts
@@ -1,6 +1,8 @@
-import { UUIIconElement } from './uui-icon.element';
 import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
+
+import { UUIIconElement } from './uui-icon.element';
 
 defineElement('uui-icon', UUIIconElement);
 
 export * from './uui-icon.element';
+export * from './UUIIconRequestEvent';
--- packages/uui-menu-item/lib/index.ts
+++ packages/uui-menu-item/lib/index.ts
@@ -1,6 +1,8 @@
-import { UUIMenuItemElement } from './uui-menu-item.element';
 import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
+
+import { UUIMenuItemElement } from './uui-menu-item.element';
 
 defineElement('uui-menu-item', UUIMenuItemElement);
 
 export * from './uui-menu-item.element';
+export * from './UUIMenuItemEvent';
