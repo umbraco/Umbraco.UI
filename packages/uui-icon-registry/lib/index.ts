99f21b5e22f34285b85b710cbef81259252548ca
jov <jov@umbraco.dk>
2022-01-28 10:43:36 +0100

    uui-icon-registry: Add missing exports

--- packages/uui-icon-registry/lib/index.ts
+++ packages/uui-icon-registry/lib/index.ts
@@ -1,6 +1,9 @@
-import { UUIIconRegistryElement } from './uui-icon-registry.element';
 import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
+
+import { UUIIconRegistryElement } from './uui-icon-registry.element';
 
 defineElement('uui-icon-registry', UUIIconRegistryElement);
 
 export * from './uui-icon-registry.element';
+export * from './UUIIconHost';
+export * from './UUIIconRegistry';
