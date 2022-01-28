7fc06cc7f8e2ddf1fd186b8e902c7bdaed47bdc3
jov <jov@umbraco.dk>
2022-01-28 15:11:07 +0100

    Define.ts is an entrypoint so must not import from index

--- packages/uui-radio/lib/define.ts
+++ packages/uui-radio/lib/define.ts
@@ -1,4 +1,7 @@
-import { UUIRadioElement } from './uui-radio.element';
 import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
 
+import { UUIRadioGroupElement } from './uui-radio-group.element';
+import { UUIRadioElement } from './uui-radio.element';
+
 defineElement('uui-radio', UUIRadioElement);
+defineElement('uui-radio-group', UUIRadioGroupElement);
--- packages/uui-table/lib/define.ts
+++ packages/uui-table/lib/define.ts
@@ -1,13 +1,11 @@
 import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
 
-import {
-  UUITableCellElement,
-  UUITableColumnElement,
-  UUITableElement,
-  UUITableHeadCellElement,
-  UUITableHeadElement,
-  UUITableRowElement,
-} from '.';
+import { UUITableCellElement } from './uui-table-cell.element';
+import { UUITableColumnElement } from './uui-table-column.element';
+import { UUITableHeadCellElement } from './uui-table-head-cell.element';
+import { UUITableHeadElement } from './uui-table-head.element';
+import { UUITableRowElement } from './uui-table-row.element';
+import { UUITableElement } from './uui-table.element';
 
 defineElement('uui-table-column', UUITableColumnElement);
 defineElement('uui-table-head-cell', UUITableHeadCellElement);
