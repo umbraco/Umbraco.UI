fa3a9f2216c3ca3c4f6189aa6319a96463321fdc
Niels Lyngsø <niels.lyngso@gmail.com>
2022-02-01 10:54:42 +0100

    paste test code

--- packages/uui-form/lib/uui-form.element.ts
+++ packages/uui-form/lib/uui-form.element.ts
@@ -1,18 +1,50 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html } from 'lit';
+import { query } from 'lit/decorators';
 
 /**
  * @element uui-form
  */
 export class UUIFormElement extends LitElement {
-  static styles = [
-    css`
-      :host {
-        /* Styles goes here */
-      }
-    `,
-  ];
+  @query('form')
+  _form: HTMLFormElement | undefined;
+
+  _onSubmit(event: Event) {
+    event.preventDefault();
+
+    if (!this._form) {
+      return;
+    }
+
+    const isValid = this._form.checkValidity();
+
+    if (!isValid) {
+      this._form.removeAttribute('hide-validation');
+      return;
+    }
+    this._form.setAttribute('hide-validation', '');
+
+    const formData = new FormData(this._form);
+
+    for (const value of formData.values()) {
+      console.log(value);
+    }
+  }
+  _onReset() {
+    if (!this._form) {
+      return;
+    }
+    this._form.setAttribute('hide-validation', '');
+  }
 
   render() {
-    return html` Markup goes here `;
+    return html`
+      <form
+        @submit="${this._onSubmit}"
+        @reset="${this._onReset}"
+        novalidate
+        hide-validation>
+        <slot></slot>
+      </form>
+    `;
   }
 }
