5f48be862e28e18e890d6275100c21abc6d93465
Niels Lyngsø <niels.lyngso@gmail.com>
2022-02-17 15:04:08 +0100

    tests

--- packages/uui-popover/lib/uui-popover.test.ts
+++ packages/uui-popover/lib/uui-popover.test.ts
@@ -16,4 +16,40 @@ describe('UUIPopoverElement', () => {
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
   });
+
+  describe('properties', () => {
+    it('has an useClamp property', () => {
+      expect(element).to.have.property('useClamp');
+    });
+
+    it('has a useAutoPlacement property', () => {
+      expect(element).to.have.property('useAutoPlacement');
+    });
+
+    it('has a margin property', () => {
+      expect(element).to.have.property('margin');
+    });
+
+    it('has a placement property', () => {
+      expect(element).to.have.property('placement');
+    });
+
+    it('has a open property', () => {
+      expect(element).to.have.property('open');
+    });
+  });
+  /*
+  describe('events', () => {
+    describe('close', () => {
+      it('emits a close event when user clicks outside', async () => {
+        const listener = oneEvent(element, 'close');
+        element.open = true;
+        //element.parentElement?.click();
+        const event = await listener;
+        expect(event).to.exist;
+        expect(event.type).to.equal('close');
+      });
+    });
+  });
+*/
 });
