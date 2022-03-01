b8822827d8ca38fd8adfd0c2332c5eaf5fa258ce
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2022-03-01 15:54:36 +0100

    input file

--- packages/uui-input-file/lib/uui-input-file.element.ts
+++ packages/uui-input-file/lib/uui-input-file.element.ts
@@ -1,6 +1,10 @@
 import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
 import { state } from 'lit/decorators.js';
 import { css, html, LitElement } from 'lit';
+import '@umbraco-ui/uui-action-bar/lib';
+import '@umbraco-ui/uui-button/lib';
+import '@umbraco-ui/uui-icon/lib';
+import '@umbraco-ui/uui-icon-registry-essential/lib';
 
 /**
  * @element uui-input-file
@@ -9,36 +13,18 @@ import { css, html, LitElement } from 'lit';
 export class UUIInputFileElement extends LitElement {
   static styles = [
     css`
-      .file-item {
-        display: grid;
-        grid-template-columns: 32px 1fr 32px;
-        border: 1px solid;
+      :host {
+        max-width: 100%;
       }
-
-      .file-item:not(:first-child) {
-        border-top: none;
+      #drop-button {
+        width: 100%;
+        height: 100px;
       }
-
-      .file-item-name,
-      .file-item-type,
-      .file-item-remove {
-        padding: 4px;
+      #files {
+        margin-top: 16px;
         display: flex;
-      }
-
-      .file-item-type,
-      .file-item-remove {
-        align-items: center;
-        justify-content: center;
-      }
-
-      .file-item-remove {
-        cursor: pointer;
-      }
-
-      .file-item-name {
-        border-left: 1px solid black;
-        border-right: 1px solid black;
+        flex-wrap: wrap;
+        gap: 16px;
       }
     `,
   ];
@@ -49,6 +35,29 @@ export class UUIInputFileElement extends LitElement {
   private handleFileDrop(e: CustomEvent) {
     const newFiles = e.detail.files as FileSystemEntry[];
     this.files = [...this.files, ...newFiles];
+
+    const folders: FileSystemEntry[][] = [];
+
+    const sortByFolderCount = this.files.sort((a, b) => this.pathCompare(a, b));
+
+    sortByFolderCount.forEach(file => {
+      const index = file.fullPath.split('/').length - 2;
+
+      if (!folders[index]) {
+        folders[index] = [];
+      }
+
+      folders[index].push(file);
+    });
+
+    console.log('FILES', folders);
+  }
+
+  private pathCompare(a: FileSystemEntry, b: FileSystemEntry) {
+    const aLength = a.fullPath.split('/').length;
+    const bLength = b.fullPath.split('/').length;
+
+    return aLength - bLength;
   }
 
   private removeFile(index: number) {
@@ -58,29 +67,37 @@ export class UUIInputFileElement extends LitElement {
   }
 
   private renderFileItem(file: FileSystemEntry, index: number) {
-    return html`<div class="file-item">
-      <div class="file-item-type">${file.isFile ? 'F' : 'D'}</div>
-      <div class="file-item-name">${file.name}</div>
-      <div
-        class="file-item-remove"
-        @click=${() => this.removeFile(index)}
-        @keydown=${() => ''}>
-        &#10005;
-      </div>
-    </div>`;
+    const name = file.name.split('.')[0];
+    const extension = file.name.split('.')[1];
+
+    return html`<uui-file-preview
+      .name=${name}
+      .extension=${extension}
+      .source=${'https://umbraco.com'}
+      .size=${696969}
+      .isDirectory=${file.isDirectory}>
+      <uui-action-bar slot="actions">
+        <uui-button @click=${() => this.removeFile(index)} look="danger">
+          <uui-icon name="delete"></uui-icon>
+        </uui-button>
+      </uui-action-bar>
+    </uui-file-preview>`;
   }
 
   render() {
     return html`
-      <uui-file-dropzone @file-drop=${this.handleFileDrop}>
-        <uui-button look="placeholder"> Hello throw files here plz </uui-button>
-      </uui-file-dropzone>
-      <div>
-        UPLOADED FILES:
-        ${this.files.map((file: FileSystemEntry, index: number) =>
-          this.renderFileItem(file, index)
-        )}
-      </div>
+      <uui-icon-registry-essential>
+        <uui-file-dropzone @file-drop=${this.handleFileDrop}>
+          <uui-button id="drop-button" look="placeholder">
+            Click to browse or drag files here
+          </uui-button>
+        </uui-file-dropzone>
+        <div id="files">
+          ${this.files.map((file: FileSystemEntry, index: number) =>
+            this.renderFileItem(file, index)
+          )}
+        </div>
+      </uui-icon-registry-essential>
     `;
   }
 }
