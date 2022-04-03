71db5fa2700f161bebf836a25d1b0f6456dff540
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2022-04-03 21:01:59 +0200

    fix PR Issues

--- packages/uui-input-file/lib/uui-input-file.story.ts
+++ packages/uui-input-file/lib/uui-input-file.story.ts
@@ -19,29 +19,56 @@ export default {
 export const AAAOverview: Story = () => html`<uui-input-file></uui-input-file>`;
 AAAOverview.storyName = 'Overview';
 
+const submit = (e: SubmitEvent) => {
+  e.preventDefault();
+  const formElement = e.target as HTMLFormElement;
+  const formData = new FormData(formElement);
+  const data = formData.getAll('input-file');
+  console.log('Files', data);
+};
+
 export const Form: Story = () => {
-  const submit = (e: SubmitEvent) => {
-    e.preventDefault();
-    const formElement = e.target as HTMLFormElement;
-    const formData = new FormData(formElement);
-
-    const data = formData.getAll('input-file');
-    console.log('Files', data);
-  };
-
   return html`
-    <form @submit=${submit} enctype="multipart/form-data">
-      <h2>This is a form</h2>
+    <form @submit=${submit}>
       <div style="display: grid; grid-template-columns: 2fr 1fr">
         <uui-input-file
-          style="height: min-content; max-heigh: 300px"
           name="input-file"
+          style="height: min-content; max-heigh: 300px"
           multiple></uui-input-file>
         <div></div>
       </div>
       <uui-button style="margin-top: 16px" type="submit" look="primary">
-        Upload
+        Submit
       </uui-button>
     </form>
   `;
+};
+
+Form.parameters = {
+  docs: {
+    source: {
+      code: `
+const submit = (e: SubmitEvent) => {
+  e.preventDefault();
+  const formElement = e.target as HTMLFormElement;
+  const formData = new FormData(formElement);
+  const data = formData.getAll('input-file');
+  console.log('Files', data);
+};
+
+<form @submit="submit">
+  <div style="display: grid; grid-template-columns: 2fr 1fr">
+    <uui-input-file
+      name="input-file"
+      style="height: min-content; max-heigh: 300px"
+      multiple></uui-input-file>
+    <div></div>
+  </div>
+  <uui-button style="margin-top: 16px" type="submit" look="primary">
+    Submit
+  </uui-button>
+</form>
+    `,
+    },
+  },
 };
