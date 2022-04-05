02b1edaacf22b57bd6a4ee7ff527903ae39dcdac
Mads Rasmussen <madsr@hey.com>
2022-04-05 14:19:46 +0200

    symbol-file-thumbnail: use story as code example

--- packages/uui-symbol-file-thumbnail/lib/uui-symbol-file-thumbnail.story.ts
+++ packages/uui-symbol-file-thumbnail/lib/uui-symbol-file-thumbnail.story.ts
@@ -7,21 +7,15 @@ export default {
   id: 'uui-symbol-file-thumbnail',
   title: 'Symbols/File Thumbnail',
   component: 'uui-symbol-file-thumbnail',
-  parameters: {
-    docs: {
-      source: {
-        code: `<uui-symbol-file-thumbnail></uui-symbol-file-thumbnail>`,
-      },
-    },
-  },
 };
 
 export const Overview: Story = props =>
   html`<uui-symbol-file-thumbnail
-    .source=${props.source}
-    .alt=${props.alt}></uui-symbol-file-thumbnail>`;
+    style="max-width: 300px;"
+    source=${props.source}
+    alt=${props.alt}></uui-symbol-file-thumbnail>`;
 
 Overview.args = {
-  source: 'https://picsum.photos/2000/3000',
+  source: 'https://picsum.photos/300/200',
   alt: 'Image alt',
 };
