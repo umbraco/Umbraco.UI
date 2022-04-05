3149c0ef033b04fc4901875c366f015d353b8058
Mads Rasmussen <madsr@hey.com>
2022-04-05 13:51:19 +0200

    symbol-file-dropzone: make code examples match stories

--- packages/uui-symbol-file-dropzone/lib/uui-symbol-file-dropzone.story.ts
+++ packages/uui-symbol-file-dropzone/lib/uui-symbol-file-dropzone.story.ts
@@ -7,17 +7,11 @@ export default {
   id: 'uui-symbol-file-dropzone',
   title: 'Symbols/File Dropzone',
   component: 'uui-symbol-file-dropzone',
-  parameters: {
-    docs: {
-      source: {
-        code: `<uui-symbol-file-dropzone></uui-symbol-file-dropzone>`,
-      },
-    },
-  },
 };
 
-export const Overview: Story = () =>
-  html`<uui-symbol-file-dropzone></uui-symbol-file-dropzone>`;
+export const Overview: Story = props =>
+  html`<uui-symbol-file-dropzone
+    ?error=${props.error}></uui-symbol-file-dropzone>`;
 
 export const Default = () =>
   html` <uui-symbol-file-dropzone></uui-symbol-file-dropzone> `;
