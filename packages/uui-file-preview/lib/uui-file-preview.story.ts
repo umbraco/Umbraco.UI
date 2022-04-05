4e25027fca912cfae3f9f32763300bded1518bfb
Mads Rasmussen <madsr@hey.com>
2022-04-05 15:58:28 +0200

    file preview: add stories for file and image

--- packages/uui-file-preview/lib/uui-file-preview.story.ts
+++ packages/uui-file-preview/lib/uui-file-preview.story.ts
@@ -13,30 +13,45 @@ export default {
   component: 'uui-file-preview',
 };
 
-export const Overview: Story = props =>
-  html`
-    <uui-icon-registry-essential>
-      <uui-file-preview
-        name=${props.name}
-        extension=${props.extension}
-        size=${props.size}
-        ?isDirectory=${props.isDirectory}
-        src=${props.src}
-        url="${props.url}">
-        <uui-action-bar slot="actions">
-          <uui-button look="">
-            <uui-icon name="copy"></uui-icon>
-          </uui-button>
-          <uui-button look="danger">
-            <uui-icon name="delete"></uui-icon>
-          </uui-button>
-        </uui-action-bar>
-      </uui-file-preview>
-    </uui-icon-registry-essential>
-  `;
+const Template: Story = props => html`
+  <uui-icon-registry-essential>
+    <uui-file-preview
+      name=${props.name}
+      extension=${props.extension}
+      size=${props.size}
+      ?isDirectory=${props.isDirectory}
+      src=${props.src}
+      url="${props.url}">
+      <uui-action-bar slot="actions">
+        <uui-button look="">
+          <uui-icon name="copy"></uui-icon>
+        </uui-button>
+        <uui-button look="danger">
+          <uui-icon name="delete"></uui-icon>
+        </uui-button>
+      </uui-action-bar>
+    </uui-file-preview>
+  </uui-icon-registry-essential>
+`;
 
-Overview.args = {
+export const AAAOverview = Template.bind({});
+
+AAAOverview.storyName = 'Overview';
+
+AAAOverview.args = {
+  name: 'ThisFileHasAVeryAndIMeanVeryLongName',
+  extension: 'jpg',
+  size: '12376',
+  src: 'https://i.picsum.photos/id/323/300/200.jpg?hmac=BJ1RcgeAgjXdKzYxBF0RBcRyuwapnAUk9K9c465dOKE',
+  url: 'https://i.picsum.photos/id/323/300/200.jpg?hmac=BJ1RcgeAgjXdKzYxBF0RBcRyuwapnAUk9K9c465dOKE',
+};
+
+export const NoImage = Template.bind({});
+
+NoImage.args = {
   name: 'ThisFileHasAVeryAndIMeanVeryLongName',
   extension: 'pdf',
   size: '12376',
+  src: '',
+  url: '',
 };
