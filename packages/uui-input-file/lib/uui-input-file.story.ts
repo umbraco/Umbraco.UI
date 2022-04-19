fdeb5416bfaf88cb90680ec5f0bd88b12ffcf2b8
Mads Rasmussen <madsr@hey.com>
2022-04-19 21:23:20 +0200

    add code examples for input-file single and multiple

--- packages/uui-input-file/lib/uui-input-file.story.ts
+++ packages/uui-input-file/lib/uui-input-file.story.ts
@@ -7,34 +7,6 @@ export default {
   id: 'uui-input-file',
   title: 'Inputs/Files/Input File',
   component: 'uui-input-file',
-  parameters: {
-    docs: {
-      source: {
-        code: `
-const submit = (e: SubmitEvent) => {
-  e.preventDefault();
-  const formElement = e.target as HTMLFormElement;
-  const formData = new FormData(formElement);
-  const data = formData.getAll('input-file');
-  console.log('Files', data);
-};
-
-<form @submit="submit">
-  <div style="display: grid; grid-template-columns: 2fr 1fr">
-    <uui-input-file
-      name="input-file"
-      style="height: min-content; max-heigh: 300px"
-      multiple></uui-input-file>
-    <div></div>
-  </div>
-  <uui-button style="margin-top: 16px" type="submit" look="primary">
-    Submit
-  </uui-button>
-</form>
-            `,
-      },
-    },
-  },
 };
 
 export const AAAOverview: Story = () => html`<uui-input-file></uui-input-file>`;
@@ -56,17 +28,36 @@ const submit = (e: SubmitEvent) => {
 export const Form: Story = () => {
   return html`
     <form @submit=${submit}>
-      <div style="display: grid; grid-template-columns: 2fr 1fr">
-        <uui-input-file
-          name="input-file"
-          style="height: min-content; max-heigh: 300px"
-          multiple>
-        </uui-input-file>
-        <div></div>
-      </div>
+      <uui-input-file name="input-file" multiple> </uui-input-file>
       <uui-button style="margin-top: 16px" type="submit" look="primary">
         Submit
       </uui-button>
     </form>
   `;
+};
+
+Form.parameters = {
+  docs: {
+    source: {
+      code: `
+const submit = (e: SubmitEvent) => {
+  e.preventDefault();
+  const formElement = e.target as HTMLFormElement;
+  const formData = new FormData(formElement);
+  const data = formData.getAll('input-file');
+  console.log('Files', data);
+};
+
+<form @submit="submit">
+    <uui-input-file
+      name="input-file"
+      multiple>
+    </uui-input-file>
+  <uui-button style="margin-top: 16px" type="submit" look="primary">
+    Submit
+  </uui-button>
+</form>
+`,
+    },
+  },
 };
