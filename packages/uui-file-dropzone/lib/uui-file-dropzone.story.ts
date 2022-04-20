0056c7962c06a6cc02348158844065b896e36bfc
Mads Rasmussen <madsr@hey.com>
2022-04-20 12:43:58 +0200

    update dropzone stories to match new api

--- packages/uui-file-dropzone/lib/uui-file-dropzone.story.ts
+++ packages/uui-file-dropzone/lib/uui-file-dropzone.story.ts
@@ -1,93 +1,89 @@
-import '.';
-
 import { Story } from '@storybook/web-components';
 import { html } from 'lit-html';
-import { UUIFileDropzoneElement } from '.';
+import { UUIFileDropzoneEvent } from './UUIFileDropzoneEvents';
+import { UUIFileDropzoneElement } from './uui-file-dropzone.element';
+import '.';
 
 export default {
   id: 'uui-file-dropzone',
   title: 'Inputs/Files/File Dropzone',
   component: 'uui-file-dropzone',
-  parameters: {
-    docs: {
-      source: {
-        code: `
-<uui-file-dropzone @file-drop="e => console.log(e.detail)">
-  <uui-button look="placeholder" style="width: 100px; height: 100px;"></uui-button>
-</uui-file-dropzone>`,
-      },
-    },
-  },
 };
 
+const handleFileChange = (e: UUIFileDropzoneEvent) => console.log(e.detail);
+
 export const AAAOverview: Story = props => {
-  const handler = (e: CustomEvent) => {
-    const dropzone = document.getElementById(
-      'dropzone'
-    ) as UUIFileDropzoneElement;
-    dropzone.browse();
-  };
   return html`
     <uui-file-dropzone
-      id="dropzone"
       ?multiple=${props.multiple}
-      ?directory=${props.directory}
-      .accept=${props.accept}
-      @file-drop=${e => console.log(e.detail)}>
+      accept=${props.accept}
+      @file-change=${handleFileChange}>
       Drop files here
-      <uui-button @click=${handler} look="primary"> Browse </uui-button>
     </uui-file-dropzone>
   `;
 };
 AAAOverview.storyName = 'Overview';
 
-export const MultipleFiles: Story = () =>
+export const Multiple: Story = () =>
   html`
-    <h4>Selected files are logged to the console</h4>
-    <uui-file-dropzone multiple @file-drop=${e => console.log(e.detail)}>
-      <uui-button look="placeholder" style="width: 100px; height: 100px;">
-      </uui-button>
-    </uui-file-dropzone>
+    <uui-file-dropzone multiple @file-change=${handleFileChange}
+      >Drop files here</uui-file-dropzone
+    >
   `;
 
-MultipleFiles.parameters = {
+Multiple.parameters = {
   docs: {
-    source: {
-      code: `
-<uui-file-dropzone multiple @file-drop="e => console.log(e.detail)">
-<uui-button look="placeholder" style="width: 100px; height: 100px;"></uui-button>
-</uui-file-dropzone>`,
-    },
+    storyDescription:
+      'When the multiple attribute is specified, the file input allows the user to select more than one file.',
   },
 };
 
-export const SingleFile: Story = () =>
+export const Accept: Story = () =>
   html`
-    <h4>Selected files are logged to the console</h4>
-    <uui-file-dropzone @file-drop=${e => console.log(e.detail)}>
-      <uui-button look="placeholder" style="width: 100px; height: 100px;">
-      </uui-button>
-    </uui-file-dropzone>
+    <uui-file-dropzone accept="image/*" @file-change=${handleFileChange}
+      >Drop files here</uui-file-dropzone
+    >
   `;
 
-export const WithSymbol: Story = () =>
-  html`
-    <h4>Selected files are logged to the console</h4>
-    <uui-file-dropzone @file-drop=${e => console.log(e.detail)}>
-      <uui-button look="placeholder">
-        <uui-symbol-file-dropzone></uui-symbol-file-dropzone>
-      </uui-button>
-    </uui-file-dropzone>
-  `;
-
-WithSymbol.parameters = {
+Accept.parameters = {
   docs: {
+    storyDescription:
+      'The accept attribute takes as its value a comma-separated list of one or more file types, or unique file type specifiers, describing which file types to allow. See the [MDN docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/accept) for more information.',
+  },
+};
+
+export const BrowseFiles: Story = () => {
+  const handleBrowse = () => {
+    const dropzone = document.getElementById(
+      'browse-dropzone'
+    ) as UUIFileDropzoneElement;
+    dropzone.browse();
+  };
+
+  return html`
+    <uui-file-dropzone id="browse-dropzone" @file-change=${handleFileChange}>
+      Drop files here
+      <uui-button style="margin-top: 9px;" @click=${handleBrowse} look="primary"
+        >Browse</uui-button
+      >
+    </uui-file-dropzone>
+  `;
+};
+
+BrowseFiles.parameters = {
+  docs: {
+    storyDescription:
+      'The browse method allows the user to select a file from their computer.',
     source: {
       code: `
-<uui-file-dropzone @file-drop="e => console.log(e.detail)">
-<uui-button look="placeholder">
-  <uui-symbol-file-dropzone></uui-symbol-file-dropzone>
-</uui-button>
+const handleBrowse = () => {
+  const dropzone = document.getElementById('browse-dropzone');
+  dropzone.browse();
+};
+
+<uui-file-dropzone id="browse-dropzone">
+  Drop files here
+  <uui-button style="margin-top: 9px;" @click="handleBrowse" look="primary">Browse</uui-button>
 </uui-file-dropzone>`,
     },
   },
