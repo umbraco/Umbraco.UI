da7518068e3682ebe7d63c82927433c8ef18b6c6
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2022-04-22 09:47:40 +0200

    added no source story to symbol thumbail

--- packages/uui-symbol-file-thumbnail/lib/uui-symbol-file-thumbnail.story.ts
+++ packages/uui-symbol-file-thumbnail/lib/uui-symbol-file-thumbnail.story.ts
@@ -11,11 +11,22 @@ export default {
 
 export const Overview: Story = props =>
   html`<uui-symbol-file-thumbnail
-    style="max-width: 300px;"
+    style="max-width: 300px; max-height: 300px"
     src=${props.src}
     alt=${props.alt}></uui-symbol-file-thumbnail>`;
 
 Overview.args = {
   src: 'https://picsum.photos/300/200',
+  alt: 'Image alt',
+};
+
+export const NoSource: Story = props =>
+  html`<uui-symbol-file-thumbnail
+    style="max-width: 300px; max-height: 300px"
+    src=${props.src}
+    alt=${props.alt}></uui-symbol-file-thumbnail>`;
+
+NoSource.args = {
+  src: '',
   alt: 'Image alt',
 };
