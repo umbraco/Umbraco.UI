68292d5a0560d39987f5d4012961237049cddbe9
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2022-05-10 15:28:04 +0200

    Add missing events file

---
+++ packages/uui-color-swatches/lib/UUIColorSwatchesEvents.ts
@@ -0,0 +1,7 @@
+import { UUIEvent } from '@umbraco-ui/uui-base/lib/events';
+/*import { UUISliderElement } from './uui-slider.element';
+
+export class UUISliderEvent extends UUIEvent<{}, UUISliderElement> {
+  public static readonly INPUT = 'input';
+  public static readonly CHANGE = 'change';
+}*/
