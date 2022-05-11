892eeaf489a6edf8fbddf3fcf673590aa8f78c90
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2022-05-12 00:03:41 +0200

    Add events to drag slider

--- packages/uui-color-slider/lib/uui-color-slider.element.ts
+++ packages/uui-color-slider/lib/uui-color-slider.element.ts
@@ -4,6 +4,9 @@ import { property } from 'lit/decorators.js';
 
 import { styleMap } from 'lit/directives/style-map.js';
 
+//import { drag } from '../../internal/drag';
+//import { clamp } from '../../internal/math';
+
 /**
  *  @element uui-color-slider
  *  @description 
@@ -19,14 +22,14 @@ export class UUIColorSliderElement extends LitElement {
         display: block;
       }
 
-      .color-picker__slider {
+      .color-slider {
         position: relative;
         height: var(--slider-height);
         border-radius: 3px;
         box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.2);
       }
 
-      .color-picker__slider-handle {
+      .color-slider__handle {
         position: absolute;
         top: calc(50% - var(--slider-handle-size) / 2);
         width: var(--slider-handle-size);
@@ -58,6 +61,9 @@ export class UUIColorSliderElement extends LitElement {
   @property({ type: Number })
   max = 100;
 
+  /** The minimum increment value allowed by the control. */
+  @property({ type: Number }) precision = 1;
+
   /**
   * The orientation of the slider.
   * @type {string}
@@ -75,26 +81,143 @@ export class UUIColorSliderElement extends LitElement {
    @property({ type: String })
    public label!: string;
 
-  @property() value = '';
+  @property() value = 0;
 
   /** Disables the color picker slider. */
   @property({ type: Boolean, reflect: true }) disabled = false;
 
+  handleDrag(event: Event) {
+    const container = this.shadowRoot!.querySelector<HTMLElement>('.color-slider')!;
+    const handle = container.querySelector<HTMLElement>('.color-slider__handle')!;
+    const { width } = container.getBoundingClientRect();
+
+    handle.focus();
+    event.preventDefault();
+
+    this.drag(container, x => {
+      this.value = this.clamp((x / width) * this.max, this.min, this.max);
+      this.syncValues();
+    });
+  }
+
+  handleClick(event: MouseEvent) {
+    console.log("handle click");
+    this.value = this.getValueFromMousePosition(event);
+    this.syncValues();
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    const increment = event.shiftKey ? 10 : 1;
+
+    if (event.key === 'ArrowLeft') {
+      event.preventDefault();
+      this.value = this.clamp(this.value - increment, this.min, this.max);
+      this.syncValues();
+    }
+
+    if (event.key === 'ArrowRight') {
+      event.preventDefault();
+      this.value = this.clamp(this.value + increment, this.min, this.max);
+      this.syncValues();
+    }
+
+    if (event.key === 'Home') {
+      event.preventDefault();
+      this.value = this.min;
+      this.syncValues();
+    }
+
+    if (event.key === 'End') {
+      event.preventDefault();
+      this.value = this.max;
+      this.syncValues();
+    }
+  }
+
+  getValueFromMousePosition(event: MouseEvent) {
+    return this.getValueFromXCoordinate(event.clientX);
+  }
+
+  getValueFromTouchPosition(event: TouchEvent) {
+    return this.getValueFromXCoordinate(event.touches[0].clientX);
+  }
+
+  getValueFromXCoordinate(coordinate: number) {
+    const container = this.shadowRoot!.querySelector<HTMLElement>('.color-slider')!;
+    const containerLeft = container.getBoundingClientRect().left;
+    const containerWidth = container.getBoundingClientRect().width;
+    
+    return this.clamp(
+      this.roundToPrecision(((coordinate - containerLeft) / containerWidth) * this.max, this.precision),
+      this.min,
+      this.max
+    );
+  }
+
+  roundToPrecision(numberToRound: number, precision = 0.5) {
+    const multiplier = 1 / precision;
+    return Math.ceil(numberToRound * multiplier) / multiplier;
+  }
+
+  // Export functon so it can be re-used?
+  clamp(value: number, min: number, max: number) {
+    if (value < min) {
+      return min;
+    }
+    if (value > max) {
+      return max;
+    }
+    return value;
+  }
+
+  // Export functon so it can be re-used?
+  drag(container: HTMLElement, onMove: (x: number, y: number) => void) {
+    function move(pointerEvent: PointerEvent) {
+      const dims = container.getBoundingClientRect();
+      const defaultView = container.ownerDocument.defaultView!;
+      const offsetX = dims.left + defaultView.pageXOffset;
+      const offsetY = dims.top + defaultView.pageYOffset;
+      const x = pointerEvent.pageX - offsetX;
+      const y = pointerEvent.pageY - offsetY;
+  
+      onMove(x, y);
+    }
+  
+    function stop() {
+      document.removeEventListener('pointermove', move);
+      document.removeEventListener('pointerup', stop);
+    }
+  
+    document.addEventListener('pointermove', move, { passive: true });
+    document.addEventListener('pointerup', stop);
+  }
+
+  syncValues() {
+
+  }
+
     render(){
       return html`
-          <div class="color-picker__hue color-picker__slider">
+          <div
+            class="color-slider"
+            @click=${this.handleClick}
+            @mousedown=${this.handleDrag}
+            @touchstart=${this.handleDrag}
+          >
             <span
-              class="color-picker__slider-handle"
+              class="color-slider__handle"
               style=${styleMap({
-                left: `0%` //`${this.hue === 0 ? 0 : 100 / (360 / this.hue)}%`
+                left: `${this.value === 0 ? 0 : 100 / (this.max / this.value)}%`
               })}
               role="slider"
               aria-label="${this.label}"
               aria-orientation="${this.orientation}"
               aria-valuemin="${Math.round(this.min)}"
               aria-valuemax="${Math.round(this.max)}"
+              @keydown=${this.handleKeyDown}
             ></span>
-          </div>`;
+          </div>
+          ${this.value}`;
 
         /*return html`
           <div
