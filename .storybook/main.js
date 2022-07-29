e5b5e29c38184678a90542e97ca1e44a72ea6f42
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2022-07-29 14:59:30 +0200

    no optimize deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,14 +24,14 @@ module.exports = {
       config.plugins.push(tsconfigPaths());
 
       // pre bundle dependencies for faster startup time
-      config.optimizeDeps.include.push('@storybook/web-components');
-      config.optimizeDeps.include.push('lit-html');
-      config.optimizeDeps.include.push('lit');
-      config.optimizeDeps.include.push('lit/decorators.js');
-      config.optimizeDeps.include.push('lit/directives/style-map.js');
-      config.optimizeDeps.include.push('lit/directives/if-defined.js');
-      config.optimizeDeps.include.push('lit/directives/unsafe-html.js');
-      config.optimizeDeps.include.push('element-internals-polyfill');
+      // config.optimizeDeps.include.push('@storybook/web-components');
+      // config.optimizeDeps.include.push('lit-html');
+      // config.optimizeDeps.include.push('lit');
+      // config.optimizeDeps.include.push('lit/decorators.js');
+      // config.optimizeDeps.include.push('lit/directives/style-map.js');
+      // config.optimizeDeps.include.push('lit/directives/if-defined.js');
+      // config.optimizeDeps.include.push('lit/directives/unsafe-html.js');
+      // config.optimizeDeps.include.push('element-internals-polyfill');
     }
 
     return config;
