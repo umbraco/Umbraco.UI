dbd429f0172f9b8cb917e3dd2e610a8e75aa21ea
Jesper Møller Jensen <jmj@umbraco.dk>
2022-08-01 15:44:52 +0200

    remove commented code

--- .storybook/main.js
+++ .storybook/main.js
@@ -23,16 +23,6 @@ module.exports = {
     if (configType === 'DEVELOPMENT') {
       // add plugins
       config.plugins.push(tsconfigPaths());
-
-      // pre bundle dependencies for faster startup time
-      // config.optimizeDeps.include.push('@storybook/web-components');
-      // config.optimizeDeps.include.push('lit-html');
-      // config.optimizeDeps.include.push('lit');
-      // config.optimizeDeps.include.push('lit/decorators.js');
-      // config.optimizeDeps.include.push('lit/directives/style-map.js');
-      // config.optimizeDeps.include.push('lit/directives/if-defined.js');
-      // config.optimizeDeps.include.push('lit/directives/unsafe-html.js');
-      // config.optimizeDeps.include.push('element-internals-polyfill');
     }
 
     return config;
