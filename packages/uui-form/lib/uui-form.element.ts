45a5ed8015b00b39d7d7cdb4a115609c35d6ace2
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2022-08-05 11:07:52 +0200

    submit form on enter

--- packages/uui-form/lib/uui-form.element.ts
+++ packages/uui-form/lib/uui-form.element.ts
@@ -17,6 +17,7 @@ export class UUIFormElement extends LitElement {
     if (this._formElement) {
       this._formElement.removeEventListener('submit', this._onSubmit);
       this._formElement.removeEventListener('reset', this._onReset);
+      this._formElement.removeEventListener('keypress', this._onKeypress);
     }
 
     const formElements = (event.target as HTMLSlotElement)
@@ -28,11 +29,13 @@ export class UUIFormElement extends LitElement {
       this._formElement.setAttribute('novalidate', '');
       this._formElement.addEventListener('submit', this._onSubmit);
       this._formElement.addEventListener('reset', this._onReset);
+      this._formElement.addEventListener('keypress', this._onKeypress);
     }
   }
 
   private _onSubmit(event: SubmitEvent) {
     if (event.target === null) {
+      console.log('target is null');
       return;
     }
     const formNode = event.target as HTMLFormElement;
@@ -51,6 +54,12 @@ export class UUIFormElement extends LitElement {
       return;
     }
     (event.target as HTMLFormElement).removeAttribute('submit-invalid');
+  }
+
+  private _onKeypress(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.dispatchEvent(new SubmitEvent('submit'));
+    }
   }
 
   render() {
