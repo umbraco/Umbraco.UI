4cffcf7a581abf0ed7e19af73a830e7538c5d7ba
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2022-11-22 19:44:40 +0100

    Simplify clamp function

--- packages/uui-base/lib/utils/math.ts
+++ packages/uui-base/lib/utils/math.ts
@@ -3,11 +3,5 @@
 //
 export const clamp = (value: number, min: number, max: number) => 
 {
-    if (value < min) {
-      return min;
-    }
-    if (value > max) {
-      return max;
-    }
-    return value;
+   return Math.min(Math.max(value, min), max);
 };
