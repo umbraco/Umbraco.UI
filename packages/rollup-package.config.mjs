652d22a01f0c9a44bb1daf557f0beaa40acde0a7
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-11-22 11:13:08 +0100

    feat: update minifyhtml

--- packages/rollup-package.config.mjs
+++ packages/rollup-package.config.mjs
@@ -73,7 +73,7 @@ const createBundleConfig = (bundle, namespace) => {
           nodeResolve(),
           importCss(),
           processLitCSSPlugin(),
-          minifyHTML(),
+          minifyHTML.default(),
           esbuild(esbuidOptions),
         ],
       }
