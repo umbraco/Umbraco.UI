5c24128605b16c0f2e5778402ca48b7ebb340ac8
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-01-06 15:43:32 +0100

    uui-color-picker: add support for controls and actions on storybook

--- packages/uui-color-picker/lib/uui-color-picker.story.ts
+++ packages/uui-color-picker/lib/uui-color-picker.story.ts
@@ -1,35 +1,69 @@
 import '.';
 
-import { Story } from '@storybook/web-components';
+import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
+import { UUIColorPickerElement } from './uui-color-picker.element';
+
+const defaultSwatches = [
+  '#d0021b',
+  '#f5a623',
+  '#f8e71c',
+  '#8b572a',
+  '#7ed321',
+  '#417505',
+  '#bd10e0',
+  '#9013fe',
+  '#4a90e2',
+  '#50e3c2',
+  '#b8e986',
+  '#000',
+  '#444',
+  '#888',
+  '#ccc',
+  '#fff',
+];
 
 export default {
   id: 'uui-color-picker',
   title: 'Inputs/Color/Color Picker',
   component: 'uui-color-picker',
+  args: {
+    inline: false,
+    swatches: defaultSwatches,
+  },
   parameters: {
     docs: {
       source: {
         code: `<uui-color-picker></uui-color-picker>`,
       },
     },
+    actions: {
+      handles: ['change'],
+    },
   },
-};
+} as Meta<UUIColorPickerElement>;
 
-export const AAAOverview: Story = () =>
-  html`<uui-color-picker></uui-color-picker>`;
-
-AAAOverview.storyName = 'Overview';
-
-const Template: Story = props => html`
-  <uui-color-picker inline=${props.inline} .value=${props.value}>
+const Template: Story<UUIColorPickerElement> = props => html`
+  <uui-color-picker
+    .inline=${props.inline}
+    .value=${props.value}
+    .format=${props.format}
+    .disabled=${props.disabled}
+    .swatches=${props.swatches}
+    .size=${props.size}
+    .opacity=${props.opacity}
+    .uppercase=${props.uppercase}
+    .name=${props.name}
+    .noFormatToggle=${props.noFormatToggle}>
   </uui-color-picker>
 `;
+
+export const AAAOverview = Template.bind({});
+AAAOverview.storyName = 'Overview';
 
 export const Inline = Template.bind({});
 Inline.args = {
   inline: true,
-  //value: 'blue'
 };
 Inline.parameters = {
   docs: {
