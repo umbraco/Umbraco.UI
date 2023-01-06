b24bed955138ed7ceccd8886a2fd19564609c085
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-01-06 15:13:48 +0100

    uui-color-area: add support for storybook controls

--- packages/uui-color-area/lib/uui-color-area.story.ts
+++ packages/uui-color-area/lib/uui-color-area.story.ts
@@ -1,19 +1,31 @@
 import '.';
 
-import { Story } from '@storybook/web-components';
+import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
+import { UUIColorAreaElement } from './uui-color-area.element';
 
 export default {
   id: 'uui-color-area',
   title: 'Color Area',
   component: 'uui-color-area',
+  argTypes: {
+    value: { control: 'color' },
+  },
   parameters: {
     docs: {
       source: {
         code: `<uui-color-area></uui-color-area>`,
       },
     },
+    actions: {
+      handles: ['change'],
+    },
   },
-};
+} as Meta<UUIColorAreaElement>;
 
-export const Overview: Story = () => html`<uui-color-area></uui-color-area>`;
+const Template: Story<UUIColorAreaElement> = props =>
+  html`<uui-color-area
+    .value=${props.value}
+    .disabled=${props.disabled}></uui-color-area>`;
+
+export const Overview = Template.bind({});
