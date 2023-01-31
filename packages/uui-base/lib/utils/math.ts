747a8019b4f8aafc4406156a3fc07f7f812cf8af
Julia Gru <56249914+julczka@users.noreply.github.com>
2023-01-31 13:07:39 +0100

    add reverse number to math utils

--- packages/uui-base/lib/utils/math.ts
+++ packages/uui-base/lib/utils/math.ts
@@ -4,3 +4,19 @@
 export const clamp = (value: number, min: number, max: number) => {
   return Math.min(Math.max(value, min), max);
 };
+
+/**
+ * Reverses a number within a range
+ *
+ * @param {number} num
+ * @param {number} min
+ * @param {number} max
+ * @return {*}  {number}
+ */
+export const reverseNumberInRange = (
+  num: number,
+  min: number,
+  max: number
+): number => {
+  return max + min - num;
+};
