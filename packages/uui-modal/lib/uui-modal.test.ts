9abfc15e61c481dff6eb4c85967cf3df995d8aeb
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-03-17 21:21:46 +1300

    fix modal tests

--- packages/uui-modal/lib/uui-modal.test.ts
+++ packages/uui-modal/lib/uui-modal.test.ts
@@ -1,15 +1,53 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { UUIModalElement } from './uui-modal.element';
+import {
+  UUIModalDialogElement,
+  UUIModalSidebarElement,
+  UUIModalContainerElement,
+} from '.';
 
-describe('UUIModalElement', () => {
-  let element: UUIModalElement;
+describe('UUIModalContainerElement', () => {
+  let element: UUIModalContainerElement;
 
   beforeEach(async () => {
-    element = await fixture(html` <uui-modal></uui-modal> `);
+    element = await fixture(
+      html` <uui-modal-container></uui-modal-container> `
+    );
   });
 
   it('is defined with its own instance', () => {
-    expect(element).to.be.instanceOf(UUIModalElement);
+    expect(element).to.be.instanceOf(UUIModalContainerElement);
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UUIModalDialogElement', () => {
+  let element: UUIModalDialogElement;
+
+  beforeEach(async () => {
+    element = await fixture(html` <uui-modal-dialog></uui-modal-dialog> `);
+  });
+
+  it('is defined with its own instance', () => {
+    expect(element).to.be.instanceOf(UUIModalDialogElement);
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UUIModalSidebarElement', () => {
+  let element: UUIModalSidebarElement;
+
+  beforeEach(async () => {
+    element = await fixture(html` <uui-modal-sidebar></uui-modal-sidebar> `);
+  });
+
+  it('is defined with its own instance', () => {
+    expect(element).to.be.instanceOf(UUIModalSidebarElement);
   });
 
   it('passes the a11y audit', async () => {
