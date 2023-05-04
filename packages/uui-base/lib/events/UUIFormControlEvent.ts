42c21de7ff7aeae6ee960f3eb824089196209eec
Niels Lyngsø <nsl@umbraco.dk>
2023-05-04 10:53:16 +0200

    UUIFormControlEvent not composed

--- packages/uui-base/lib/events/UUIFormControlEvent.ts
+++ packages/uui-base/lib/events/UUIFormControlEvent.ts
@@ -7,7 +7,7 @@ export class UUIFormControlEvent extends UUIEvent<
 > {
   constructor(evName: string, eventInit: any | null = {}) {
     super(evName, {
-      ...{ bubbles: true, composed: true },
+      ...{ bubbles: true },
       ...eventInit,
     });
   }
