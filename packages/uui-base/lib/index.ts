28a28a924f7c63da896ca87c477b52c1e33d8872
Niels Lyngsø <niels.lyngso@gmail.com>
2023-05-23 14:48:21 +0200

    Correct import paths (#496)

--- packages/uui-base/lib/index.ts
+++ packages/uui-base/lib/index.ts
@@ -1,2 +1,9 @@
 import 'lit';
 import 'lit/decorators.js';
+
+export * from './animations';
+export * from './events';
+export * from './mixins';
+export * from './registration';
+export * from './types';
+export * from './utils';
