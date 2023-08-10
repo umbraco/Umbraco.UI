b9f3b909286129b4abe3869c9764bfa67937d665
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-08-10 13:20:18 +0200

    build: run storybook 7.2.2 migrations

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from 'path';
 const remarkGfm = require('remark-gfm').default;
 const tsconfigPaths = require('vite-tsconfig-paths').default;
 
@@ -14,10 +15,10 @@ module.exports = {
     '../stories/**/*.mdx',
   ],
   addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    '../storyhelpers/storybook-readme',
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('../storyhelpers/storybook-readme'),
     {
       name: '@storybook/addon-docs',
       options: {
@@ -30,7 +31,7 @@ module.exports = {
     },
   ],
   framework: {
-    name: '@storybook/web-components-vite',
+    name: getAbsolutePath('@storybook/web-components-vite'),
     options: {},
   },
   features: {
@@ -54,3 +55,7 @@ module.exports = {
     autodocs: true,
   },
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
