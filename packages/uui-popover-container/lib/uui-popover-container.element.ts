f458b4fc0131c2b30763ad2ad3ae2a48af57ca54
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-08-24 16:41:21 +1200

    WIP placement

--- packages/uui-popover-container/lib/uui-popover-container.element.ts
+++ packages/uui-popover-container/lib/uui-popover-container.element.ts
@@ -1,11 +1,52 @@
 import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
 import { css, html, LitElement } from 'lit';
+import { property, state } from 'lit/decorators.js';
+
+export type PopoverContainerPlacement =
+  | 'auto'
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end';
 
 /**
  * @element uui-popover-container
  */
 @defineElement('uui-popover-container')
 export class UUIPopoverContainerElement extends LitElement {
+  /**
+   * Set the distance between popover-modal and trigger.
+   * @type {number}
+   * @attr disabled
+   * @default false
+   */
+  @property({ type: Number })
+  margin = 0;
+
+  /**
+   * Define the placement of the popover-modal.
+   * @attr placement
+   * @default 'bottom-start'
+   */
+  @property({ type: String, reflect: true })
+  get placement(): PopoverContainerPlacement {
+    return this._placement;
+  }
+  set placement(newValue: PopoverContainerPlacement) {
+    this._placement = newValue;
+  }
+
+  @state()
+  _placement: PopoverContainerPlacement = 'bottom-start';
+
   #target: HTMLElement | null = null;
 
   connectedCallback(): void {
@@ -22,7 +63,7 @@ export class UUIPopoverContainerElement extends LitElement {
     document.removeEventListener('scroll', this.#updatePosition);
   }
 
-  #beforeToggle = (event: any) => {
+  #beforeToggle = async (event: any) => {
     if (event.newState !== 'open') return;
 
     this.id;
@@ -31,17 +72,54 @@ export class UUIPopoverContainerElement extends LitElement {
       'popovertarget',
       this.id
     );
-    this.#updatePosition();
+
+    requestAnimationFrame(() => {
+      this.#updatePosition();
+    });
   };
 
   #updatePosition = () => {
     if (this.#target === null) return;
 
-    const targetRect = this.#target.getBoundingClientRect();
-    // const popoverRect = this.getBoundingClientRect();
+    const isTopPlacement = this.placement.indexOf('top') !== -1;
+    const isBottomPlacement = this.placement.indexOf('bottom') !== -1;
+    const isLeftPlacement = this.placement.indexOf('left') !== -1;
+    const isRightPlacement = this.placement.indexOf('right') !== -1;
 
-    const top = targetRect.top + targetRect.height;
-    const left = targetRect.left;
+    const isStart = this.placement.indexOf('-start') !== -1;
+    const isEnd = this.placement.indexOf('-end') !== -1;
+
+    const targetRect = this.#target.getBoundingClientRect();
+    const popoverRect = this.getBoundingClientRect();
+
+    let top = 0;
+    let left = 0;
+
+    const verticalOffset = isStart
+      ? 0
+      : isEnd
+      ? targetRect.height - popoverRect.height
+      : (targetRect.height - popoverRect.height) / 2;
+
+    const horizontalOffset = isStart
+      ? 0
+      : isEnd
+      ? targetRect.width - popoverRect.width
+      : (targetRect.width - popoverRect.width) / 2;
+
+    if (isTopPlacement) {
+      top = targetRect.top - popoverRect.height - verticalOffset;
+      left = targetRect.left + horizontalOffset;
+    } else if (isBottomPlacement) {
+      top = targetRect.top + targetRect.height + verticalOffset;
+      left = targetRect.left + horizontalOffset;
+    } else if (isLeftPlacement) {
+      top = targetRect.top + verticalOffset;
+      left = targetRect.left - popoverRect.width - horizontalOffset;
+    } else if (isRightPlacement) {
+      top = targetRect.top + verticalOffset;
+      left = targetRect.left + targetRect.width + horizontalOffset;
+    }
 
     this.style.transform = `translate(${left}px, ${top}px)`;
   };
@@ -72,18 +150,52 @@ export class UUIPopoverContainerElement extends LitElement {
   }
 
   render() {
-    return html` Markup goes here `;
+    const array: PopoverContainerPlacement[] = [
+      'auto',
+      'top',
+      'top-start',
+      'top-end',
+      'bottom',
+      'bottom-start',
+      'bottom-end',
+      'right',
+      'right-start',
+      'right-end',
+      'left',
+      'left-start',
+      'left-end',
+    ];
+
+    return html`
+      <!-- Debug buttons to test placement -->
+      <div style="position: absolute">
+        ${array.map(
+          placement => html`
+            <button
+              @click=${() => {
+                this.placement = placement as PopoverContainerPlacement;
+                this.#updatePosition();
+              }}>
+              ${placement}
+            </button>
+          `
+        )}
+      </div>
+      <slot></slot>
+    `;
   }
 
   static styles = [
     css`
       :host {
         margin: 0;
-        border: 1px solid var(--uui-color-border);
-        border-radius: var(--uui-border-radius);
-        padding: var(--uui-size-space-2);
-        background-color: var(--uui-color-surface);
-        box-shadow: var(--uui-shadow-depth-3);
+        width: fit-content;
+        height: fit-content;
+        border: none;
+        border-radius: 0;
+        padding: 0;
+        background-color: none;
+        background: none;
       }
     `,
   ];
--- packages/uui-popover-container/lib/uui-popover-container.story.ts
+++ packages/uui-popover-container/lib/uui-popover-container.story.ts
@@ -24,79 +24,23 @@ type Story = StoryObj<UUIPopoverContainerElement>;
 
 export const Overview: Story = {
   render: () => html`
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <button id="popover-button" popovertarget="popover-container">
+    <button
+      id="popover-button"
+      popovertarget="popover-container"
+      style="margin: 2000px">
       open popover
     </button>
-    <div>Whatever</div>
-    <uui-popover-container
-      id="popover-container"
-      popover></uui-popover-container>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur
-      voluptate nihil iste praesentium nesciunt reiciendis voluptates? Dolore
-      quis animi soluta perspiciatis, debitis tempore, harum qui hic ea
-      necessitatibus molestias!
-    </div>
+    <uui-popover-container id="popover-container" popover>
+      <div
+        style="max-width: 300px; background: white; border: 1px solid black; border-radius: 4px;">
+        <h2>This is my popover</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem,
+          est. Eius dolorem dolorum praesentium ut impedit recusandae at maiores
+          adipisci nulla atque, est consectetur modi porro blanditiis expedita
+          rem dolore.
+        </p>
+      </div>
+    </uui-popover-container>
   `,
 };
