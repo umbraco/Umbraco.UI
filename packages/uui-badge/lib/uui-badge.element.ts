5c4064c9c14fbae5c569446cc0549ebb3cb74aed
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2024-01-22 14:07:37 +0100

    prefix interface types with UUI

--- packages/uui-badge/lib/uui-badge.element.ts
+++ packages/uui-badge/lib/uui-badge.element.ts
@@ -2,7 +2,7 @@ import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
 import { css, html, LitElement } from 'lit';
 import { property } from 'lit/decorators.js';
 import type {
-  InterfaceColor,
+  UUIInterfaceColor,
   InterfaceLook,
 } from '@umbraco-ui/uui-base/lib/types';
 
@@ -21,7 +21,7 @@ export class UUIBadgeElement extends LitElement {
    * @default "default"
    */
   @property({ reflect: true })
-  color: InterfaceColor = 'default';
+  color: UUIInterfaceColor = 'default';
 
   /**
    * Changes the look of the button to one of the predefined, symbolic looks. For example - set this to positive if you want nice, green "confirm" button.
