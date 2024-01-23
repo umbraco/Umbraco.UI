656f1811a1de862c7a7f6ca0221bf14e3532d53c
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-01-23 14:44:50 +0100

    fix: uui-popover container scroll event (#713)
    * look for scroll parent and listen for scroll
    
    * update story
    
    * make eventlisteners passive and remove in disconnecedCallback

--- packages/uui-popover-container/lib/uui-popover-container.story.ts
+++ packages/uui-popover-container/lib/uui-popover-container.story.ts
@@ -158,3 +158,61 @@ export const Tooltip: Story = {
     </uui-popover-container>
   `,
 };
+
+export const InsideScrollContainer: Story = {
+  args: {
+    placement: 'bottom-start',
+    margin: 0,
+  },
+  argTypes: {
+    open: {
+      control: false,
+    },
+    placement: {
+      options: [
+        'auto',
+        'top',
+        'top-start',
+        'top-end',
+        'bottom',
+        'bottom-start',
+        'bottom-end',
+        'right',
+        'right-start',
+        'right-end',
+        'left',
+        'left-start',
+        'left-end',
+      ],
+    },
+  },
+  render: args => html`
+    <div style="height: 500px; overflow: auto; outline: 1px solid black">
+      <div
+        style="width: 300px; height: 300px; outline: 1px solid black; overflow: auto;">
+        <div style="height: 150px"></div>
+        <uui-button
+          id="popover-button"
+          popovertarget="popover-container"
+          look="primary"
+          label="Open popover">
+          Open popover
+        </uui-button>
+        <div style="height: 150px"></div>
+        <div style="height: 150px"></div>
+      </div>
+      <div style="height: 400px"></div>
+    </div>
+    <uui-popover-container
+      id="popover-container"
+      popover
+      placement=${args.placement}
+      margin=${args.margin}>
+      <div
+        style="width: 100%; background-color: var(--uui-color-surface); max-width: 200px; box-shadow: var(--uui-shadow-depth-4); padding: var(--uui-size-space-4); border-radius: var(--uui-border-radius); font-size: 0.9rem;">
+        <h3>Scroll!</h3>
+        Scrolling in any of the 2 boxes should trigger an update
+      </div>
+    </uui-popover-container>
+  `,
+};
