81dcf3ea1dd4e6930b1ef1446e181e48f631bf92
Niels Lyngsø <niels.lyngso@gmail.com>
2024-04-17 15:51:28 +0200

    ts ignorer for updated

--- packages/uui-base/lib/mixins/FormControlMixin.ts
+++ packages/uui-base/lib/mixins/FormControlMixin.ts
@@ -385,7 +385,8 @@ export const UUIFormControlMixin = <
     }
 
     updated(changedProperties: Map<string | number | symbol, unknown>) {
-      super.updated(changedProperties);
+      // @ts-expect-error We don't know if we are extending a lit-element:
+      super.updated?.(changedProperties);
       this._runValidators();
     }
 
