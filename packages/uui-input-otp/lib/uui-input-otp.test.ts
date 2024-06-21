5f6023d6c9b4a7510a86ccbc2cac6e396dc4c131
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-06-21 15:59:24 +0200

    test: add tests for element

--- packages/uui-input-otp/lib/uui-input-otp.test.ts
+++ packages/uui-input-otp/lib/uui-input-otp.test.ts
@@ -17,4 +17,136 @@ describe('UUIInputOtpElement', () => {
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
   });
+
+  describe('properties', () => {
+    it('has a default length of 6', () => {
+      expect(element.length).to.equal(6);
+      expect(element.shadowRoot?.querySelectorAll('input').length).to.equal(6);
+    });
+
+    it('can set the length', async () => {
+      element.length = 4;
+      await element.updateComplete;
+      expect(element.shadowRoot?.querySelectorAll('input').length).to.equal(4);
+    });
+
+    it('can set integerOnly', async () => {
+      element.integerOnly = true;
+      await element.updateComplete;
+      expect(element.inputMode).to.equal('numeric');
+
+      element.integerOnly = false;
+      await element.updateComplete;
+      expect(element.inputMode).to.equal('text');
+    });
+
+    it('can set masked', async () => {
+      element.masked = true;
+      await element.updateComplete;
+      expect(element._input).to.equal('password');
+
+      element.masked = false;
+      await element.updateComplete;
+      expect(element._input).to.equal('text');
+    });
+
+    it('can set readonly', async () => {
+      element.readonly = true;
+      await element.updateComplete;
+      expect(element.hasAttribute('readonly')).to.be.true;
+
+      element.readonly = false;
+      await element.updateComplete;
+      expect(element.hasAttribute('readonly')).to.be.false;
+    });
+
+    it('can set disabled', async () => {
+      element.disabled = true;
+      await element.updateComplete;
+      expect(element.hasAttribute('disabled')).to.be.true;
+
+      element.disabled = false;
+      await element.updateComplete;
+      expect(element.hasAttribute('disabled')).to.be.false;
+    });
+
+    it('can set autofocus', async () => {
+      element.autofocus = true;
+      await element.updateComplete;
+      expect(element.hasAttribute('autofocus')).to.be.true;
+
+      element.autofocus = false;
+      await element.updateComplete;
+      expect(element.hasAttribute('autofocus')).to.be.false;
+    });
+
+    it('can set required', async () => {
+      element.required = true;
+      await element.updateComplete;
+      expect(element.hasAttribute('required')).to.be.true;
+
+      element.required = false;
+      await element.updateComplete;
+      expect(element.hasAttribute('required')).to.be.false;
+    });
+
+    it('can set error', async () => {
+      element.error = true;
+      await element.updateComplete;
+      expect(element.hasAttribute('error')).to.be.true;
+
+      element.error = false;
+      await element.updateComplete;
+      expect(element.hasAttribute('error')).to.be.false;
+    });
+
+    it('can set autocomplete', async () => {
+      element.autocomplete = 'one-time-code';
+      await element.updateComplete;
+      expect(element.getAttribute('autocomplete')).to.equal('one-time-code');
+    });
+  });
+
+  describe('logic', () => {
+    it('can distribute a value', async () => {
+      element.value = '123456';
+      await element.updateComplete;
+      expect(element.shadowRoot?.querySelectorAll('input')[0].value).to.equal(
+        '1',
+      );
+      expect(element.shadowRoot?.querySelectorAll('input')[1].value).to.equal(
+        '2',
+      );
+      expect(element.shadowRoot?.querySelectorAll('input')[2].value).to.equal(
+        '3',
+      );
+      expect(element.shadowRoot?.querySelectorAll('input')[3].value).to.equal(
+        '4',
+      );
+      expect(element.shadowRoot?.querySelectorAll('input')[4].value).to.equal(
+        '5',
+      );
+      expect(element.shadowRoot?.querySelectorAll('input')[5].value).to.equal(
+        '6',
+      );
+    });
+
+    it('can distribute a value with a different length', async () => {
+      element.length = 4;
+      element.value = '123456';
+      await element.updateComplete;
+      expect(element.shadowRoot?.querySelectorAll('input')[0].value).to.equal(
+        '1',
+      );
+      expect(element.shadowRoot?.querySelectorAll('input')[1].value).to.equal(
+        '2',
+      );
+      expect(element.shadowRoot?.querySelectorAll('input')[2].value).to.equal(
+        '3',
+      );
+      expect(element.shadowRoot?.querySelectorAll('input')[3].value).to.equal(
+        '4',
+      );
+    });
+  });
 });
