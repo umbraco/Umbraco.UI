42b6b1f4e7aad27472f588c626b52aa9121d81ad
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2024-07-17 21:22:50 +0200

    Remove output

--- packages/uui-checkbox/lib/uui-checkbox.element.ts
+++ packages/uui-checkbox/lib/uui-checkbox.element.ts
@@ -25,7 +25,6 @@ export class UUICheckboxElement extends UUIBooleanInputElement {
   renderCheckbox() {
     return html`
       <div id="ticker">
-      ${this.indeterminate}
         <div id="icon-check">${this.indeterminate ? iconMinus : iconCheck}</div>
       </div>
     `;
