06272d31f7f3fa7aa6dce2d701804b354c13e287
Niels Lyngsø <niels.lyngso@gmail.com>
2024-08-09 10:35:47 +0200

    white space correction

--- packages/uui-base/lib/mixins/FormControlMixin.ts
+++ packages/uui-base/lib/mixins/FormControlMixin.ts
@@ -313,11 +313,14 @@ export const UUIFormControlMixin = <
         checkMethod: checkMethod,
       };
       this.#validators.push(validator);
-			// Sort validators based on the WeightedErrorFlagTypes order. [NL]
-			this.#validators.sort((a, b) => {
-				// This could easily be extended with a weight set on the validator object itself. [NL]
-				return WeightedErrorFlagTypes.indexOf(a.flagKey) - WeightedErrorFlagTypes.indexOf(b.flagKey);
-			});
+      // Sort validators based on the WeightedErrorFlagTypes order. [NL]
+      this.#validators.sort((a, b) => {
+        // This could easily be extended with a weight set on the validator object itself. [NL]
+        return (
+          WeightedErrorFlagTypes.indexOf(a.flagKey) -
+          WeightedErrorFlagTypes.indexOf(b.flagKey)
+        );
+      });
       return validator;
     }
 
@@ -382,55 +385,55 @@ export const UUIFormControlMixin = <
      * Such are mainly properties that are not declared as a Lit state and or Lit property.
      */
     protected _runValidators() {
-			this.#validity = {};
-			//const messages: Set<string> = new Set();
-			let message: string | undefined = undefined;
-			let innerFormControlEl: NativeFormControlElement | undefined = undefined;
+      this.#validity = {};
+      //const messages: Set<string> = new Set();
+      let message: string | undefined = undefined;
+      let innerFormControlEl: NativeFormControlElement | undefined = undefined;
 
-			// Loop through custom validators, currently its intentional to have them overwritten native validity. but might need to be reconsidered (This current way enables to overwrite with custom messages) [NL]
-			this.#validators.some((validator) => {
-				if (validator.checkMethod()) {
-					this.#validity[validator.flagKey] = true;
-					//messages.add(validator.getMessageMethod());
-					message = validator.getMessageMethod();
-					return true;
-				}
-				return false;
-			});
+      // Loop through custom validators, currently its intentional to have them overwritten native validity. but might need to be reconsidered (This current way enables to overwrite with custom messages) [NL]
+      this.#validators.some(validator => {
+        if (validator.checkMethod()) {
+          this.#validity[validator.flagKey] = true;
+          //messages.add(validator.getMessageMethod());
+          message = validator.getMessageMethod();
+          return true;
+        }
+        return false;
+      });
 
-			if (message) {
-				// Loop through inner native form controls to adapt their validityState. [NL]
-				this.#formCtrlElements.some((formCtrlEl) => {
-					let key: keyof ValidityState;
-					for (key in formCtrlEl.validity) {
-						if (key !== 'valid' && formCtrlEl.validity[key]) {
-							this.#validity[key] = true;
-							//messages.add(formCtrlEl.validationMessage);
-							message = formCtrlEl.validationMessage;
-							innerFormControlEl ??= formCtrlEl;
-							return true;
-						}
-					}
-					return false;
-				});
-			}
+      if (message) {
+        // Loop through inner native form controls to adapt their validityState. [NL]
+        this.#formCtrlElements.some(formCtrlEl => {
+          let key: keyof ValidityState;
+          for (key in formCtrlEl.validity) {
+            if (key !== 'valid' && formCtrlEl.validity[key]) {
+              this.#validity[key] = true;
+              //messages.add(formCtrlEl.validationMessage);
+              message = formCtrlEl.validationMessage;
+              innerFormControlEl ??= formCtrlEl;
+              return true;
+            }
+          }
+          return false;
+        });
+      }
 
-			const hasError = Object.values(this.#validity).includes(true);
+      const hasError = Object.values(this.#validity).includes(true);
 
-			// https://developer.mozilla.org/en-US/docs/Web/API/ValidityState#valid
-			this.#validity.valid = !hasError;
+      // https://developer.mozilla.org/en-US/docs/Web/API/ValidityState#valid
+      this.#validity.valid = !hasError;
 
-			// Transfer the new validityState to the ElementInternals. [NL]
-			this._internals.setValidity(
-				this.#validity,
-				// Turn messages into an array and join them with a comma. [NL]:
-				//[...messages].join(', '),
-				message,
-				innerFormControlEl ?? this.getFormElement() ?? undefined,
-			);
+      // Transfer the new validityState to the ElementInternals. [NL]
+      this._internals.setValidity(
+        this.#validity,
+        // Turn messages into an array and join them with a comma. [NL]:
+        //[...messages].join(', '),
+        message,
+        innerFormControlEl ?? this.getFormElement() ?? undefined,
+      );
 
-			this.#dispatchValidationState();
-		}
+      this.#dispatchValidationState();
+    }
 
     #dispatchValidationState() {
       // Do not fire validation events unless we are not pristine/'untouched'/not-in-validation-mode. [NL]
