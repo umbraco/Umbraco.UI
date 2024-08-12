9ff519c2469aea4691b7d1cb5b5122e5beb273d9
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-12 16:25:09 +0200

    fix story paths

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,8 +11,8 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    // '../packages/**/*.mdx',
+    '../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
