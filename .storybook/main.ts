13c5331b203d767855ccf323bb6fff03d2a1e54d
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-13 11:23:53 +0200

    cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
-
 import { join, dirname } from 'path';
 
 /**
@@ -9,6 +8,7 @@ import { join, dirname } from 'path';
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config: StorybookConfig = {
   stories: [
     // '../packages/**/*.mdx',
