2a8ad17d3dbed5bb9276b44b36183bdba63cab79
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-20 14:47:19 +0200

    avatar group

---
+++ packages/uui-avatar-group/lib/uui-avatar-group.stories.ts
@@ -0,0 +1,50 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers/spread-directive';
+
+import '@umbraco-ui/uui-badge/lib';
+
+const meta: Meta = {
+  component: 'uui-avatar-group',
+  title: 'Displays/Avatar/Avatar Group',
+  args: {
+    '--uui-avatar-border-color': '#ffffff',
+  },
+  argTypes: {
+    '--uui-avatar-border-color': { control: { type: 'color' } },
+  },
+  render: args => {
+    return html`<uui-avatar-group
+      style="font-size: ${args['font-size']};"
+      ${spread(args)}>
+      <uui-avatar name="Mads Rasmussen"></uui-avatar>
+      <uui-avatar name="Niels Lyngsø"></uui-avatar>
+      <uui-avatar name="Jacob Overgaard"></uui-avatar>
+      <uui-avatar name="Jesper Møller Jensen"></uui-avatar>
+    </uui-avatar-group>`;
+  },
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
+
+export const Sizing: Story = {
+  args: {
+    'font-size': '32px',
+  },
+};
+
+export const Border: Story = {
+  args: {
+    '--uui-avatar-border-color': '#000000',
+  },
+};
+
+export const Limit: Story = {
+  args: {
+    limit: 3,
+  },
+};
