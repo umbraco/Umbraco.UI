44f6704405905dbf3cbe966afe900d24947814c4
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-23 12:54:16 +0200

    go back to old component analyzer

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -37,6 +37,55 @@ WebComponentFormatter(customElements);
 setCustomElementsManifest(customElements);
 
 function WebComponentFormatter(customElements) {
+  for (let tag of customElements.tags || []) {
+    // Hide all attributes, since we only use props for storybook
+    tag.attributes = [];
+
+    // Hide all 'styles' and 'formAssociated' entries for properties
+    for (let prop in tag.properties || []) {
+      if (
+        tag.properties[prop].name === 'styles' ||
+        tag.properties[prop].name === 'formAssociated'
+      ) {
+        delete tag.properties[prop];
+      }
+    }
+
+    // Run through all CSS Custom Properties and clean them a bit
+    for (let cssProp of tag.cssProperties || []) {
+      // If the property does not have a type, set it to string
+      if (!cssProp.type) {
+        cssProp.type = 'string';
+      }
+    }
+
+    // Find all names of properties
+    const propertyNames = (tag.properties || []).map(p => p.name);
+
+    // Run through all slots to clean them up a bit
+    for (let slot of tag.slots || []) {
+      // Replace the name of the default slot so Storybook will show it
+      if (typeof slot.name === 'string' && slot.name.length === 0) {
+        slot.name = 'slot';
+      }
+
+      // If the slot has the same name as a property, then add the word 'slot' to the name
+      // Bug reported to Storybook here: https://github.com/storybookjs/storybook/issues/17733
+      if (propertyNames.includes(slot.name)) {
+        slot.name = `${slot.name} slot`;
+      }
+
+      // Set type of slots
+      if (typeof slot.type === 'undefined' || slot.type.length === 0) {
+        slot.type = 'HTMLElement';
+      }
+    }
+  }
+
+  return customElements;
+}
+
+function WebComponentFormatterNew(customElements) {
   (customElements.modules ?? [])
     .flatMap(module => module.declarations ?? [])
     .forEach(declaration => {
