b2e9d977cf97a8f28ca47ce29bfe5591e47d4fe7
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-23 11:05:08 +0200

    breadcrumbs

---
+++ packages/uui-breadcrumbs/lib/uui-breadcrumbs.stories.ts
@@ -0,0 +1,21 @@
+import '.';
+import { html, nothing } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers/spread-directive';
+
+const meta: Meta = {
+  id: 'uui-breadcrumbs',
+  component: 'uui-breadcrumbs',
+  title: 'Buttons/Breadcrumbs',
+  render: () =>
+    html`<uui-breadcrumbs>
+      <uui-breadcrumb-item href="#Home">Home</uui-breadcrumb-item>
+      <uui-breadcrumb-item href="#Products">Products</uui-breadcrumb-item>
+      <uui-breadcrumb-item href="#Cars">Cars</uui-breadcrumb-item>
+    </uui-breadcrumbs>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
