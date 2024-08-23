74dded767691ac4cb30734078996c45fb05f8a12
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-23 12:48:43 +0200

    uui card wip

---
+++ packages/uui-card/lib/uui-card.stories.ts
@@ -0,0 +1,29 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers/spread-directive';
+
+const meta: Meta = {
+  id: 'uui-card',
+  component: 'uui-card',
+  title: 'Displays/Cards/Card',
+};
+
+export default meta;
+type Story = StoryObj;
+
+/**
+ * Card is a Component that provides the basics for a Card component. This can be extended in code to match a certain need.
+ */
+export const Default: Story = {
+  render: args => html`
+    <uui-card ${spread(args)} selectable>
+      <div>
+        <p>
+          This is an example of a simple card. It can be used to display
+          information in a structured way.
+        </p>
+      </div>
+    </uui-card>
+  `,
+};
