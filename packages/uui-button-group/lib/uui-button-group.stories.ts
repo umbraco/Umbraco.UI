8673813c334fc4886b396cc02cab363173b99567
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-23 11:11:50 +0200

    button group

---
+++ packages/uui-button-group/lib/uui-button-group.stories.ts
@@ -0,0 +1,23 @@
+import '.';
+import { html, nothing } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers/spread-directive';
+
+const meta: Meta = {
+  id: 'uui-button-group',
+  component: 'uui-button-group',
+  title: 'Buttons/Button Group',
+  render: () => html`
+    <uui-button-group>
+      <uui-button look="primary" label="Button 1"></uui-button>
+      <uui-button look="primary" label="Button 2"></uui-button>
+      <uui-button look="secondary" color="positive" label="Save"></uui-button>
+      <uui-button look="primary" color="danger" label="Delete"></uui-button>
+    </uui-button-group>
+  `,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
