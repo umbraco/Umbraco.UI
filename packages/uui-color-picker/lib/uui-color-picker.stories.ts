38b378b8f8b266aa728c227eaf16105bd05e5f40
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-29 12:17:25 +0200

    color picker

---
+++ packages/uui-color-picker/lib/uui-color-picker.stories.ts
@@ -0,0 +1,72 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers';
+
+import '@umbraco-ui/uui-button-group/lib';
+import '@umbraco-ui/uui-button/lib';
+import '@umbraco-ui/uui-color-area/lib';
+import '@umbraco-ui/uui-color-slider/lib';
+import '@umbraco-ui/uui-color-swatch/lib';
+import '@umbraco-ui/uui-color-swatches/lib';
+import '@umbraco-ui/uui-icon/lib';
+import '@umbraco-ui/uui-input/lib';
+import '@umbraco-ui/uui-popover-container/lib';
+
+const formats = ['hex', 'rgb', 'hsl', 'hsv'];
+const sizes = ['small', 'medium', 'large'];
+
+const meta: Meta = {
+  id: 'uui-color-picker',
+  component: 'uui-color-picker',
+  title: 'Inputs/Color/Color Picker',
+  argTypes: {
+    format: {
+      options: formats,
+    },
+    size: {
+      options: sizes,
+    },
+    value: {
+      control: { type: 'color' },
+    },
+  },
+  render: args => html`<uui-color-picker ${spread(args)}></uui-color-picker>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {
+  args: {
+    value: '#000000',
+  },
+};
+
+export const Empty: Story = {};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Inline: Story = {
+  args: {
+    inline: true,
+  },
+};
+
+export const Opacity: Story = {
+  args: {
+    opacity: true,
+    inline: true,
+  },
+};
+
+export const Formats: Story = {
+  args: {
+    format: 'hex',
+    inline: true,
+  },
+};
