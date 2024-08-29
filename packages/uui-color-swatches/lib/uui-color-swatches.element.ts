78d1b99f99e0e6272f1914e7c0197aa11933af92
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-29 13:18:02 +0200

    color swatches

--- packages/uui-color-swatches/lib/uui-color-swatches.element.ts
+++ packages/uui-color-swatches/lib/uui-color-swatches.element.ts
@@ -10,7 +10,7 @@ import { UUIColorSwatchesEvent } from './UUIColorSwatchesEvent';
 //TODO maybe implement multiple selection
 
 /**
- *  Put uui-stacked-color-swatch elements inside this element to create a color swatch selector.
+ *  Put uui-color-swatch elements inside this element to create a color swatch selector.
  *  @element uui-color-swatches
  *  @slot - Default slot for content.
  *  @fires {UUIColorSwatchesEvent} change - Fires when a color swatch is selected.
@@ -34,7 +34,7 @@ export class UUIColorSwatchesElement extends LabelMixin('label', LitElement) {
   @property({ type: Boolean, reflect: true }) disabled = false;
 
   @queryAssignedElements({ selector: 'uui-color-swatch' })
-  swatches!: Array<UUIColorSwatchElement>;
+  private _swatches!: Array<UUIColorSwatchElement>;
 
   private __activeElement: UUIColorSwatchElement | undefined;
   private get _activeElement(): UUIColorSwatchElement | undefined {
@@ -74,8 +74,8 @@ export class UUIColorSwatchesElement extends LabelMixin('label', LitElement) {
   }
 
   private _handleSlotChange() {
-    if (!this.swatches || this.swatches.length === 0) return;
-    this.swatches.forEach(swatch => {
+    if (!this._swatches || this._swatches.length === 0) return;
+    this._swatches.forEach(swatch => {
       swatch.setAttribute('aria-checked', 'false');
       swatch.setAttribute('role', 'radio');
 
@@ -99,7 +99,7 @@ export class UUIColorSwatchesElement extends LabelMixin('label', LitElement) {
     const target = event.target as UUIColorSwatchElement;
     //react only to selectable events from UUI-Color-Swatch elements
     //? can I use instanceof here instead? that creates a dependency on the uui-color-swatch element
-    if (!this.swatches.includes(target)) return;
+    if (!this._swatches.includes(target)) return;
     if (this._selectedElement) {
       this._selectedElement.selected = false;
       this._selectedElement.active = false;
@@ -115,7 +115,7 @@ export class UUIColorSwatchesElement extends LabelMixin('label', LitElement) {
 
   private _onDeselected = (event: Event) => {
     const target = event.target as UUIColorSwatchElement;
-    if (!this.swatches.includes(target)) return;
+    if (!this._swatches.includes(target)) return;
 
     if (this._activeElement === target) {
       this._activeElement = undefined;
@@ -137,7 +137,7 @@ export class UUIColorSwatchesElement extends LabelMixin('label', LitElement) {
    * @memberof UUIColorSwatchesElement
    */
   resetSelection() {
-    this.swatches.forEach(swatch => {
+    this._swatches.forEach(swatch => {
       swatch.selected = false;
       swatch.active = false;
       swatch.selectable = !swatch.disabled;
---
+++ packages/uui-color-swatches/lib/uui-color-swatches.stories.ts
@@ -0,0 +1,61 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers';
+import { repeat } from 'lit/directives/repeat.js';
+
+const swatchesColor = [
+  { label: 'Blood Orange', value: '#d0021b' },
+  { label: 'Avocado', value: '#417505' },
+  { label: 'Tufts Blue', value: '#4a90e2' },
+];
+
+const meta: Meta = {
+  id: 'uui-color-swatches',
+  component: 'uui-color-swatches',
+  title: 'Inputs/Color/Color Swatches',
+  args: {
+    swatches: swatchesColor,
+  },
+  argTypes: {
+    showLabel: { control: { type: 'boolean' } },
+  },
+  // prettier-ignore
+  render: (args: any) => html`
+<uui-color-swatches
+  ${spread(args, ['swatches', 'showLabel'])}
+  label="My color pallette">${repeat(args.swatches, (swatch: any) => {
+    const label = typeof swatch === 'string' ? swatch : swatch.label;
+    const value = typeof swatch === 'string' ? swatch : swatch.value;
+    return html`
+  <uui-color-swatch
+    label="${label}"
+    .showLabel=${args.showLabel}
+    .value=${value}></uui-color-swatch>`;
+  })}
+</uui-color-swatches>
+  `,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
+
+export const Preselected: Story = {
+  args: {
+    value: '#417505',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const ShowLabel: Story = {
+  args: {
+    showLabel: true,
+  },
+};
