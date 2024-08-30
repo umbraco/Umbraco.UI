bf9e39989a43856b677cc2f00e55a511ba408af8
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-08-30 15:28:50 +0200

    combobox list

---
+++ packages/uui-combobox-list/lib/uui-combobox-list.stories.ts
@@ -0,0 +1,29 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers';
+
+const meta: Meta = {
+  id: 'uui-combobox-list',
+  component: 'uui-combobox-list',
+  title: 'Inputs/Combobox/Combobox List',
+  render: args =>
+    html`<uui-combobox-list ${spread(args)}>
+      <uui-combobox-list-option>apple</uui-combobox-list-option>
+      <uui-combobox-list-option>orange</uui-combobox-list-option>
+      <uui-combobox-list-option>lemon</uui-combobox-list-option>
+    </uui-combobox-list>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
+export const Disabled: Story = {
+  render: args =>
+    html`<uui-combobox-list ${spread(args)}>
+      <uui-combobox-list-option>apple</uui-combobox-list-option>
+      <uui-combobox-list-option disabled>orange</uui-combobox-list-option>
+      <uui-combobox-list-option>lemon</uui-combobox-list-option>
+    </uui-combobox-list>`,
+};
