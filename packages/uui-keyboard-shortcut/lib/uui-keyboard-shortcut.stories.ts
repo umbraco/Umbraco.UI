0628daac5e8dd369361e4a67b7401a734563c5f5
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-04 22:03:59 +0200

    shortcuts

---
+++ packages/uui-keyboard-shortcut/lib/uui-keyboard-shortcut.stories.ts
@@ -0,0 +1,28 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+
+const meta: Meta = {
+  id: 'uui-keyboard-shortcut',
+  component: 'uui-keyboard-shortcut',
+  title: 'Displays/Keyboard Shortcut',
+  args: {
+    label: 'Label',
+  },
+  render: () => html`
+    <uui-keyboard-shortcut>
+      <uui-key>ALT</uui-key>
+      +
+      <uui-key>shift</uui-key>
+      +
+      <uui-key>&#8593;</uui-key>
+      +
+      <uui-key>z</uui-key>
+    </uui-keyboard-shortcut>
+  `,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
