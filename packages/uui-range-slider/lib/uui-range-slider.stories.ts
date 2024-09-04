7cc879bef389a4dba35b169323a16c1a90c3f95e
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-05 00:33:28 +0200

    range slider

---
+++ packages/uui-range-slider/lib/uui-range-slider.stories.ts
@@ -0,0 +1,41 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers';
+
+const meta: Meta = {
+  id: 'uui-range-slider',
+  component: 'uui-range-slider',
+  title: 'Inputs/Range Slider',
+  args: {
+    step: 10,
+    min: 0,
+    max: 100,
+    value: '0,20',
+    label: 'range',
+  },
+  render: args => html`<uui-range-slider ${spread(args)}></uui-range-slider>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
+
+export const HiddenValues: Story = {
+  args: {
+    hideStepValues: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Readonly: Story = {
+  args: {
+    readonly: true,
+  },
+};
