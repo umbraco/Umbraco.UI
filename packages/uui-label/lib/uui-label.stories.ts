518440b24731a02dafb5194274278f43d8efe555
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-04 22:04:04 +0200

    label

---
+++ packages/uui-label/lib/uui-label.stories.ts
@@ -0,0 +1,36 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { renderSlots, spread } from '../../../storyhelpers';
+
+import '@umbraco-ui/uui-input/lib';
+
+const meta: Meta = {
+  id: 'uui-label',
+  component: 'uui-label',
+  title: 'Inputs/Label',
+  args: {
+    slot: 'Label',
+    for: 'MyInput',
+  },
+  render: args =>
+    html`<uui-label ${spread(args)}>${renderSlots(args)}</uui-label>
+      <uui-input id="MyInput" label="My A11Y Label"></uui-input>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Required: Story = {
+  args: {
+    required: true,
+  },
+};
