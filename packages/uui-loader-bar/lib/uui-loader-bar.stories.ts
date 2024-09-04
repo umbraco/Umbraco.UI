8495437cbe4df8163c1b8afc7b66ebac956da5ca
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-04 22:14:43 +0200

    loaders

---
+++ packages/uui-loader-bar/lib/uui-loader-bar.stories.ts
@@ -0,0 +1,34 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers';
+
+const meta: Meta = {
+  id: 'uui-loader-bar',
+  component: 'uui-loader-bar',
+  title: 'Loaders/Loader Bar',
+  args: {
+    color: '#006eff',
+  },
+  argTypes: {
+    progress: { control: { type: 'range', min: 0, max: 100, step: 1 } },
+    animationDuration: {
+      control: { type: 'number', min: 0, max: 100 },
+    },
+  },
+  render: args =>
+    html`<uui-loader-bar
+      style="color: ${args.color}"
+      ${spread(args, ['color'])}></uui-loader-bar>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
+
+export const Progress: Story = {
+  args: {
+    progress: 50,
+  },
+};
---
+++ packages/uui-loader/lib/uui-loader.stories.ts
@@ -0,0 +1,18 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+
+const meta: Meta = {
+  id: 'uui-loader',
+  component: 'uui-loader',
+  title: 'Loaders/Loader',
+  args: {
+    color: 'color: #006eff',
+  },
+  render: args => html`<uui-loader style="color: ${args.color}"></uui-loader>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
