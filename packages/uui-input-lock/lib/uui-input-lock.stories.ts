e5c448812431133a6a0b6c06d005bae6c06014fb
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-04 21:56:17 +0200

    render slots

--- packages/uui-input-lock/lib/uui-input-lock.stories.ts
+++ packages/uui-input-lock/lib/uui-input-lock.stories.ts
@@ -1,7 +1,7 @@
 import '.';
 import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
-import { spread } from '../../../storyhelpers';
+import { renderSlots, spread } from '../../../storyhelpers';
 
 /**
  * uui-input-lock extends uui-input. See [uui-input](/docs/uui-input--docs) for more details.
@@ -13,7 +13,8 @@ const meta: Meta = {
   args: {
     label: 'Label',
   },
-  render: args => html`<uui-input-lock ${spread(args)}></uui-input-lock>`,
+  render: args =>
+    html`<uui-input-lock ${spread(args)}>${renderSlots(args)}</uui-input-lock>`,
 };
 
 export default meta;
--- packages/uui-input-password/lib/uui-input-password.stories.ts
+++ packages/uui-input-password/lib/uui-input-password.stories.ts
@@ -1,7 +1,7 @@
 import '.';
 import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
-import { spread } from '../../../storyhelpers';
+import { renderSlots, spread } from '../../../storyhelpers';
 
 /**
  * uui-input-password extends uui-input. See [uui-input](/docs/uui-input--docs) for more details.
@@ -14,7 +14,9 @@ const meta: Meta = {
     label: 'Label',
   },
   render: args =>
-    html`<uui-input-password ${spread(args)}></uui-input-password>`,
+    html`<uui-input-password ${spread(args)}
+      >${renderSlots(args)}</uui-input-password
+    >`,
 };
 
 export default meta;
