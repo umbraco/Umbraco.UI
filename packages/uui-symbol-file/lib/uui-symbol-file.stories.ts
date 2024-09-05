8fe80df711fb9339688852d8b98035809135d84c
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-05 02:23:41 +0200

    symbol file

---
+++ packages/uui-symbol-file/lib/uui-symbol-file.stories.ts
@@ -0,0 +1,20 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers';
+
+const meta: Meta = {
+  id: 'uui-symbol-file',
+  component: 'uui-symbol-file',
+  title: 'Symbols/File',
+  args: {
+    type: 'pdf',
+  },
+  render: args => html`<uui-symbol-file ${spread(args)}></uui-symbol-file>`,
+  decorators: [story => html`<div style="width: 240px">${story()}</div>`],
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
