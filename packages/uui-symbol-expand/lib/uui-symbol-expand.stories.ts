5e29be05fbaa53221a1489ec1716fc25a36e8037
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-05 02:23:01 +0200

    symbol expand

---
+++ packages/uui-symbol-expand/lib/uui-symbol-expand.stories.ts
@@ -0,0 +1,21 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers';
+
+const meta: Meta = {
+  id: 'uui-symbol-expand',
+  component: 'uui-symbol-expand',
+  title: 'Symbols/Expand',
+  render: args =>
+    html`<uui-symbol-expand
+      ${spread(args)}
+      @click=${(e: MouseEvent) => {
+        (e.target as any).open = !(e.target as any).open;
+      }}></uui-symbol-expand>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
