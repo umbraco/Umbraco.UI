34e8f9002a6c6f7b49d97f6389b5546ac5acc9ef
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-05 02:23:07 +0200

    symbol file dropzone

---
+++ packages/uui-symbol-file-dropzone/lib/uui-symbol-file-dropzone.stories.ts
@@ -0,0 +1,23 @@
+import '.';
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { spread } from '../../../storyhelpers';
+
+const meta: Meta = {
+  id: 'uui-symbol-file-dropzone',
+  component: 'uui-symbol-file-dropzone',
+  title: 'Symbols/File Dropzone',
+  render: args =>
+    html`<uui-symbol-file-dropzone ${spread(args)}></uui-symbol-file-dropzone>`,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {};
+
+export const Error: Story = {
+  args: {
+    error: true,
+  },
+};
