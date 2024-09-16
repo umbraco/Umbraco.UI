403164228ee6b3055aa038e555b09e4b52802688
Niels Lyngsø <niels.lyngso@gmail.com>
2024-09-16 08:05:11 +0200

    remove width and height

--- packages/uui-caret/lib/uui-caret.element.ts
+++ packages/uui-caret/lib/uui-caret.element.ts
@@ -27,8 +27,6 @@ export class UUICaretElement extends LitElement {
   render() {
     return html`<svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
--- packages/uui-symbol-expand/lib/uui-symbol-expand.element.ts
+++ packages/uui-symbol-expand/lib/uui-symbol-expand.element.ts
@@ -20,8 +20,6 @@ export class UUISymbolExpandElement extends LitElement {
   render() {
     return html`<svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
