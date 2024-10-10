bdfd6d3f0760bea909f72acfea561d01fb99a3ff
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-10-10 15:18:38 +0200

    chore: remove uncommented code

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -65,9 +65,6 @@ function WebComponentFormatter(customElements) {
       event.name = `${event.name} event`;
     }
 
-    // Find all names of properties
-    // const propertyNames = (tag.properties || []).map(p => p.name);
-
     // Run through all slots to clean them up a bit
     for (let slot of tag.slots || []) {
       // Replace the name of the default slot so Storybook will show it
@@ -75,14 +72,9 @@ function WebComponentFormatter(customElements) {
         slot.name = 'slot';
       } else {
         // Add slot to the name. This will allow us to filter out slots in various situations. Example the spread directive.
+        // Bug reported to Storybook here: https://github.com/storybookjs/storybook/issues/17733
         slot.name = `${slot.name} slot`;
       }
-
-      // // If the slot has the same name as a property, then add the word 'slot' to the name
-      // // Bug reported to Storybook here: https://github.com/storybookjs/storybook/issues/17733
-      // if (propertyNames.includes(slot.name)) {
-      //   slot.name = `${slot.name} slot`;
-      // }
 
       // Set type of slots
       if (typeof slot.type === 'undefined' || slot.type.length === 0) {
@@ -92,45 +84,6 @@ function WebComponentFormatter(customElements) {
   }
 
   return customElements;
-}
-
-function WebComponentFormatterNew(customElements) {
-  (customElements.modules ?? [])
-    .flatMap(module => module.declarations ?? [])
-    .forEach(declaration => {
-      declaration.attributes = [];
-
-      for (let cssProp of declaration.cssProperties || []) {
-        // If the property does not have a type, set it to string
-        if (!cssProp.type) {
-          cssProp.type = 'string';
-        }
-      }
-
-      declaration.members = declaration.members?.filter(
-        member => member.privacy !== 'private' && !member.name.startsWith('_'),
-      );
-
-      const propertyNames = (declaration.members || []).map(p => p.name);
-
-      for (let slot of declaration.slots || []) {
-        // Replace the name of the default slot so Storybook will show it
-        if (typeof slot.name === 'string' && slot.name.length === 0) {
-          slot.name = 'slot';
-        }
-
-        // If the slot has the same name as a property, then add the word 'slot' to the name
-        // Bug reported to Storybook here: https://github.com/storybookjs/storybook/issues/17733
-        if (propertyNames.includes(slot.name)) {
-          slot.name = `${slot.name} slot`;
-        }
-
-        // Set type of slots
-        if (typeof slot.type === 'undefined' || slot.type.length === 0) {
-          slot.type = 'HTMLElement';
-        }
-      }
-    });
 }
 
 export default preview;
