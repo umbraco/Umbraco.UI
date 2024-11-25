44b23c79db606a6d09e78a2497cb4811e21fcac1
Niels Lyngsø <niels.lyngso@gmail.com>
2024-11-25 13:27:17 +0100

    prevent clicking of slotted content in media card

--- packages/uui-card-media/lib/uui-card-media.element.ts
+++ packages/uui-card-media/lib/uui-card-media.element.ts
@@ -164,6 +164,7 @@ export class UUICardMediaElement extends UUICardElement {
         object-fit: cover;
         width: 100%;
         height: 100%;
+        pointer-events: none;
       }
 
       #open-part {
