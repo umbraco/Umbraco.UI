c2a5f20fc74b84e688ed69a942565d6fa19982c4
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-11-25 10:46:52 +0100

    docs(storybook): add more stories to symbol expand

--- packages/uui-symbol-expand/lib/uui-symbol-expand.story.ts
+++ packages/uui-symbol-expand/lib/uui-symbol-expand.story.ts
@@ -3,8 +3,10 @@ import readme from '../README.md?raw';
 import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { spread } from '../../../storyhelpers';
+import { UUISymbolExpandElement } from './uui-symbol-expand.element.js';
+import '@umbraco-ui/uui-button/lib';
 
-const meta: Meta = {
+const meta: Meta<UUISymbolExpandElement> = {
   id: 'uui-symbol-expand',
   component: 'uui-symbol-expand',
   title: 'Symbols/Expand',
@@ -25,3 +27,15 @@ export default meta;
 type Story = StoryObj;
 
 export const Default: Story = {};
+
+export const Open: Story = {
+  args: { open: true },
+};
+
+export const WithButton: Story = {
+  render: args =>
+    html`<uui-button look="primary">
+      Toggle
+      <uui-symbol-expand ${spread(args)}></uui-symbol-expand>
+    </uui-button>`,
+};
