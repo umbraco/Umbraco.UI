0901ebe13622e8a0f975566c1cbcb4f3030cdf99
Warren Buckley <warren@hackmakedo.com>
2025-01-02 09:45:15 +0000

    WIP: To add useful tests

--- packages/uui-copy/lib/uui-copy.test.ts
+++ packages/uui-copy/lib/uui-copy.test.ts
@@ -5,7 +5,7 @@ describe('UUICopyElement', () => {
   let element: UUICopyElement;
 
   beforeEach(async () => {
-    element = await fixture(html` <uui-copy></uui-copy> `);
+    element = await fixture(html`<uui-copy value="Oh hi there"></uui-copy>`);
   });
 
   it('is defined with its own instance', () => {
@@ -14,5 +14,54 @@ describe('UUICopyElement', () => {
 
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
+  });
+
+  it('renders correctly', async () => {
+    expect(element).shadowDom.to.equal(`
+      <uui-button>
+        <slot>
+          <uui-icon name="copy"></uui-icon> Copy
+        </slot>
+      </uui-button>
+    `);
+  });
+
+  it('copies the value property to clipboard when button is clicked', async () => {
+    const button = element.shadowRoot?.querySelector('uui-button');
+    // // Mock the clipboard API
+    // navigator.clipboard = {
+    //   writeText: async text => {
+    //     expect(text).to.equal('Test Text');
+    //     return Promise.resolve();
+    //   },
+    // };
+
+    button?.click();
+  });
+
+  it('fires copying and copied events', async () => {
+    const button = element.shadowRoot?.querySelector('uui-button');
+    // // Mock the clipboard API
+    // navigator.clipboard = {
+    //   writeText: async text => {
+    //     return Promise.resolve();
+    //   },
+    // };
+
+    let copyingEventFired = false;
+    let copiedEventFired = false;
+
+    element.addEventListener('copying', () => {
+      copyingEventFired = true;
+    });
+
+    element.addEventListener('copied', () => {
+      copiedEventFired = true;
+    });
+
+    button?.click();
+
+    expect(copyingEventFired).to.be.true;
+    expect(copiedEventFired).to.be.true;
   });
 });
