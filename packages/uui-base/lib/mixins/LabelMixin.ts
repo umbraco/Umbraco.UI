91d56441e24360db94eb977ba3df3cb19171f347
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-05-23 10:16:22 +0200

    revert change to firstUpdated

--- packages/uui-base/lib/mixins/LabelMixin.ts
+++ packages/uui-base/lib/mixins/LabelMixin.ts
@@ -37,7 +37,8 @@ export const LabelMixin = <T extends Constructor<LitElement>>(
     @property({ type: String })
     public label!: string;
 
-    firstUpdated() {
+    firstUpdated(_changedProperties: Map<string | number | symbol, unknown>) {
+      super.firstUpdated(_changedProperties);
       if (!this.label) {
         console.warn(this.tagName + ' needs a `label`', this);
       }
