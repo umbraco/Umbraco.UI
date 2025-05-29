c58b6128927db6501d89b6d0f8aba7a2138ab65f
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2025-05-29 22:26:21 +0200

    Add uui-divider component

---
+++ packages/uui-divider/README.md
@@ -0,0 +1,31 @@
+# uui-divider
+
+![npm](https://img.shields.io/npm/v/@umbraco-ui/uui-divider?logoColor=%231B264F)
+
+Umbraco style divider component.
+
+## Installation
+
+### ES imports
+
+```zsh
+npm i @umbraco-ui/uui-divider
+```
+
+Import the registration of `<uui-divider>` via:
+
+```javascript
+import '@umbraco-ui/uui-divider';
+```
+
+When looking to leverage the `UUIDividerElement` base class as a type and/or for extension purposes, do so via:
+
+```javascript
+import { UUIDividerElement } from '@umbraco-ui/uui-divider';
+```
+
+## Usage
+
+```html
+<uui-divider></uui-divider>
+```
---
+++ packages/uui-divider/lib/uui-divider.element.ts
@@ -0,0 +1,60 @@
+import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
+import { css, LitElement } from 'lit';
+import { property } from 'lit/decorators.js';
+
+/**
+ * @element uui-divider
+ * @cssprop --uui-divider-width - Divider width
+ * @cssprop --uui-divider-color - Divider color
+ * @cssprop --uui-divider-spacing - Spacing around the divider
+ */
+@defineElement('uui-divider')
+export class UUIDividerElement extends LitElement {
+  /**
+   * Draws the divider in a vertical orientation.
+   * @type {boolean}
+   * @attr
+   * @default false
+   **/
+  @property({ type: Boolean, reflect: true }) vertical = false;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.setAttribute('role', 'separator');
+  }
+
+  updated(_changedProperties: Map<string | number | symbol, unknown>) {
+    super.updated(_changedProperties);
+    if (_changedProperties.has('vertical')) {
+      this.setAttribute(
+        'aria-orientation',
+        this.vertical ? 'vertical' : 'horizontal',
+      );
+    }
+  }
+
+  static styles = [
+    css`
+      :host(:not([vertical])) {
+        display: block;
+        border-top: var(--uui-divider-width, 1px) solid
+          var(--uui-divider-color, var(--uui-color-border));
+        margin: var(--uui-divider-spacing) 0;
+      }
+
+      :host([vertical]) {
+        display: inline-block;
+        height: 100%;
+        border-left: solid var(--uui-divider-border-width, 1px)
+          var(--uui-divider-color, var(--uui-color-border));
+        margin: 0 var(--uui-divider-spacing);
+      }
+    `,
+  ];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'uui-divider': UUIDividerElement;
+  }
+}
