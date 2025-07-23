2199308df4f4f257399a9dc077dacbef1e4e6e43
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2025-07-23 14:43:23 +0200

    Add input color component

---
+++ packages/uui-input-color/lib/uui-input-color.element.ts
@@ -0,0 +1,56 @@
+import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
+import { demandCustomElement } from '@umbraco-ui/uui-base/lib/utils';
+import { css, html } from 'lit';
+import { InputType, UUIInputElement } from '@umbraco-ui/uui-input/lib';
+import { property, state } from 'lit/decorators.js';
+
+/**
+ * @element uui-input-color
+ * @extends uui-input
+ */
+@defineElement('uui-input-color')
+export class UUIInputColorElement extends UUIInputElement {
+  @state()
+  private inputType: InputType = 'color';
+
+  // this overrides the inherited type property, and moves the input's type handling to the passwordType state.
+  @property()
+  get type() {
+    return this.inputType;
+  }
+  set type(newValue) {
+    this.inputType = newValue;
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    demandCustomElement(this, 'uui-swatch');
+  }
+
+  renderPrepend() {
+    return html`<uui-swatch
+      .disabled=${this.disabled}
+      .readonly=${this.readonly}
+      .value=${this.value}>
+    </uui-swatch>`;
+  }
+
+  static styles = [...UUIInputElement.styles, css``];
+
+  /*render(){
+      return html`
+          <div class="color-wrapper">
+            <uui-input id="input" label="Value" placeholder="Value" required style="--uui-show-focus-outline: 0;">
+              <uui-color-swatch slot="prepend" label="" value="#"></uui-color-swatch>
+            </uui-input>
+            <input type="color" id="color" aria-hidden="true" value="">
+          </div>`;
+  }*/
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'uui-input-color': UUIInputColorElement;
+  }
+}
---
+++ packages/uui-input-color/lib/uui-input-color.story.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+
+import './uui-input-color.element';
+import type { UUIInputColorElement } from './uui-input-color.element';
+import readme from '../README.md?raw';
+
+const meta: Meta<UUIInputColorElement> = {
+  id: 'uui-input-color',
+  title: 'Inputs/Input Color',
+  component: 'uui-input-color',
+  parameters: {
+    readme: { markdown: readme },
+    docs: {
+      source: {
+        code: `<uui-input-color></uui-input-color>`,
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<UUIInputColorElement>;
+
+export const Overview: Story = {};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Readonly: Story = {
+  args: {
+    readonly: true,
+  },
+};
