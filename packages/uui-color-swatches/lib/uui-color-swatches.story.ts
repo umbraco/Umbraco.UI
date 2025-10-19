1cca5545b782035a3424cd7301aeb3314ce9c49d
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2025-10-19 14:37:55 +0200

    Add example of transparent swatches

--- packages/uui-color-swatches/lib/uui-color-swatches.story.ts
+++ packages/uui-color-swatches/lib/uui-color-swatches.story.ts
@@ -17,6 +17,25 @@ const swatches = [
   { label: 'Dusty Grey', value: '#9b9b9b' },
 ];
 
+const swatchesTransparent = [
+  'rgba(208, 2, 27, 0.5)',
+  'rgba(245, 166, 35, 0.5)',
+  'rgba(248, 231, 28, 0.5)',
+  'rgba(139, 87, 42, 0.5)',
+  'rgba(126, 211, 33, 0.5)',
+  'rgba(65, 117, 5, 0.5)',
+  'rgba(189, 16, 224, 0.5)',
+  'rgba(144, 19, 254, 0.5)',
+  'rgba(74, 144, 226, 0.5)',
+  'rgba(80, 227, 194, 0.5)',
+  'rgba(184, 233, 134, 0.5)',
+  'rgba(0, 0, 0, 0.5)',
+  'rgba(68, 68, 68, 0.5)',
+  'rgba(136, 136, 136, 0.5)',
+  'rgba(204, 204, 204, 0.5)',
+  'rgba(255, 255, 255, 0.5)',
+];
+
 const gradients = [
   {
     label: 'Vital Ocean',
@@ -126,6 +145,12 @@ export const Gradient: Story = {
     swatches: gradients,
     showLabel: true,
     value: 'lemon-twist',
+  },
+};
+
+export const Transparent: Story = {
+  args: {
+    swatches: swatchesTransparent,
   },
 };
 
