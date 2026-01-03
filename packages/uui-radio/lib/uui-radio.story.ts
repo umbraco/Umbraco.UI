a44920d342dc3ce5edd9ffe4153a92bd523bd83f
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2026-01-03 18:26:15 +0100

    Fix wording

--- packages/uui-radio/lib/uui-radio.story.ts
+++ packages/uui-radio/lib/uui-radio.story.ts
@@ -80,8 +80,8 @@ export const RadioGroup: Story = {
         label="Social - I'm here for the people, the vibes, the good times. I would probably hang out with you all if I didn't work with the product.">
       </uui-radio>
       <uui-radio
-        value="Keen but irregular - I love contributing when I have time but time is short these days since the new ."
-        label="Keen but irregular - I love contributing when I have time but time is short these days since the new .">
+        value="Keen but irregular - I love contributing when I have time but time is short these days."
+        label="Keen but irregular - I love contributing when I have time but time is short these days.">
       </uui-radio>
       <uui-radio
         value="Shy - I would do more but I am not sure how welcome or valuable my experience is."
