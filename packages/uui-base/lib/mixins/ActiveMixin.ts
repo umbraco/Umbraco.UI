5ed40481d9e58afdfa92ed96ac7cb90f4d466ae6
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2026-02-16 16:11:43 +0100

    fix(uui-base): deprecates any class without `UUI`-prefix (#1272)

--- packages/uui-base/lib/mixins/ActiveMixin.ts
+++ packages/uui-base/lib/mixins/ActiveMixin.ts
@@ -3,7 +3,7 @@ import { property } from 'lit/decorators.js';
 
 type Constructor<T = {}> = new (...args: any[]) => T;
 
-export declare class ActiveMixinInterface {
+export declare class UUIActiveMixinInterface {
   /**
    * Set this boolean to true for then the related composition is sorted.
    * @type {boolean}
@@ -22,7 +22,7 @@ export declare class ActiveMixinInterface {
 export const ActiveMixin = <T extends Constructor<LitElement>>(
   superClass: T,
 ) => {
-  class ActiveMixinClass extends superClass {
+  class UUIActiveMixinClass extends superClass {
     /**
      * Set this boolean to true for then the related composition is sorted.
      * @type {boolean}
@@ -32,5 +32,9 @@ export const ActiveMixin = <T extends Constructor<LitElement>>(
     public active = false;
   }
 
-  return ActiveMixinClass as unknown as Constructor<ActiveMixinInterface> & T;
+  return UUIActiveMixinClass as unknown as Constructor<UUIActiveMixinInterface> &
+    T;
 };
+
+/** @deprecated Use UUIActiveMixinInterface instead */
+export type ActiveMixinInterface = UUIActiveMixinInterface;
