ba82eeda0f6d1bf5e3ddf1b99e609f999e91bdfc
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2026-05-24 17:27:19 +0200

    Allow to override addon border radius

--- src/components/input-group-addon/input-group-addon.element.ts
+++ src/components/input-group-addon/input-group-addon.element.ts
@@ -2,6 +2,7 @@ import { css, html, LitElement } from 'lit';
 
 /**
  * @element uui-input-group-addon
+ * @cssprop --uui-input-group-addon-border-radius - Border radius
  */
 export class UUIInputGroupAddonElement extends LitElement {
   render() {
@@ -14,6 +15,10 @@ export class UUIInputGroupAddonElement extends LitElement {
         padding-inline: var(--uui-size-space-4);
         background: var(--uui-color-surface-alt);
         border: 1px solid var(--uui-color-border);
+        border-radius: var(
+          --uui-input-group-addon-border-radius,
+          var(--uui-border-radius)
+        );
         color: var(--uui-color-text);
         font-size: var(--uui-type-small-size);
         display: inline-flex;
