971245248121e25f3a33e211a1c3a41d3d048cba
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2026-05-25 07:06:38 +0200

    Update example

--- src/components/input-group/input-group-select.example.ts
+++ src/components/input-group/input-group-select.example.ts
@@ -2,16 +2,17 @@ import { defineElement } from '../../internal/registration/index.js';
 import { html, LitElement } from 'lit';
 import { state } from 'lit/decorators.js';
 import type { UUISelectOption } from '../select/select.element.js';
+import type { UUISelectEvent } from '../select/UUISelectEvent.js';
 
 import '../input-group/input-group.element.js';
 import '../input-group-addon/input-group-addon.element.js';
 import '../select/select.element.js';
 
 const options: Array<UUISelectOption> = [
-  { name: '🐰 Rabbit', value: 'rabbit' },
-  { name: '🦒 Giraffe', value: 'giraffe' },
-  { name: '🦈 Shark', value: 'shark' },
-  { name: '🦄 Unicorn', value: 'unicorn' },
+  { name: 'Rabbit', value: 'rabbit' },
+  { name: 'Giraffe', value: 'giraffe' },
+  { name: 'Shark', value: 'shark' },
+  { name: 'Unicorn', value: 'unicorn' },
 ];
 
 @defineElement('uui-input-group-select-example')
@@ -29,18 +30,16 @@ export class UUIInputGroupSelectExample extends LitElement {
   render() {
     return html`<uui-input-group>
       <uui-input-group-addon slot="prepend">
-        ${this.emojiMap[this.value] ?? ''}
-      </uui-input-group-addon>
-      <uui-select
+        ${this.emojiMap[this.value] ?? ''}</uui-input-group-addon
+      ><uui-select
         .options=${options}
         .value=${this.value}
         @change=${this.#onChange}></uui-select>
     </uui-input-group>`;
   }
 
-  #onChange = (e: Event) => {
-    const target = e.target as HTMLInputElement;
-    this.value = target.value;
+  #onChange = (e: UUISelectEvent) => {
+    this.value = e.target.value as string;
   };
 }
 
