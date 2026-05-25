fcadfdc98d1adabf13eb03b34740dd6ef59cd5f7
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2026-05-25 10:14:15 +0200

    Adjust styling

--- src/components/input-group/input-group.element.ts
+++ src/components/input-group/input-group.element.ts
@@ -26,14 +26,12 @@ export class UUIInputGroupElement extends LitElement {
       border-radius: var(--uui-input-border-radius, var(--uui-border-radius));
     }
 
-    /* remove left radius if not first */
     ::slotted(uui-input:not(:first-child)),
     ::slotted(uui-select:not(:first-child)) {
       border-top-left-radius: 0;
       border-bottom-left-radius: 0;
     }
 
-    /* remove right radius if not last */
     ::slotted(uui-input:not(:last-child)),
     ::slotted(uui-select:not(:last-child)) {
       border-top-right-radius: 0;
@@ -46,35 +44,45 @@ export class UUIInputGroupElement extends LitElement {
 
     ::slotted(uui-input-group-addon),
     ::slotted(uui-button) {
-      border-radius: var(--uui-input-border-radius, var(--uui-border-radius));
-    }
-
-    /* not first addon → remove LEFT border + radius */
-    ::slotted(uui-input-group-addon:not(:first-child)),
-    ::slotted(uui-button:not(:first-child)) {
-      border-left: none;
-      border-top-left-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-
-    /* not last addon → remove RIGHT border + radius */
-    ::slotted(uui-input-group-addon:not(:last-child)),
-    ::slotted(uui-button:not(:last-child)) {
-      border-right: none;
-      border-top-right-radius: 0;
-      border-bottom-right-radius: 0;
-    }
-
-    /* middle addons fully square */
-    ::slotted(uui-input-group-addon:not(:first-child):not(:last-child)),
-    ::slotted(uui-button:not(:first-child):not(:last-child)) {
       border-radius: 0;
     }
 
-    /* layout */
-    ::slotted(uui-input),
-    ::slotted(uui-select) {
-      flex: 1;
+    /* FIRST addon → only LEFT radius */
+    ::slotted(uui-input-group-addon:first-child),
+    ::slotted(uui-button:first-child) {
+      border-top-left-radius: var(
+        --uui-input-border-radius,
+        var(--uui-border-radius)
+      );
+      border-bottom-left-radius: var(
+        --uui-input-border-radius,
+        var(--uui-border-radius)
+      );
+    }
+
+    /* LAST addon → only RIGHT radius */
+    ::slotted(uui-input-group-addon:last-child),
+    ::slotted(uui-button:last-child) {
+      border-top-right-radius: var(
+        --uui-input-border-radius,
+        var(--uui-border-radius)
+      );
+      border-bottom-right-radius: var(
+        --uui-input-border-radius,
+        var(--uui-border-radius)
+      );
+    }
+
+    /* not first addon → remove LEFT border */
+    ::slotted(uui-input-group-addon:not(:first-child)),
+    ::slotted(uui-button:not(:first-child)) {
+      border-left: none;
+    }
+
+    /* not last addon → remove RIGHT border (only if another addon follows) */
+    ::slotted(uui-input-group-addon:not(:last-child)),
+    ::slotted(uui-button:not(:last-child)) {
+      border-right: none;
     }
   `;
 }
