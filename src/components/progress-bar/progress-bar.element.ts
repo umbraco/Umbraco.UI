fb194772e420f1d7fe1893f5028e796c85cd34f5
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2026-07-29 11:35:33 +0200

    Add aria attributes to progress bar

--- src/components/progress-bar/progress-bar.element.ts
+++ src/components/progress-bar/progress-bar.element.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from 'lit';
 import { property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import { styleMap } from 'lit/directives/style-map.js';
 
 const clamp = (num: number, min: number, max: number) =>
@@ -10,6 +11,16 @@ const clamp = (num: number, min: number, max: number) =>
  */
 export class UUIProgressBarElement extends LitElement {
   private _progress = 0;
+
+  /**
+   * Accessible label for the progress bar.
+   * @type {string}
+   * @attr label
+   * @default undefined
+   */
+  @property({ type: String })
+  label?: string;
+
   /**
    * Set this to a number between 0 and 100 to reflect the progress of some operation.
    * @type {number}
@@ -33,7 +44,20 @@ export class UUIProgressBarElement extends LitElement {
 
   render() {
     return html`
-      <div id="bar" style=${styleMap(this._getProgressStyle())}></div>
+      <progress
+        id="bar"
+        aria-label=${ifDefined(
+          this.getAttribute('aria-label') || this.label || undefined,
+        )}
+        aria-labelledby=${ifDefined(
+          this.getAttribute('aria-labelledby') || undefined,
+        )}
+        aria-valuemin="0"
+        aria-valuemax="100"
+        aria-valuenow=${this._progress}
+        value=${this._progress}
+        max="100"
+        style=${styleMap(this._getProgressStyle())}></progress>
     `;
   }
 
