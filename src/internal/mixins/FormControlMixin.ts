c270e05f7e06425f3af11814537f10a3a4e6931d
Niels Lyngsø <niels.lyngso@gmail.com>
2026-08-21 15:51:58 +0200

    prevent form control mixin firing the same event twice

--- src/internal/mixins/FormControlMixin.ts
+++ src/internal/mixins/FormControlMixin.ts
@@ -433,12 +433,20 @@ export function UUIFormControlBaseMixin<
     }
 
     #lastEventType: string | undefined = undefined;
+    #lastMessage: string | undefined = undefined;
     #dispatchValidationState() {
       if (this.#validity.valid) {
         if (this.#lastEventType === UUIFormControlEvent.VALID) return;
         this.#lastEventType = UUIFormControlEvent.VALID;
+        this.#lastMessage = this.validationMessage;
         this.dispatchEvent(new UUIFormControlEvent(UUIFormControlEvent.VALID));
       } else if (this._pristine === false) {
+        if (
+          this.#lastEventType === UUIFormControlEvent.INVALID &&
+          this.#lastMessage === this.validationMessage
+        ) {
+          return;
+        }
         // Only fire invalid events when the form control is not pristine, as we do not want to show validation messages for untouched form controls. [NL]
         // Always fire an Invalid event when the validity is invalid, even if the last event was also Invalid, as the message might have changed. [NL]
         this.#lastEventType = UUIFormControlEvent.INVALID;
@@ -481,6 +489,8 @@ export function UUIFormControlBaseMixin<
       this.#hadFocus = false;
       this.value = this.getInitialValue() ?? this.getDefaultValue();
       this.#valueOnFocus = undefined;
+      this.#lastEventType = undefined;
+      this.#lastMessage = undefined;
     }
 
     protected getDefaultValue(): DefaultValueType {
